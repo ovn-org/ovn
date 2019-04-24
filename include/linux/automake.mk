5d08a5dbba23a26840db262b37730b21dacce7b2
Mark Michelson <mmichels@redhat.com>
2019-04-24 11:28:45 -0400

    Remove unnecessary directories.
    This includes the following directories:
    * datapath
    * datapath-windows
    * include
    * lib
    * ofproto
    * ovsdb
    * utilities
    * vswitchd
    * vtep
    * windows

--- include/linux/automake.mk
+++
@@ -1,6 +0,0 @@
-noinst_HEADERS += \
-	include/linux/pkt_cls.h \
-	include/linux/tc_act/tc_pedit.h \
-	include/linux/tc_act/tc_skbedit.h \
-	include/linux/tc_act/tc_tunnel_key.h \
-	include/linux/tc_act/tc_vlan.h
