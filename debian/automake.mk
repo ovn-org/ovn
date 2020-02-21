6b47f4fd81c2007f793628b6b914335617862c00
James Page <james.page@ubuntu.com>
2020-02-21 09:59:24 +0000

    debian: Initial packaging for OVN
    Update debian packaging folder based on packaging work
    completed in Ubuntu.
    
    Signed-off-by: James Page <james.page@ubuntu.com>

--- debian/automake.mk
+++ debian/automake.mk
@@ -2,84 +2,46 @@ EXTRA_DIST += \
 	debian/changelog \
 	debian/compat \
 	debian/control \
-	debian/control.modules.in \
 	debian/copyright \
 	debian/copyright.in \
-	debian/dkms.conf.in \
-	debian/dirs \
-	debian/libopenvswitch.install \
-	debian/libopenvswitch-dev.install \
-	debian/openvswitch-common.dirs \
-	debian/openvswitch-common.docs \
-	debian/openvswitch-common.install \
-	debian/openvswitch-common.manpages \
-	debian/openvswitch-datapath-module-_KVERS_.postinst.modules.in \
-	debian/openvswitch-datapath-dkms.postinst \
-	debian/openvswitch-datapath-dkms.prerm \
-	debian/openvswitch-datapath-source.README.Debian \
-	debian/openvswitch-datapath-source.copyright \
-	debian/openvswitch-datapath-source.dirs \
-	debian/openvswitch-datapath-source.install \
-	debian/openvswitch-ipsec.dirs \
-	debian/openvswitch-ipsec.init \
-	debian/openvswitch-ipsec.install \
-	debian/openvswitch-pki.dirs \
-	debian/openvswitch-pki.postinst \
-	debian/openvswitch-pki.postrm \
-	debian/openvswitch-switch.README.Debian \
-	debian/openvswitch-switch.dirs \
-	debian/openvswitch-switch.init \
-	debian/openvswitch-switch.install \
-	debian/openvswitch-switch.logrotate \
-	debian/openvswitch-switch.manpages \
-	debian/openvswitch-switch.postinst \
-	debian/openvswitch-switch.postrm \
-	debian/openvswitch-switch.template \
-	debian/openvswitch-switch.links \
-	debian/openvswitch-test.dirs \
-	debian/openvswitch-test.install \
-	debian/openvswitch-test.manpages \
-	debian/openvswitch-testcontroller.README.Debian \
-	debian/openvswitch-testcontroller.default \
-	debian/openvswitch-testcontroller.dirs \
-	debian/openvswitch-testcontroller.init \
-	debian/openvswitch-testcontroller.install \
-	debian/openvswitch-testcontroller.manpages \
-	debian/openvswitch-testcontroller.postinst \
-	debian/openvswitch-testcontroller.postrm \
-	debian/openvswitch-vtep.default \
-	debian/openvswitch-vtep.dirs \
-	debian/openvswitch-vtep.init \
-	debian/openvswitch-vtep.install \
-	debian/openvswitch-vtep.manpages \
-	debian/ovn-central.dirs \
-	debian/ovn-central.init \
+	debian/ovn-central.default \
 	debian/ovn-central.install \
 	debian/ovn-central.manpages \
-	debian/ovn-central.postinst \
+	debian/ovn-central.ovn-northd.service \
+	debian/ovn-central.ovn-ovsdb-server-nb.service \
+	debian/ovn-central.ovn-ovsdb-server-sb.service \
 	debian/ovn-central.postrm \
-	debian/ovn-central.template \
-	debian/ovn-controller-vtep.init \
-	debian/ovn-controller-vtep.install \
-	debian/ovn-controller-vtep.manpages \
+	debian/ovn-central.service \
+	debian/ovn-common.dirs \
+	debian/ovn-common.docs \
 	debian/ovn-common.install \
+	debian/ovn-common.logrotate \
 	debian/ovn-common.manpages \
 	debian/ovn-common.postinst \
 	debian/ovn-common.postrm \
+	debian/ovn-controller-vtep.install \
+	debian/ovn-controller-vtep.manpages \
+	debian/ovn-controller-vtep.service \
+	debian/ovn-doc.doc-base \
+	debian/ovn-doc.install \
 	debian/ovn-docker.install \
-	debian/ovn-host.dirs \
-	debian/ovn-host.init \
+	debian/ovn-host.default \
 	debian/ovn-host.install \
 	debian/ovn-host.manpages \
-	debian/ovn-host.postinst \
+	debian/ovn-host.ovn-controller.service \
 	debian/ovn-host.postrm \
-	debian/ovn-host.template \
-	debian/python-openvswitch.dirs \
-	debian/python-openvswitch.install \
+	debian/ovn-host.service \
+	debian/ovn-ic-db.install \
+	debian/ovn-ic-db.manpages \
+	debian/ovn-ic-db.ovn-ovsdb-server-ic-nb.service \
+	debian/ovn-ic-db.ovn-ovsdb-server-ic-sb.service \
+	debian/ovn-ic-db.service \
+	debian/ovn-ic.install \
+	debian/ovn-ic.manpages \
+	debian/ovn-ic.service \
 	debian/rules \
-	debian/rules.modules \
-	debian/ifupdown.sh \
-	debian/source/format
+	debian/source/format \
+	debian/watch
 
 check-debian-changelog-version:
 	@DEB_VERSION=`echo '$(VERSION)' | sed 's/pre/~pre/'`;		     \
@@ -100,5 +62,3 @@ $(srcdir)/debian/copyright: AUTHORS.rst debian/copyright.in
 		sed -n -e '/^$$/q' -e 's/^/  /p';			   \
 	  sed -e '34,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
 	} > $@
-
-CLEANFILES += debian/copyright
