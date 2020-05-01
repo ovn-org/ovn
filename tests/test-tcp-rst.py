3c2908b51e7ed70b1a50379c54b41802ca7ce70b
Han Zhou <hzhou@ovn.org>
2020-05-01 09:37:44 -0700

    test-tcp-rst.py: Fix flake8 errors.
    Build fails due to flake8 errors:
    ===
    ../tests/test-tcp-rst.py:20:1: F401 'sys' imported but unused
    ../tests/test-tcp-rst.py:25:80: E501 line too long (85 > 79 characters)
    ../tests/test-tcp-rst.py:36:80: E501 line too long (90 > 79 characters)
    Makefile:2944: recipe for target 'flake8-check' failed
    make[1]: *** [flake8-check] Error 1
    ===
    
    Fixes: b4b6817 ("Fix conntrack entry leaks because of TCP RST packets not sent to conntrack.")
    Acked-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: Han Zhou <hzhou@ovn.org>

--- tests/test-tcp-rst.py
+++ tests/test-tcp-rst.py
@@ -17,12 +17,12 @@
 # resets the connection.
 import argparse
 import socket
-import sys
 import struct
 import time
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument("--src-port", type=int, default=11337, help="source port to use")
+parser.add_argument("--src-port", type=int, default=11337,
+                    help="source port to use")
 parser.add_argument("--dst-port", type=int, help="dst port to use")
 parser.add_argument("--dst-ip", help="server ip to use")
 args = parser.parse_args()
@@ -33,5 +33,6 @@ sock.connect(server_address)
 l_onoff = 1
 l_linger = 0
 time.sleep(1)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', l_onoff, l_linger))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER,
+                struct.pack('ii', l_onoff, l_linger))
 sock.close()
