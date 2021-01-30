029c8308d24e5e98cc002ea70ea7d6b64d16e7ed
Girish Moodalbail <gmoodalbail@nvidia.com>
2021-01-30 01:02:47 -0800

    add support for building debian packages again
    Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

--- debian/automake.mk
+++ debian/automake.mk
@@ -2,56 +2,8 @@ EXTRA_DIST += \
 	debian/changelog \
 	debian/compat \
 	debian/control \
-	debian/control.modules.in \
-	debian/copyright \
 	debian/copyright.in \
-	debian/dkms.conf.in \
 	debian/dirs \
-	debian/libopenvswitch.install \
-	debian/libopenvswitch-dev.install \
-	debian/openvswitch-common.dirs \
-	debian/openvswitch-common.docs \
-	debian/openvswitch-common.install \
-	debian/openvswitch-common.manpages \
-	debian/openvswitch-datapath-module-_KVERS_.postinst.modules.in \
-	debian/openvswitch-datapath-dkms.postinst \
-	debian/openvswitch-datapath-dkms.prerm \
-	debian/openvswitch-datapath-source.README.Debian \
-	debian/openvswitch-datapath-source.copyright \
-	debian/openvswitch-datapath-source.dirs \
-	debian/openvswitch-datapath-source.install \
-	debian/openvswitch-ipsec.dirs \
-	debian/openvswitch-ipsec.init \
-	debian/openvswitch-ipsec.install \
-	debian/openvswitch-pki.dirs \
-	debian/openvswitch-pki.postinst \
-	debian/openvswitch-pki.postrm \
-	debian/openvswitch-switch.README.Debian \
-	debian/openvswitch-switch.dirs \
-	debian/openvswitch-switch.init \
-	debian/openvswitch-switch.install \
-	debian/openvswitch-switch.logrotate \
-	debian/openvswitch-switch.manpages \
-	debian/openvswitch-switch.postinst \
-	debian/openvswitch-switch.postrm \
-	debian/openvswitch-switch.template \
-	debian/openvswitch-switch.links \
-	debian/openvswitch-test.dirs \
-	debian/openvswitch-test.install \
-	debian/openvswitch-test.manpages \
-	debian/openvswitch-testcontroller.README.Debian \
-	debian/openvswitch-testcontroller.default \
-	debian/openvswitch-testcontroller.dirs \
-	debian/openvswitch-testcontroller.init \
-	debian/openvswitch-testcontroller.install \
-	debian/openvswitch-testcontroller.manpages \
-	debian/openvswitch-testcontroller.postinst \
-	debian/openvswitch-testcontroller.postrm \
-	debian/openvswitch-vtep.default \
-	debian/openvswitch-vtep.dirs \
-	debian/openvswitch-vtep.init \
-	debian/openvswitch-vtep.install \
-	debian/openvswitch-vtep.manpages \
 	debian/ovn-central.dirs \
 	debian/ovn-central.init \
 	debian/ovn-central.install \
@@ -66,7 +18,6 @@ EXTRA_DIST += \
 	debian/ovn-common.manpages \
 	debian/ovn-common.postinst \
 	debian/ovn-common.postrm \
-	debian/ovn-docker.install \
 	debian/ovn-host.dirs \
 	debian/ovn-host.init \
 	debian/ovn-host.install \
@@ -74,11 +25,7 @@ EXTRA_DIST += \
 	debian/ovn-host.postinst \
 	debian/ovn-host.postrm \
 	debian/ovn-host.template \
-	debian/python-openvswitch.dirs \
-	debian/python-openvswitch.install \
 	debian/rules \
-	debian/rules.modules \
-	debian/ifupdown.sh \
 	debian/source/format
 
 check-debian-changelog-version:
