02823c72c141e9983b01d0c5d40b81495b536fd5
Frode Nordahl <frode.nordahl@canonical.com>
2021-03-05 13:16:23 +0100

    northd: Amend RBAC rules for Port_Binding table
    When `ovn-controller` claims a virtual lport it will update the
    Port_Binding table with which chassis currently has claimed the
    port as well as recording information about the virtual parent
    lport [0].
    
    When `ovn-controller` claims a lport it will also update the
    encap field of the Port_Binding table if set and an update is
    needed.
    
    The current RBAC rules does not allow for these updates.
    
    0: https://github.com/ovn-org/ovn/blob/b7b0fbdab03ce8b39d5bdc114876e6b0d0683892/controller/pinctrl.c#L6150
    Fixes: 054f4c85c ("Add a new logical switch port type - 'virtual'")
    Fixes: 6c8b9a132 (" ovn-controller: Store the local port bindings in the runtime data I-P state")
    Reported-At: https://bugs.launchpad.net/ubuntu/+source/ovn/+bug/1917475
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>
    
    (cherry-picked from master commit 525d78946e6db29430fc2f946b9348eda6356fc6)

--- northd/ovn-northd.c
+++ northd/ovn-northd.c
@@ -12624,7 +12624,7 @@ static const char *rbac_encap_update[] =
 static const char *rbac_port_binding_auth[] =
     {""};
 static const char *rbac_port_binding_update[] =
-    {"chassis"};
+    {"chassis", "encap", "virtual_parent"};
 
 static const char *rbac_mac_binding_auth[] =
     {""};
