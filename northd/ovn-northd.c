8848c622bd01f54cab0be14a69478cc2ce0a19b7
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2021-04-07 10:26:31 +0100

    ovn-northd: Add configuration option for parallel lflow build
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- northd/ovn-northd.c
+++ northd/ovn-northd.c
@@ -13203,6 +13203,10 @@ ovnnb_db_run(struct northd_context *ctx,
     northd_probe_interval_nb = get_probe_interval(ovnnb_db, nb);
     northd_probe_interval_sb = get_probe_interval(ovnsb_db, nb);
 
+    use_parallel_build =
+        (smap_get_bool(&nb->options, "use_parallel_build", false) &&
+         ovn_can_parallelize_hashes(false));
+
     use_logical_dp_groups = smap_get_bool(&nb->options,
                                           "use_logical_dp_groups", false);
     /* deprecated, use --event instead */
