42ce56175f8cd18014fac3c62048c70de30b9002
Frode Nordahl <frode.nordahl@canonical.com>
2021-11-05 15:00:13 +0100

    controller: Consider plugging VIF on CMS request.
    When OVN is linked with an appropriate VIF plug provider,
    CMS can request an OVN controller to plug individual lports into
    the Open vSwitch instance it manages.
    
    The port and interface record will be maintained throughout the
    lifetime of the lport and it will be removed on release of lport.
    
    Add port by interfaces index - To be able to effectively remove
    ports previously plugged by us we need to look up ports by
    interface records.
    
    Add Port_Binding by requested_chassis index - To be able to
    effectively iterate over ports destined to our chassis we need to
    look up Port_Binding records by requested_chassis.
    
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
    Acked-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: Han Zhou <hzhou@ovn.org>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -106,6 +106,7 @@ static unixctl_cb_func debug_delay_nb_cfg_report;
 #define IF_STATUS_MGR_UPDATE_STOPWATCH_NAME "if-status-mgr-update"
 #define OFCTRL_SEQNO_RUN_STOPWATCH_NAME "ofctrl-seqno-run"
 #define BFD_RUN_STOPWATCH_NAME "bfd-run"
+#define VIF_PLUG_RUN_STOPWATCH_NAME "vif-plug-run"
 
 #define OVS_NB_CFG_NAME "ovn-nb-cfg"
 #define OVS_NB_CFG_TS_NAME "ovn-nb-cfg-ts"
@@ -937,6 +938,7 @@ ctrl_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     binding_register_ovs_idl(ovs_idl);
     bfd_register_ovs_idl(ovs_idl);
     physical_register_ovs_idl(ovs_idl);
+    vif_plug_register_ovs_idl(ovs_idl);
 }
 
 #define SB_NODES \
@@ -3091,6 +3093,10 @@ main(int argc, char *argv[])
         ovsdb_idl_create(ovs_remote, &ovsrec_idl_class, false, true));
     ctrl_register_ovs_idl(ovs_idl_loop.idl);
 
+    struct ovsdb_idl_index *ovsrec_port_by_interfaces
+        = ovsdb_idl_index_create1(ovs_idl_loop.idl,
+                                  &ovsrec_port_col_interfaces);
+
     ovsdb_idl_get_initial_snapshot(ovs_idl_loop.idl);
 
     /* Configure OVN SB database. */
@@ -3126,6 +3132,9 @@ main(int argc, char *argv[])
     struct ovsdb_idl_index *sbrec_port_binding_by_type
         = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
                                   &sbrec_port_binding_col_type);
+    struct ovsdb_idl_index *sbrec_port_binding_by_requested_chassis
+        = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
+                                  &sbrec_port_binding_col_requested_chassis);
     struct ovsdb_idl_index *sbrec_datapath_binding_by_key
         = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
                                   &sbrec_datapath_binding_col_tunnel_key);
@@ -3198,6 +3207,7 @@ main(int argc, char *argv[])
     stopwatch_create(IF_STATUS_MGR_UPDATE_STOPWATCH_NAME, SW_MS);
     stopwatch_create(OFCTRL_SEQNO_RUN_STOPWATCH_NAME, SW_MS);
     stopwatch_create(BFD_RUN_STOPWATCH_NAME, SW_MS);
+    stopwatch_create(VIF_PLUG_RUN_STOPWATCH_NAME, SW_MS);
 
     /* Define inc-proc-engine nodes. */
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA_IS_VALID(ct_zones, "ct_zones");
@@ -3429,6 +3439,11 @@ main(int argc, char *argv[])
     };
     struct if_status_mgr *if_mgr = ctrl_engine_ctx.if_mgr;
 
+    struct shash vif_plug_deleted_iface_ids =
+        SHASH_INITIALIZER(&vif_plug_deleted_iface_ids);
+    struct shash vif_plug_changed_iface_ids =
+        SHASH_INITIALIZER(&vif_plug_changed_iface_ids);
+
     char *ovn_version = ovn_get_internal_version();
     VLOG_INFO("OVN internal version is : [%s]", ovn_version);
 
@@ -3485,6 +3500,7 @@ main(int argc, char *argv[])
             if (!new_ovnsb_cond_seqno) {
                 VLOG_INFO("OVNSB IDL reconnected, force recompute.");
                 engine_set_force_recompute(true);
+                vif_plug_reset_idl_prime_counter();
             }
             ovnsb_cond_seqno = new_ovnsb_cond_seqno;
         }
@@ -3639,6 +3655,37 @@ main(int argc, char *argv[])
                             ovsrec_port_table_get(ovs_idl_loop.idl),
                             br_int, chassis, &runtime_data->local_datapaths);
                         stopwatch_stop(PATCH_RUN_STOPWATCH_NAME, time_msec());
+                        if (vif_plug_provider_has_providers() && ovs_idl_txn) {
+                            struct vif_plug_ctx_in vif_plug_ctx_in = {
+                                .ovs_idl_txn = ovs_idl_txn,
+                                .sbrec_port_binding_by_name =
+                                    sbrec_port_binding_by_name,
+                                .sbrec_port_binding_by_requested_chassis =
+                                    sbrec_port_binding_by_requested_chassis,
+                                .ovsrec_port_by_interfaces =
+                                    ovsrec_port_by_interfaces,
+                                .ovs_table = ovs_table,
+                                .br_int = br_int,
+                                .iface_table =
+                                    ovsrec_interface_table_get(
+                                                    ovs_idl_loop.idl),
+                                .chassis_rec = chassis,
+                                .local_bindings =
+                                        &runtime_data->lbinding_data.bindings,
+                            };
+                            struct vif_plug_ctx_out vif_plug_ctx_out = {
+                                .deleted_iface_ids =
+                                    &vif_plug_deleted_iface_ids,
+                                .changed_iface_ids =
+                                    &vif_plug_changed_iface_ids,
+                            };
+                            stopwatch_start(VIF_PLUG_RUN_STOPWATCH_NAME,
+                                            time_msec());
+                            vif_plug_run(&vif_plug_ctx_in,
+                                         &vif_plug_ctx_out);
+                            stopwatch_stop(VIF_PLUG_RUN_STOPWATCH_NAME,
+                                           time_msec());
+                        }
                         stopwatch_start(PINCTRL_RUN_STOPWATCH_NAME,
                                         time_msec());
                         pinctrl_run(ovnsb_idl_txn,
@@ -3795,7 +3842,16 @@ main(int argc, char *argv[])
             engine_set_force_recompute(true);
         }
 
-        if (ovsdb_idl_loop_commit_and_wait(&ovs_idl_loop) == 1) {
+        int ovs_txn_status = ovsdb_idl_loop_commit_and_wait(&ovs_idl_loop);
+        if (!ovs_txn_status) {
+            /* The transaction failed. */
+            vif_plug_clear_deleted(
+                    &vif_plug_deleted_iface_ids);
+            vif_plug_clear_changed(
+                    &vif_plug_changed_iface_ids);
+        } else if (ovs_txn_status == 1) {
+            /* The transaction committed successfully
+             * (or it did not change anything in the database). */
             ct_zones_data = engine_get_data(&en_ct_zones);
             if (ct_zones_data) {
                 struct shash_node *iter, *iter_next;
@@ -3808,6 +3864,15 @@ main(int argc, char *argv[])
                     }
                 }
             }
+
+            vif_plug_finish_deleted(
+                    &vif_plug_deleted_iface_ids);
+            vif_plug_finish_changed(
+                    &vif_plug_changed_iface_ids);
+        } else if (ovs_txn_status == -1) {
+            /* The commit is still in progress */
+        } else {
+            OVS_NOT_REACHED();
         }
 
         ovsdb_idl_track_clear(ovnsb_idl_loop.idl);
@@ -3883,6 +3948,8 @@ loop_done:
     pinctrl_destroy();
     patch_destroy();
     if_status_mgr_destroy(if_mgr);
+    shash_destroy(&vif_plug_deleted_iface_ids);
+    shash_destroy(&vif_plug_changed_iface_ids);
     vif_plug_provider_destroy_all();
 
     ovsdb_idl_loop_destroy(&ovs_idl_loop);
