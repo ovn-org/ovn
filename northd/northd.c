4833ce610c383aa9a04450f76de148cb8e356a87
liushy <liuxie_11@163.com>
2022-02-26 13:45:13 +0800

    No need snat if packet has been matched ct_dnat
    No need process the snat lflows in lr_out_snat if the
    reply packet has been matched ct_dnat.

--- northd/northd.c
+++ northd/northd.c
@@ -12872,6 +12872,27 @@ build_lrouter_out_snat_flow(struct hmap *lflows, struct ovn_datapath *od,
                                     priority + 1, ds_cstr(match),
                                     ds_cstr(actions), &nat->header_);
         }
+        /* No need process the snat lflows in lr_out_snat if the
+        * packet has been matched ct_dnat. */
+        if (!strcmp(nat->type, "dnat_and_snat")) {
+            priority = cidr_bits + 1;
+
+            /* Only distributed router. */
+            ds_clear(match);
+            ds_put_format(match, "ip && ip%s.src == %s && outport == %s",
+                          is_v6 ? "6" : "4", nat->external_ip,
+                          od->l3dgw_ports[0]->json_key);
+            if (!distributed && od->n_l3dgw_ports) {
+                /* Flows for NAT rules that are centralized are only
+                * programmed on the gateway chassis. */
+                priority += 128;
+                ds_put_format(match, " && is_chassis_resident(%s)",
+                              od->l3dgw_ports[0]->cr_port->json_key);
+            }
+            ovn_lflow_add_with_hint(lflows, od, S_ROUTER_OUT_SNAT,
+                                    priority, ds_cstr(match),
+                                    "next;", &nat->header_);
+        }
     }
 }
 
