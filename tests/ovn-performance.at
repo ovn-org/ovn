ovn-org/ovn

commit 4d3cb42b076bb58fd8f01ab8ad146ffd539f2152

binding: Set Logical_Switch_Port.up when all OVS flows are installed.
Using the ofctrl-seqno generic barrier, register a new type of
notifications for Port_Bindings.  This allows us to delay setting
the Logical_Switch_Port.up field until all OVS flows corresponding
to the logical port and underlying OVS interface have been installed.

This commit also marks the OVS interface as "installed by OVN" by
setting a new "ovn-installed" external-id in the OVS Interface record
when the port is fully wired by OVN.

Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/binding.c
@@ -18,6 +18,7 @@
 #include "ha-chassis.h"
 #include "lflow.h"
 #include "lport.h"
+#include "ofctrl-seqno.h"
 #include "patch.h"
 
 #include "lib/bitmap.h"
@@ -33,6 +34,38 @@
 #include "ovn-controller.h"
 
 VLOG_DEFINE_THIS_MODULE(binding);
+
+/* External ID to be set in the OVS.Interface record when the OVS interface
+ * is ready for use, i.e., is bound to an OVN port and its corresponding
+ * flows have been installed.
+ */
+#define OVN_INSTALLED_EXT_ID "ovn-installed"
+
+/* Set of OVS interface IDs that have been released in the most recent
+ * processing iterations.  This gets updated in release_lport() and is
+ * periodically emptied in binding_seqno_run().
+ */
+static struct sset binding_iface_released_set =
+    SSET_INITIALIZER(&binding_iface_released_set);
+
+/* Set of OVS interface IDs that have been bound in the most recent
+ * processing iterations.  This gets updated in release_lport() and is
+ * periodically emptied in binding_seqno_run().
+ */
+static struct sset binding_iface_bound_set =
+    SSET_INITIALIZER(&binding_iface_bound_set);
+
+static void
+binding_iface_released_add(const char *iface_id)
+{
+    sset_add(&binding_iface_released_set, iface_id);
+}
+
+static void
+binding_iface_bound_add(const char *iface_id)
+{
+    sset_add(&binding_iface_bound_set, iface_id);
+}
 
 #define OVN_QOS_TYPE "linux-htb"
 
@@ -837,6 +870,7 @@ claim_lport(const struct sbrec_port_binding *pb,
             return false;
         }
 
+        binding_iface_bound_add(pb->logical_port);
         if (pb->chassis) {
             VLOG_INFO("Changing chassis for lport %s from %s to %s.",
                     pb->logical_port, pb->chassis->name,
@@ -900,7 +934,9 @@ release_lport(const struct sbrec_port_binding *pb, bool sb_readonly,
         sbrec_port_binding_set_virtual_parent(pb, NULL);
     }
 
+    sbrec_port_binding_set_up(pb, NULL, 0);
     update_lport_tracking(pb, tracked_datapaths);
+    binding_iface_released_add(pb->logical_port);
     VLOG_INFO("Releasing lport %s from this chassis.", pb->logical_port);
     return true;
 }
@@ -2286,4 +2322,149 @@ binding_handle_port_binding_changes(struct binding_ctx_in *b_ctx_in,
 
     destroy_qos_map(&qos_map);
     return handled;
+}
+
+/* Registered ofctrl seqno type for port_binding flow installation. */
+static size_t binding_seq_type_pb_cfg;
+
+/* Binding specific seqno to be acked by ofctrl when flows for new interfaces
+ * have been installed.
+ */
+static uint32_t binding_iface_seqno = 0;
+
+/* Map indexed by iface-id containing the sequence numbers that when acked
+ * indicate that the OVS flows for the iface-id have been installed.
+ */
+static struct simap binding_iface_seqno_map =
+    SIMAP_INITIALIZER(&binding_iface_seqno_map);
+
+void
+binding_init(void)
+{
+    binding_seq_type_pb_cfg = ofctrl_seqno_add_type();
+}
+
+/* Processes new release/bind operations OVN ports.  For newly bound ports
+ * it creates ofctrl seqno update requests that will be acked when
+ * corresponding OVS flows have been installed.
+ *
+ * NOTE: Should be called only when valid SB and OVS transactions are
+ * available.
+ */
+void
+binding_seqno_run(struct shash *local_bindings)
+{
+    const char *iface_id;
+    const char *iface_id_next;
+
+    SSET_FOR_EACH_SAFE (iface_id, iface_id_next, &binding_iface_released_set) {
+        struct shash_node *lb_node = shash_find(local_bindings, iface_id);
+
+        /* If the local binding still exists (i.e., the OVS interface is
+         * still configured locally) then remove the external id and remove
+         * it from the in-flight seqno map.
+         */
+        if (lb_node) {
+            struct local_binding *lb = lb_node->data;
+
+            if (lb->iface && smap_get(&lb->iface->external_ids,
+                                      OVN_INSTALLED_EXT_ID)) {
+                ovsrec_interface_update_external_ids_delkey(
+                    lb->iface, OVN_INSTALLED_EXT_ID);
+            }
+        }
+        simap_find_and_delete(&binding_iface_seqno_map, iface_id);
+        sset_delete(&binding_iface_released_set,
+                    SSET_NODE_FROM_NAME(iface_id));
+    }
+
+    bool new_ifaces = false;
+    uint32_t new_seqno = binding_iface_seqno + 1;
+
+    SSET_FOR_EACH_SAFE (iface_id, iface_id_next, &binding_iface_bound_set) {
+        struct shash_node *lb_node = shash_find(local_bindings, iface_id);
+
+        if (lb_node) {
+            /* This is a newly bound interface, make sure we reset the
+             * Port_Binding 'up' field and the OVS Interface 'external-id'.
+             */
+            struct local_binding *lb = lb_node->data;
+
+            ovs_assert(lb->pb && lb->iface);
+            new_ifaces = true;
+
+            if (smap_get(&lb->iface->external_ids, OVN_INSTALLED_EXT_ID)) {
+                ovsrec_interface_update_external_ids_delkey(
+                    lb->iface, OVN_INSTALLED_EXT_ID);
+            }
+            sbrec_port_binding_set_up(lb->pb, NULL, 0);
+            simap_put(&binding_iface_seqno_map, lb->name, new_seqno);
+        }
+        sset_delete(&binding_iface_bound_set, SSET_NODE_FROM_NAME(iface_id));
+    }
+
+    /* Request a seqno update when the flows for new interfaces have been
+     * installed in OVS.
+     */
+    if (new_ifaces) {
+        binding_iface_seqno = new_seqno;
+        ofctrl_seqno_update_create(binding_seq_type_pb_cfg, new_seqno);
+    }
+}
+
+/* Processes ofctrl seqno ACKs for new bindings.  Sets the
+ * 'OVN_INSTALLED_EXT_ID' external-id in the OVS interface and the
+ * Port_Binding.up field for all ports for which OVS flows have been
+ * installed.
+ *
+ * NOTE: Should be called only when valid SB and OVS transactions are
+ * available.
+ */
+void
+binding_seqno_install(struct shash *local_bindings)
+{
+    struct ofctrl_acked_seqnos *acked_seqnos =
+            ofctrl_acked_seqnos_get(binding_seq_type_pb_cfg);
+    struct simap_node *node;
+    struct simap_node *node_next;
+
+    SIMAP_FOR_EACH_SAFE (node, node_next, &binding_iface_seqno_map) {
+        struct shash_node *lb_node = shash_find(local_bindings, node->name);
+        bool up = true;
+
+        if (!lb_node) {
+            goto del_seqno;
+        }
+
+        struct local_binding *lb = lb_node->data;
+        if (!lb->pb || !lb->iface) {
+            goto del_seqno;
+        }
+
+        if (!ofctrl_acked_seqnos_contains(acked_seqnos, node->data)) {
+            continue;
+        }
+
+        ovsrec_interface_update_external_ids_setkey(lb->iface,
+                                                    OVN_INSTALLED_EXT_ID,
+                                                    "true");
+        sbrec_port_binding_set_up(lb->pb, &up, 1);
+
+        struct shash_node *child_node;
+        SHASH_FOR_EACH (child_node, &lb->children) {
+            struct local_binding *lb_child = child_node->data;
+            sbrec_port_binding_set_up(lb_child->pb, &up, 1);
+        }
+
+del_seqno:
+        simap_delete(&binding_iface_seqno_map, node);
+    }
+
+    ofctrl_acked_seqnos_destroy(acked_seqnos);
+}
+
+void
+binding_seqno_flush(void)
+{
+    simap_clear(&binding_iface_seqno_map);
 }

controller/binding.h
@@ -134,4 +134,9 @@ bool binding_handle_ovs_interface_changes(struct binding_ctx_in *,
 bool binding_handle_port_binding_changes(struct binding_ctx_in *,
                                          struct binding_ctx_out *);
 void binding_tracked_dp_destroy(struct hmap *tracked_datapaths);
+
+void binding_init(void);
+void binding_seqno_run(struct shash *local_bindings);
+void binding_seqno_install(struct shash *local_bindings);
+void binding_seqno_flush(void);
 #endif /* controller/binding.h */

controller/ovn-controller.c
@@ -981,6 +981,7 @@ en_ofctrl_is_connected_run(struct engine_node *node, void *data)
         /* Flush ofctrl seqno requests when the ofctrl connection goes down. */
         if (!of_data->connected) {
             ofctrl_seqno_flush();
+            binding_seqno_flush();
         }
         engine_set_node_state(node, EN_UPDATED);
         return;
@@ -2404,12 +2405,13 @@ main(int argc, char *argv[])
 
     daemonize_complete();
 
+    /* Register ofctrl seqno types. */
+    ofctrl_seq_type_nb_cfg = ofctrl_seqno_add_type();
+
+    binding_init();
     patch_init();
     pinctrl_init();
     lflow_init();
-
-    /* Register ofctrl seqno types. */
-    ofctrl_seq_type_nb_cfg = ofctrl_seqno_add_type();
 
     /* Connect to OVS OVSDB instance. */
     struct ovsdb_idl_loop ovs_idl_loop = OVSDB_IDL_LOOP_INITIALIZER(
@@ -2879,6 +2881,9 @@ main(int argc, char *argv[])
                                                        ovnsb_idl_loop.idl),
                                               ovnsb_cond_seqno,
                                               ovnsb_expected_cond_seqno));
+                    if (runtime_data && ovs_idl_txn && ovnsb_idl_txn) {
+                        binding_seqno_run(&runtime_data->local_bindings);
+                    }
 
                     flow_output_data = engine_get_data(&en_flow_output);
                     if (flow_output_data && ct_zones_data) {
@@ -2889,6 +2894,9 @@ main(int argc, char *argv[])
                                    engine_node_changed(&en_flow_output));
                     }
                     ofctrl_seqno_run(ofctrl_get_cur_cfg());
+                    if (runtime_data && ovs_idl_txn && ovnsb_idl_txn) {
+                        binding_seqno_install(&runtime_data->local_bindings);
+                    }
                 }
 
             }

northd/ovn-northd.c
@@ -12997,7 +12997,7 @@ handle_port_binding_changes(struct northd_context *ctx, struct hmap *ports,
             continue;
         }
 
-        bool up = (sb->chassis || lsp_is_router(op->nbsp));
+        bool up = ((sb->up && (*sb->up)) || lsp_is_router(op->nbsp));
         if (!op->nbsp->up || *op->nbsp->up != up) {
             nbrec_logical_switch_port_set_up(op->nbsp, &up, 1);
         }
@@ -13147,7 +13147,7 @@ static const char *rbac_encap_update[] =
 static const char *rbac_port_binding_auth[] =
     {""};
 static const char *rbac_port_binding_update[] =
-    {"chassis"};
+    {"chassis", "up"};
 
 static const char *rbac_mac_binding_auth[] =
     {""};
@@ -13633,6 +13633,8 @@ main(int argc, char *argv[])
                          &sbrec_port_binding_col_ha_chassis_group);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl,
                          &sbrec_port_binding_col_virtual_parent);
+    ovsdb_idl_add_column(ovnsb_idl_loop.idl,
+                         &sbrec_port_binding_col_up);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl,
                          &sbrec_gateway_chassis_col_chassis);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_gateway_chassis_col_name);

ovn-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_Southbound",
-    "version": "20.13.0",
-    "cksum": "3035725595 25676",
+    "version": "20.14.0",
+    "cksum": "1412040198 25748",
     "tables": {
         "SB_Global": {
             "columns": {
@@ -225,6 +225,7 @@
                 "nat_addresses": {"type": {"key": "string",
                                            "min": 0,
                                            "max": "unlimited"}},
+                "up": {"type": {"key": "boolean", "min": 0, "max": 1}},
                 "external_ids": {"type": {"key": "string",
                                  "value": "string",
                                  "min": 0,

ovn-sb.xml
@@ -2771,6 +2771,14 @@ tcp.flags = RST;
         </p>
       </column>
 
+      <column name="up">
+        <p>
+          This is set to <code>true</code> whenever all OVS flows
+          required by this Port_Binding have been installed.  This is
+          populated by <code>ovn-controller</code>.
+        </p>
+      </column>
+
       <column name="tunnel_key">
         <p>
           A number that represents the logical port in the key (e.g. STT key or

tests/ovn.at
@@ -23560,3 +23560,35 @@ as ovn-nb
 OVS_APP_EXIT_AND_WAIT([ovsdb-server])
 
 AT_CLEANUP
+
+AT_SETUP([ovn -- propagate Port_Binding.up to NB and OVS])
+ovn_start
+
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+
+check ovn-nbctl ls-add ls
+
+AS_BOX([add OVS port for existing LSP])
+check ovn-nbctl lsp-add ls lsp1
+check ovn-nbctl --wait=hv sync
+check_column "[]" Port_Binding up logical_port=lsp1
+
+check ovs-vsctl add-port br-int lsp1 -- set Interface lsp1 external-ids:iface-id=lsp1
+check_column "true" Port_Binding up logical_port=lsp1
+wait_column "true" nb:Logical_Switch_Port up name=lsp1
+OVS_WAIT_UNTIL([test `ovs-vsctl get Interface lsp1 external_ids:ovn-installed` = '"true"'])
+
+AS_BOX([add LSP for existing OVS port])
+check ovs-vsctl add-port br-int lsp2 -- set Interface lsp2 external-ids:iface-id=lsp2
+check ovn-nbctl lsp-add ls lsp2
+check ovn-nbctl --wait=hv sync
+check_column "true" Port_Binding up logical_port=lsp2
+wait_column "true" nb:Logical_Switch_Port up name=lsp2
+OVS_WAIT_UNTIL([test `ovs-vsctl get Interface lsp2 external_ids:ovn-installed` = '"true"'])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP

utilities/ovn-sbctl.c
@@ -526,6 +526,7 @@ pre_get_info(struct ctl_context *ctx)
     ovsdb_idl_add_column(ctx->idl, &sbrec_port_binding_col_tunnel_key);
     ovsdb_idl_add_column(ctx->idl, &sbrec_port_binding_col_chassis);
     ovsdb_idl_add_column(ctx->idl, &sbrec_port_binding_col_datapath);
+    ovsdb_idl_add_column(ctx->idl, &sbrec_port_binding_col_up);
 
     ovsdb_idl_add_column(ctx->idl, &sbrec_logical_flow_col_logical_datapath);
     ovsdb_idl_add_column(ctx->idl, &sbrec_logical_flow_col_logical_dp_group);
@@ -665,6 +666,7 @@ cmd_lsp_bind(struct ctl_context *ctx)
     struct sbctl_chassis *sbctl_ch;
     struct sbctl_port_binding *sbctl_bd;
     char *lport_name, *ch_name;
+    bool up = true;
 
     /* port_binding must exist, chassis must exist! */
     lport_name = ctx->argv[1];
@@ -683,6 +685,7 @@ cmd_lsp_bind(struct ctl_context *ctx)
         }
     }
     sbrec_port_binding_set_chassis(sbctl_bd->bd_cfg, sbctl_ch->ch_cfg);
+    sbrec_port_binding_set_up(sbctl_bd->bd_cfg, &up, 1);
     sbctl_context_invalidate_cache(ctx);
 }
 
@@ -699,6 +702,7 @@ cmd_lsp_unbind(struct ctl_context *ctx)
     sbctl_bd = find_port_binding(sbctl_ctx, lport_name, must_exist);
     if (sbctl_bd) {
         sbrec_port_binding_set_chassis(sbctl_bd->bd_cfg, NULL);
+        sbrec_port_binding_set_up(sbctl_bd->bd_cfg, NULL, 0);
     }
 }
 

commit c182ebdc6d07bd269a5587c83796fdb93a2404b3

ovn-controller: Avoid recompute triggered by external_ids:ovn-installed update.
During the process of claiming a VIF port on a chassis, there are
multiple SB and local OVSDB updates by ovn-controller, which may trigger
ovn-controller recompute, depending on the timing between the completion
of SB and local OVSDB updates:

After port claiming, ovn-controller sends an update to the local OVSDB
to clean the Interface:external_ids:ovn-installed, and also sends an
update to the SB DB to update the Port_Binding:chassis.  If the local
OVSDB update notification comes back before the SB update completes,
ovn-controller's SB IDL is still "in transaction" and read-only, but the
local Interface update triggers the I-P engine runtime_data node to
handle the OVS_interface change, which considers the VIF to be claimed
(again) potentially, which requires SB updates while it is not possible
at the moment, and so the I-P handler returns failure and the engine
aborts, which triggers recompute at the next I-P engine iteration when
SB DB is writable.

If we are lucky at the above step that the SB update completes before
the local OVSDB update is received, ovn-controller would handle the
change incrementally (and figures out that the SB Port_Binding chassis
is already the current one thus wouldn't reclaim the VIF). But the next
step after it completes flow computing for the newly bound VIF it sends
another update to the local OVSDB to set the
Interface:external_ids:ovn-installed and ovn-installed-ts, while at the
same time it also sends an update to the SB DB to set the
Port_Binding:up. These two parallel updates would lead to the same
situation as the above step and potentially cause ovn-controll
recompute.

The recompute is very likely to be triggered in production because the
SB DB is remote (on central nodes) and has heavier load, which requires
longer time to complete the updates than the local OVSDB server.

The problem is that the runtime_data's OVS_interface change handler
couldn't handle the update of the Interface:external_ids:ovn-installed
incrementally, because ovn-controller assumes it could require an SB
update that is not possible at the moment. However, the assumption is
not true. In fact it shouldn't even care about the change because this
external_id:ovn-installed is written by ovn-controller itself and it is
write-only (no need to read it as an input for any further processing).

So, the idea to fix the problem is to ignore such changes that happen to
the "write-only" fields only. OVSDB change tracking provides APIs to
check if a specific column is updated, but the difficulty here is that
the column external_ids is a k-v map which contains not only the data
that ovn-controller can ignore, such as ovn-installed, ovn-installed-ts,
but also the data ovn-controller cares about, such as iface-id,
iface-id-ver and encap-ip. To solve this problem, we need to know what's
changed exactly inside the external_ids map.

This patch solves it by introducing a new I-P engine node
ovs_interface_shadow to wrap the OVSDB input ovs_interface with an extra
copy of the older version of the column external_ids, and replace the
input OVS_interface of the runtime_data node. In the change handler of the
runtime_data node we evaluates if the change needs to be handled before
calling consider_iface_claim(), so in the above scenarios the changes
don't need to be handled and won't trigger recompute any more. A test
case is also updated to capture this (which is very likely, if not 100%,
to fail without the fix).

Fixes: 4d3cb42b076b ("binding: Set Logical_Switch_Port.up when all OVS flows are installed.")

controller/binding.c
@@ -1915,6 +1915,71 @@ is_iface_in_int_bridge(const struct ovsrec_interface *iface,
     return false;
 }
 
+static bool
+is_ext_id_changed(const struct smap *a, const struct smap *b, const char *key)
+{
+    const char *value_a = smap_get(a, key);
+    const char *value_b = smap_get(b, key);
+    if ((value_a && !value_b)
+        || (!value_a && value_b)
+        || (value_a && value_b && strcmp(value_a, value_b))) {
+        return true;
+    }
+    return false;
+}
+
+/* Check if the change in 'iface_rec' is something we are interested in from
+ * port binding perspective.  Return true if the change needs to be handled,
+ * otherwise return false.
+ *
+ * The 'iface_rec' must be change tracked, i.e. iterator from
+ * OVSREC_INTERFACE_TABLE_FOR_EACH_TRACKED. */
+static bool
+ovs_interface_change_need_handle(const struct ovsrec_interface *iface_rec,
+                                 struct shash *iface_table_external_ids_old)
+{
+    if (ovsrec_interface_is_updated(iface_rec,
+                                    OVSREC_INTERFACE_COL_NAME)) {
+        return true;
+    }
+    if (ovsrec_interface_is_updated(iface_rec,
+                                    OVSREC_INTERFACE_COL_OFPORT)) {
+        return true;
+    }
+    if (ovsrec_interface_is_updated(iface_rec,
+                                    OVSREC_INTERFACE_COL_TYPE)) {
+        return true;
+    }
+    if (ovsrec_interface_is_updated(iface_rec,
+                                    OVSREC_INTERFACE_COL_EXTERNAL_IDS)) {
+        /* Compare the external_ids that we are interested in with the old
+         * values:
+         * - iface-id
+         * - iface-id-ver
+         * - encap-ip
+         * For any other changes, such as ovn-installed, ovn-installed-ts, etc,
+         * we don't need to handle. */
+        struct smap *external_ids_old =
+            shash_find_data(iface_table_external_ids_old, iface_rec->name);
+        if (!external_ids_old) {
+            return true;
+        }
+        if (is_ext_id_changed(&iface_rec->external_ids, external_ids_old,
+                              "iface-id")) {
+            return true;
+        }
+        if (is_ext_id_changed(&iface_rec->external_ids, external_ids_old,
+                              "iface-id-ver")) {
+            return true;
+        }
+        if (is_ext_id_changed(&iface_rec->external_ids, external_ids_old,
+                              "encap-ip")) {
+            return true;
+        }
+    }
+    return false;
+}
+
 /* Returns true if the ovs interface changes were handled successfully,
  * false otherwise.
  */
@@ -2023,6 +2088,11 @@ binding_handle_ovs_interface_changes(struct binding_ctx_in *b_ctx_in,
                                              b_ctx_in->iface_table) {
         /* Loop to handle create and update changes only. */
         if (ovsrec_interface_is_deleted(iface_rec)) {
+            continue;
+        }
+
+        if (!ovs_interface_change_need_handle(
+            iface_rec, b_ctx_in->iface_table_external_ids_old)) {
             continue;
         }
 

controller/binding.h
@@ -55,6 +55,7 @@ struct binding_ctx_in {
     const struct ovsrec_bridge_table *bridge_table;
     const struct ovsrec_open_vswitch_table *ovs_table;
     const struct ovsrec_interface_table *iface_table;
+    struct shash *iface_table_external_ids_old;
 };
 
 /* Locally relevant port bindings, e.g., VIFs that might be bound locally,

controller/ovn-controller.c
@@ -1042,6 +1042,91 @@ en_ofctrl_is_connected_run(struct engine_node *node, void *data)
     engine_set_node_state(node, EN_UNCHANGED);
 }
 
+/* This engine node is to wrap the OVS_interface input and maintain a copy of
+ * the old version of data for the column external_ids.
+ *
+ * There are some special considerations of this engine node:
+ * 1. It has a single input OVS_interface, and it transparently passes the
+ *    input changes as its own output data to its dependants. So there is no
+ *    processing to OVS_interface changes but simply mark the node status as
+ *    UPDATED (and so the run() and the change handler is the same).
+ * 2. The iface_table_external_ids_old is computed/updated in the member
+ *    clear_tracked_data(), because that is when the last round of processing
+ *    has completed but the new IDL data is yet to refresh, so we replace the
+ *    old data with the current data. */
+struct ed_type_ovs_interface_shadow {
+    struct ovsrec_interface_table *iface_table;
+    struct shash iface_table_external_ids_old;
+};
+
+static void *
+en_ovs_interface_shadow_init(struct engine_node *node OVS_UNUSED,
+                             struct engine_arg *arg OVS_UNUSED)
+{
+    struct ed_type_ovs_interface_shadow *data = xzalloc(sizeof *data);
+    data->iface_table = NULL;
+    shash_init(&data->iface_table_external_ids_old);
+
+    return data;
+}
+
+static void
+iface_table_external_ids_old_destroy(struct shash *table_ext_ids)
+{
+    struct shash_node *node;
+    SHASH_FOR_EACH (node, table_ext_ids) {
+        struct smap *ext_ids = node->data;
+        smap_destroy(ext_ids);
+    }
+    shash_destroy_free_data(table_ext_ids);
+}
+
+static void
+en_ovs_interface_shadow_cleanup(void *data_)
+{
+    struct ed_type_ovs_interface_shadow *data = data_;
+    iface_table_external_ids_old_destroy(&data->iface_table_external_ids_old);
+}
+
+static void
+en_ovs_interface_shadow_clear_tracked_data(void *data_)
+{
+    struct ed_type_ovs_interface_shadow *data = data_;
+    iface_table_external_ids_old_destroy(&data->iface_table_external_ids_old);
+    shash_init(&data->iface_table_external_ids_old);
+
+    if (!data->iface_table) {
+        return;
+    }
+
+    const struct ovsrec_interface *iface_rec;
+    OVSREC_INTERFACE_TABLE_FOR_EACH (iface_rec, data->iface_table) {
+        struct smap *external_ids = xmalloc(sizeof *external_ids);
+        smap_clone(external_ids, &iface_rec->external_ids);
+        shash_add(&data->iface_table_external_ids_old, iface_rec->name,
+                  external_ids);
+    }
+}
+
+static void
+en_ovs_interface_shadow_run(struct engine_node *node, void *data_)
+{
+    struct ed_type_ovs_interface_shadow *data = data_;
+    struct ovsrec_interface_table *iface_table =
+        (struct ovsrec_interface_table *)EN_OVSDB_GET(
+            engine_get_input("OVS_interface", node));
+    data->iface_table = iface_table;
+    engine_set_node_state(node, EN_UPDATED);
+}
+
+static bool
+ovs_interface_shadow_ovs_interface_handler(struct engine_node *node,
+                                           void *data_)
+{
+    en_ovs_interface_shadow_run(node, data_);
+    return true;
+}
+
 struct ed_type_runtime_data {
     /* Contains "struct local_datapath" nodes. */
     struct hmap local_datapaths;
@@ -1214,9 +1299,8 @@ init_binding_ctx(struct engine_node *node,
         (struct ovsrec_port_table *)EN_OVSDB_GET(
             engine_get_input("OVS_port", node));
 
-    struct ovsrec_interface_table *iface_table =
-        (struct ovsrec_interface_table *)EN_OVSDB_GET(
-            engine_get_input("OVS_interface", node));
+    struct ed_type_ovs_interface_shadow *iface_shadow =
+        engine_get_input_data("ovs_interface_shadow", node);
 
     struct ovsrec_qos_table *qos_table =
         (struct ovsrec_qos_table *)EN_OVSDB_GET(
@@ -1249,7 +1333,9 @@ init_binding_ctx(struct engine_node *node,
     b_ctx_in->sbrec_port_binding_by_datapath = sbrec_port_binding_by_datapath;
     b_ctx_in->sbrec_port_binding_by_name = sbrec_port_binding_by_name;
     b_ctx_in->port_table = port_table;
-    b_ctx_in->iface_table = iface_table;
+    b_ctx_in->iface_table = iface_shadow->iface_table;
+    b_ctx_in->iface_table_external_ids_old =
+        &iface_shadow->iface_table_external_ids_old;
     b_ctx_in->qos_table = qos_table;
     b_ctx_in->port_binding_table = pb_table;
     b_ctx_in->br_int = br_int;
@@ -1331,7 +1417,7 @@ en_runtime_data_run(struct engine_node *node, void *data)
 }
 
 static bool
-runtime_data_ovs_interface_handler(struct engine_node *node, void *data)
+runtime_data_ovs_interface_shadow_handler(struct engine_node *node, void *data)
 {
     struct ed_type_runtime_data *rt_data = data;
     struct binding_ctx_in b_ctx_in;
@@ -3357,6 +3443,8 @@ main(int argc, char *argv[])
 
     /* Define inc-proc-engine nodes. */
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA_IS_VALID(ct_zones, "ct_zones");
+    ENGINE_NODE_WITH_CLEAR_TRACK_DATA(ovs_interface_shadow,
+                                      "ovs_interface_shadow");
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA(runtime_data, "runtime_data");
     ENGINE_NODE(non_vif_data, "non_vif_data");
     ENGINE_NODE(mff_ovn_geneve, "mff_ovn_geneve");
@@ -3478,6 +3566,9 @@ main(int argc, char *argv[])
     engine_add_input(&en_ct_zones, &en_runtime_data,
                      ct_zones_runtime_data_handler);
 
+    engine_add_input(&en_ovs_interface_shadow, &en_ovs_interface,
+                     ovs_interface_shadow_ovs_interface_handler);
+
     engine_add_input(&en_runtime_data, &en_ofctrl_is_connected, NULL);
 
     engine_add_input(&en_runtime_data, &en_ovs_open_vswitch, NULL);
@@ -3499,8 +3590,8 @@ main(int argc, char *argv[])
      */
     engine_add_input(&en_runtime_data, &en_ovs_port,
                      engine_noop_handler);
-    engine_add_input(&en_runtime_data, &en_ovs_interface,
-                     runtime_data_ovs_interface_handler);
+    engine_add_input(&en_runtime_data, &en_ovs_interface_shadow,
+                     runtime_data_ovs_interface_shadow_handler);
 
     engine_add_input(&en_flow_output, &en_lflow_output,
                      flow_output_lflow_output_handler);

tests/ovn-performance.at
@@ -366,6 +366,23 @@ for i in 1 2; do
          ovn-nbctl wait-until Logical_Switch_Port $lp 'up=true' &&
          ovn-nbctl --wait=hv sync]
     )
+
+    # Delete and recreate $lp to make it unbind and rebind multiple times, and
+    # ensure no recompute is triggered.
+    for k in $(seq 10); do
+        OVN_CONTROLLER_EXPECT_NO_HIT(
+            [hv$i hv$j], [lflow_run],
+            [as hv$i ovs-vsctl set Interface $vif external-ids:iface-id=xxxx &&
+             ovn-nbctl wait-until Logical_Switch_Port $lp 'up=false' &&
+             ovn-nbctl --wait=hv sync]
+        )
+        OVN_CONTROLLER_EXPECT_NO_HIT(
+            [hv$i hv$j], [lflow_run],
+            [as hv$i ovs-vsctl set Interface $vif external-ids:iface-id=$lp &&
+             ovn-nbctl wait-until Logical_Switch_Port $lp 'up=true' &&
+             ovn-nbctl --wait=hv sync]
+        )
+    done
 done
 
 for i in 1 2; do