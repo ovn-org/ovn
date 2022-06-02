c53f339342dbc3f79a34787f153f81989bac9909
Dumitru Ceara <dceara@redhat.com>
2022-06-02 12:22:27 +0200

    northd: Use ct_mark.blocked and ecmp_reply_port only when all chassis support it.
    Commit a075230e4a0f ("Use ct_mark for masked access to make flows
    HW-offloading friendly.") started using the ct_mark.blocked and
    ct_mark.ecmp_reply_port ct_label.  In usual scenarios this new feature would
    be picked up when the next stable release becomes available (i.e., 22.06.0).
    However, the commit was also backported to stable branches (branch-22.03 and
    branch-21.12).
    
    While the supported upgrade scenario for OVN when moving to a new
    stable release is to ensure that ovn-controllers are upgraded first,
    it's not really clear that this restriction applies to "z-stream"
    upgrades too (e.g., from v21.12.1 to v21.12.2).
    
    Some CMSs, like RHV (Red Hat Virtualization), expect ovn-controllers
    running older code from a stable branch to be able to interpret all
    Southbound contents generated by ovn-northd instances built from the
    same or newer versions of the stable branch.
    
    Ensure that ct_mark.blocked and ecmp_reply_port are used only when all
    chassis registered in the Southbound support it.
    
    Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2091565
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- controller/chassis.c
+++ controller/chassis.c
@@ -350,7 +350,7 @@ chassis_build_other_config(const struct ovs_chassis_cfg *ovs_cfg,
     smap_replace(config, "is-interconn",
                  ovs_cfg->is_interconn ? "true" : "false");
     smap_replace(config, OVN_FEATURE_PORT_UP_NOTIF, "true");
-    smap_replace(config, OVN_FEATURE_CT_LB_MARK, "true");
+    smap_replace(config, OVN_FEATURE_CT_NO_MASKED_LABEL, "true");
 }
 
 /*
@@ -456,7 +456,8 @@ chassis_other_config_changed(const struct ovs_chassis_cfg *ovs_cfg,
         return true;
     }
 
-    if (!smap_get_bool(&chassis_rec->other_config, OVN_FEATURE_CT_LB_MARK,
+    if (!smap_get_bool(&chassis_rec->other_config,
+                       OVN_FEATURE_CT_NO_MASKED_LABEL,
                        false)) {
         return true;
     }
