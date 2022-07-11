521c671faf147ea56d21ed45eb85c68820d5e139
Igor Zhukov <ivzhukov@sbercloud.ru>
2022-07-11 18:06:55 +0700

    Build tests with asan and ubsan together to reduce CI time.
    Also fix typo in comments
    
    Also fix memory leak in utilities/ovn-dbctl.c
    
    Also add "-g" to CFLAGS to forbid autotools from adding "-g -O2" (temporary solution)
    
    Signed-off-by: Igor Zhukov <ivzhukov@sbercloud.ru>
    Acked-by: Dumitru Ceara <dceara@redhat.com>
    Submitted-at: https://github.com/ovn-org/ovn/pull/139
    Acked-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>
    (cherry picked from commit 5ea3c71f1e47d39936fe91267cbb206c15245a18)
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -28,20 +28,14 @@ function configure_ovn()
 save_OPTS="${OPTS} $*"
 OPTS="${EXTRA_OPTS} ${save_OPTS}"
 
-# If AddressSanitizer or UdefinedBehaviorSanitizer is requested, enable it,
+# If AddressSanitizer and UndefinedBehaviorSanitizer are requested, enable them,
 # but only for OVN, not for OVS.  However, disable some optimizations for
 # OVS, to make sanitizer reports user friendly.
-if [ "$ASAN" ]; then
-    CFLAGS="-O1 -fno-omit-frame-pointer -fno-common"
-    CFLAGS_ASAN="-fsanitize=address"
-    OVN_CFLAGS="${OVN_CFLAGS} ${CFLAGS_ASAN}"
-fi
-
-if [ "$UBSAN" ]; then
+if [ "$SANITIZERS" ]; then
     # Use the default options configured in tests/atlocal.in, in UBSAN_OPTIONS.
-    CFLAGS="-O1 -fno-omit-frame-pointer -fno-common"
-    CFLAGS_UBSAN="-fsanitize=undefined"
-    OVN_CFLAGS="${OVN_CFLAGS} ${CFLAGS_UBSAN}"
+    CFLAGS="-O1 -fno-omit-frame-pointer -fno-common -g"
+    CFLAGS_SANITIZERS="-fsanitize=address,undefined"
+    OVN_CFLAGS="${OVN_CFLAGS} ${CFLAGS_SANITIZERS}"
 fi
 
 if [ "$CC" = "clang" ]; then
