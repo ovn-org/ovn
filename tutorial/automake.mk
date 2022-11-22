4eefb635920308a79a9f88e139a279cff50ca407
Dumitru Ceara <dceara@redhat.com>
2022-11-22 15:15:11 +0100

    tutorial: Add scripts to simulate node-port ovn-k8s services.
    In a sandbox run:
    
    $ ./ovn-lb-benchmark.sh <N> <VIPS> <BACKENDS> <USE_TEMPLATES>
    
    to simulate an ovn-k8s-like topology with N nodes, VIPS NodePort services
    applied to all nodes.  Each service has BACKENDS backends.
    
    If USE_TEMPLATES is "yes" then the configuration will be optimized to use
    Chassis_Template_Vars.  Otherwise it will create N LBs per service, one
    for every node.
    
    Acked-by: Han Zhou <hzhou@ovn.org>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- tutorial/automake.mk
+++ tutorial/automake.mk
@@ -1,6 +1,8 @@
 EXTRA_DIST += \
 	tutorial/ovs-sandbox \
-	tutorial/ovn-setup.sh
+	tutorial/ovn-setup.sh \
+	tutorial/ovn-lb-benchmark.sh \
+	tutorial/ovn-lb-benchmark.py
 sandbox: all
 	cd $(srcdir)/tutorial && MAKE=$(MAKE) HAVE_OPENSSL=$(HAVE_OPENSSL) \
 		./ovs-sandbox -b $(abs_builddir) --ovs-src $(ovs_srcdir) --ovs-build $(ovs_builddir) $(SANDBOXFLAGS)
