ovn-org/ovn

commit f418dce4158b45cfb47e8e87c1df3837d21b4923

build-aux/sodepends.py: Fix broken build when manpage changes.
Commit [0] updated ovn-detrace.1.in, which triggered an existing problem
of the build system. After this change, for any existed build
environments that have created a separate build dir (instead of directly
build under the root dir of the source code), the build is broken, and
complains:

utilities/ovn-detrace.1.in not found in: . srcdir OVS_MANDIR .
make: *** [Makefile:3482: ../manpages.mk] Error 1

The root cause is that an earlier commit b6a75011 had a bug and the
sodepends script didn't read the new -I input properly.

[0] 25b4d763bd4b ("ovn-detrace: Support connecting to NB and SB raft followers")

Fixes: b6a750112324 ("manpages.mk: fix dependencies path")
Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Mark Michelson <mmichels@redhat.com>

build-aux/sodepends.py
@@ -63,7 +63,7 @@ def sodepends(include_info, filenames, dst):
             continue
 
         # Open file.
-        include_dirs = [info[0] for info in include_info]
+        include_dirs = [info[1] if len(info) == 2 else info[0] for info in include_info]
         fn = soutil.find_file(include_dirs, toplevel)
         if not fn:
             ok = False

commit 52da2419770be66ecad4f5d472e4519f4a8ec1ed

build-aux/sodepends.py: Fix flake8 error.
Fixes: f418dce4158b ("build-aux/sodepends.py: Fix broken build when manpage changes.")
Signed-off-by: Han Zhou <hzhou@ovn.org>

build-aux/sodepends.py
@@ -63,7 +63,8 @@ def sodepends(include_info, filenames, dst):
             continue
 
         # Open file.
-        include_dirs = [info[1] if len(info) == 2 else info[0] for info in include_info]
+        include_dirs = [info[1] if len(info) == 2 else info[0]
+                        for info in include_info]
         fn = soutil.find_file(include_dirs, toplevel)
         if not fn:
             ok = False