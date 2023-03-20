1ad77917db2c290c5982c04033385a1c5ba539e1
Ales Musil <amusil@redhat.com>
2023-03-20 11:53:13 +0100

    controller: Add config option per LB to enable/disable CT flush
    The CT flush was enabled by default for every LB, add
    config option called "ct_flush" that allows
    users to enable/disable the CT flush. The CT flush option
    is set to "false" by default.
    
    Reported-at: https://bugzilla.redhat.com/2178962
    Acked-by: Numan Siddique <numans@ovn.org>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -2697,7 +2697,8 @@ static void
 lb_data_removed_five_tuples_add(struct ed_type_lb_data *lb_data,
                                 const struct ovn_controller_lb *lb)
 {
-    if (!ovs_feature_is_supported(OVS_CT_TUPLE_FLUSH_SUPPORT)) {
+    if (!ovs_feature_is_supported(OVS_CT_TUPLE_FLUSH_SUPPORT) ||
+        !lb->ct_flush) {
         return;
     }
 
@@ -2716,7 +2717,8 @@ static void
 lb_data_removed_five_tuples_remove(struct ed_type_lb_data *lb_data,
                                    const struct ovn_controller_lb *lb)
 {
-    if (!ovs_feature_is_supported(OVS_CT_TUPLE_FLUSH_SUPPORT)) {
+    if (!ovs_feature_is_supported(OVS_CT_TUPLE_FLUSH_SUPPORT) ||
+        !lb->ct_flush) {
         return;
     }
 
