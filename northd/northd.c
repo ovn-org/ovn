f38c8e386b016e3543703c1a1b72b4385039966e
Mark Michelson <mmichels@redhat.com>
2023-04-13 15:21:34 -0400

    Pass localnet traffic through CT when a LB is configured.
    Current code always skips conntrack for traffic that ingresses or
    egresses on a localnet port. However, this makes it impossible for
    traffic to be load-balanced when it arrives on a localnet port.
    
    This patch allows for traffic to be load balanced on localnet ports by
    making two changes:
    * Localnet ports now have a conntrack zone assigned.
    * When a load balancer is configured on a logical switch containing a
      localnet port, then conntrack is no longer skipped for traffic
      involving the localnet port.
    
    Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2164652
    
    Co-authored by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Mark Michelson <mmichels@redhat.com>
    Acked-by: Ales Musil <amusil@redhat.com>

--- northd/northd.c
+++ northd/northd.c
@@ -5770,7 +5770,8 @@ build_pre_acls(struct ovn_datapath *od, const struct hmap *port_groups,
         }
         for (size_t i = 0; i < od->n_localnet_ports; i++) {
             skip_port_from_conntrack(od, od->localnet_ports[i],
-                                     S_SWITCH_IN_PRE_ACL, S_SWITCH_OUT_PRE_ACL,
+                                     S_SWITCH_IN_PRE_ACL,
+                                     S_SWITCH_OUT_PRE_ACL,
                                      110, lflows);
         }
 
@@ -5935,10 +5936,17 @@ build_pre_lb(struct ovn_datapath *od, const struct shash *meter_groups,
                                  S_SWITCH_IN_PRE_LB, S_SWITCH_OUT_PRE_LB,
                                  110, lflows);
     }
-    for (size_t i = 0; i < od->n_localnet_ports; i++) {
-        skip_port_from_conntrack(od, od->localnet_ports[i],
-                                 S_SWITCH_IN_PRE_LB, S_SWITCH_OUT_PRE_LB,
-                                 110, lflows);
+    /* Localnet ports have no need for going through conntrack, unless
+     * the logical switch has a load balancer. Then, conntrack is necessary
+     * so that traffic arriving via the localnet port can be load
+     * balanced.
+     */
+    if (!od->has_lb_vip) {
+        for (size_t i = 0; i < od->n_localnet_ports; i++) {
+            skip_port_from_conntrack(od, od->localnet_ports[i],
+                                     S_SWITCH_IN_PRE_LB, S_SWITCH_OUT_PRE_LB,
+                                     110, lflows);
+        }
     }
 
     /* 'REGBIT_CONNTRACK_NAT' is set to let the pre-stateful table send
