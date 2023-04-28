7b8bc00ce4aa2bfbec3e14c58f899debb4b50513
Xavier Simonart <xsimonar@redhat.com>
2023-04-28 13:59:29 +0200

    ovn-controller: fixed port not always set down when unbinding interface
    When interface was unbound, the port was not always set down and the
    port_binding->chassis not always removed.
    
    Fixes: a7c7d4519e50 ("controller: avoid recomputes triggered by SBDB Port_Binding updates.")
    Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2150905
    
    Signed-off-by: Xavier Simonart <xsimonar@redhat.com>
    Reviewed-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -5227,6 +5227,8 @@ main(int argc, char *argv[])
                     if_status_mgr_update(if_mgr, binding_data, chassis,
                                          ovsrec_interface_table_get(
                                                     ovs_idl_loop.idl),
+                                         sbrec_port_binding_table_get(
+                                                    ovnsb_idl_loop.idl),
                                          !ovs_idl_txn,
                                          !ovnsb_idl_txn);
                     stopwatch_stop(IF_STATUS_MGR_UPDATE_STOPWATCH_NAME,
