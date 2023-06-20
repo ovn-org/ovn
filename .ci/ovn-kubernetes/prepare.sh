44e8192434da2edc13774d9ccd157d0f2bcdc795
Dumitru Ceara <dceara@redhat.com>
2023-06-20 15:52:31 +0200

    ci: ovn-kubernetes: Figure out dependencies dynamically.
    This avoids manual intervention when upstream ovn-kubernetes changes
    its dependency versions.
    
    Signed-off-by: Patryk Diak <pdiak@redhat.com>
    Co-authored-by: Patryk Diak <pdiak@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Acked-by: Aaron Conole <aconole@redhat.com>
    (cherry picked from commit 1f36e8bebd1f617346df87bcfeb39fae44403607)

--- .ci/ovn-kubernetes/prepare.sh
+++ .ci/ovn-kubernetes/prepare.sh
@@ -3,12 +3,23 @@
 set -ev
 
 ovnk8s_path=$1
+env_path=$2
 topdir=$PWD
+
+function extract_ci_var() {
+    local name=$1
+
+    grep "$name:" .github/workflows/test.yml | awk '{print $2}' | tr -d '"'
+}
 
 pushd ${ovnk8s_path}
 
 # Add here any custom operations that need to performed on the
 # ovn-kubernetes cloned repo, e.g., custom patches.
+
+# Set up the right GO_VERSION and K8S_VERSION.
+echo "GO_VERSION=$(extract_ci_var GO_VERSION)" >> $env_path
+echo "K8S_VERSION=$(extract_ci_var K8S_VERSION)" >> $env_path
 
 # git apply --allow-empty is too new so not all git versions from major
 # distros support it, just check if the custom patch file is not empty
