ovn-org/ovn

commit c93c626248c120eeaffafd323aef323d3b2507ab

controller: Implement a generic barrier based on ofctrl cur_cfg sync.
A new module, 'ofctrl-seqno', is added to implement this generic
barrier.  Other modules can register their own types of seqno update
requests.  The barrier implementation ensures that the a seqno update
request is acked (returned by ofctrl_acked_seqnos_get()) only if the
OVS flow operations that have been requested when the seqno update
request was queued have been processed by OVS.

For now, the only user of this barrier is the main ovn-controller
module but a future commit will use it too in order to mark
Port_Bindings and OVS interfaces as "fully installed".

This commit also adds unit tests for the new 'ofctrl-seqno' module.
The unit test structure is inspired by Mark Michelson's patch:
http://patchwork.ozlabs.org/project/ovn/patch/20201216182421.234772-3-mmichels@redhat.com/

Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/ofctrl-seqno.c
@@ -1,0 +1,254 @@
+/* Copyright (c) 2021, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "hash.h"
+#include "ofctrl-seqno.h"
+#include "openvswitch/list.h"
+#include "util.h"
+
+/* A sequence number update request, i.e., when the barrier corresponding to
+ * the 'flow_cfg' sequence number is replied to by OVS then it is safe
+ * to inform the application that the 'req_cfg' seqno has been processed.
+ */
+struct ofctrl_seqno_update {
+    struct ovs_list list_node; /* In 'ofctrl_seqno_updates'. */
+    size_t seqno_type;         /* Application specific seqno type.
+                                * Relevant only for 'req_cfg'.
+                                */
+    uint64_t flow_cfg;         /* The seqno that needs to be acked by OVS
+                                * before 'req_cfg' can be acked for the
+                                * application.
+                                */
+    uint64_t req_cfg;          /* Application specific seqno. */
+};
+
+/* List of in flight sequence number updates. */
+static struct ovs_list ofctrl_seqno_updates;
+
+/* Last sequence number request sent to OVS. */
+static uint64_t ofctrl_req_seqno;
+
+/* State of seqno requests for a given application seqno type. */
+struct ofctrl_seqno_state {
+    struct ovs_list acked_cfgs; /* Acked requests since the last time the
+                                 * application consumed acked requests.
+                                 */
+    uint64_t cur_cfg;           /* Last acked application seqno. */
+    uint64_t req_cfg;           /* Last requested application seqno. */
+};
+
+/* Per application seqno type states. */
+static size_t n_ofctrl_seqno_states;
+static struct ofctrl_seqno_state *ofctrl_seqno_states;
+
+/* ofctrl_acked_seqnos related static function prototypes. */
+static void ofctrl_acked_seqnos_init(struct ofctrl_acked_seqnos *seqnos,
+                                     uint64_t last_acked);
+static void ofctrl_acked_seqnos_add(struct ofctrl_acked_seqnos *seqnos,
+                                    uint32_t val);
+
+/* ofctrl_seqno_update related static function prototypes. */
+static void ofctrl_seqno_update_create__(size_t seqno_type, uint64_t req_cfg);
+static void ofctrl_seqno_update_list_destroy(struct ovs_list *seqno_list);
+static void ofctrl_seqno_cfg_run(size_t seqno_type,
+                                 struct ofctrl_seqno_update *update);
+
+/* Returns the collection of acked ofctrl_seqno_update requests of type
+ * 'seqno_type'.  It's the responsibility of the caller to free memory by
+ * calling ofctrl_acked_seqnos_destroy().
+ */
+struct ofctrl_acked_seqnos *
+ofctrl_acked_seqnos_get(size_t seqno_type)
+{
+    struct ofctrl_acked_seqnos *acked_seqnos = xmalloc(sizeof *acked_seqnos);
+    struct ofctrl_seqno_state *state = &ofctrl_seqno_states[seqno_type];
+    struct ofctrl_seqno_update *update;
+
+    ofctrl_acked_seqnos_init(acked_seqnos, state->cur_cfg);
+
+    ovs_assert(seqno_type < n_ofctrl_seqno_states);
+    LIST_FOR_EACH_POP (update, list_node, &state->acked_cfgs) {
+        ofctrl_acked_seqnos_add(acked_seqnos, update->req_cfg);
+        free(update);
+    }
+    return acked_seqnos;
+}
+
+void
+ofctrl_acked_seqnos_destroy(struct ofctrl_acked_seqnos *seqnos)
+{
+    if (!seqnos) {
+        return;
+    }
+
+    struct ofctrl_ack_seqno *seqno_node;
+    HMAP_FOR_EACH_POP (seqno_node, node, &seqnos->acked) {
+        free(seqno_node);
+    }
+    hmap_destroy(&seqnos->acked);
+    free(seqnos);
+}
+
+/* Returns true if 'val' is one of the acked sequence numbers in 'seqnos'. */
+bool
+ofctrl_acked_seqnos_contains(const struct ofctrl_acked_seqnos *seqnos,
+                             uint32_t val)
+{
+    struct ofctrl_ack_seqno *sn;
+
+    HMAP_FOR_EACH_WITH_HASH (sn, node, hash_int(val, 0), &seqnos->acked) {
+        if (sn->seqno == val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void
+ofctrl_seqno_init(void)
+{
+    ovs_list_init(&ofctrl_seqno_updates);
+}
+
+/* Adds a new type of application specific seqno updates. */
+size_t
+ofctrl_seqno_add_type(void)
+{
+    size_t new_type = n_ofctrl_seqno_states;
+    n_ofctrl_seqno_states++;
+
+    struct ofctrl_seqno_state *new_states =
+        xzalloc(n_ofctrl_seqno_states * sizeof *new_states);
+
+    for (size_t i = 0; i < n_ofctrl_seqno_states - 1; i++) {
+        ovs_list_move(&new_states[i].acked_cfgs,
+                      &ofctrl_seqno_states[i].acked_cfgs);
+    }
+    ovs_list_init(&new_states[new_type].acked_cfgs);
+
+    free(ofctrl_seqno_states);
+    ofctrl_seqno_states = new_states;
+    return new_type;
+}
+
+/* Creates a new seqno update request for an application specific
+ * 'seqno_type'.
+ */
+void
+ofctrl_seqno_update_create(size_t seqno_type, uint64_t new_cfg)
+{
+    ovs_assert(seqno_type < n_ofctrl_seqno_states);
+
+    struct ofctrl_seqno_state *state = &ofctrl_seqno_states[seqno_type];
+
+    /* If new_cfg didn't change since the last request there should already
+     * be an update pending.
+     */
+    if (new_cfg == state->req_cfg) {
+        return;
+    }
+
+    state->req_cfg = new_cfg;
+    ofctrl_seqno_update_create__(seqno_type, new_cfg);
+}
+
+/* Should be called when the application is certain that all OVS flow updates
+ * corresponding to 'flow_cfg' were processed.  Populates the application
+ * specific lists of acked requests in 'ofctrl_seqno_states'.
+ */
+void
+ofctrl_seqno_run(uint64_t flow_cfg)
+{
+    struct ofctrl_seqno_update *update, *prev;
+    LIST_FOR_EACH_SAFE (update, prev, list_node, &ofctrl_seqno_updates) {
+        if (flow_cfg < update->flow_cfg) {
+            break;
+        }
+
+        ovs_list_remove(&update->list_node);
+        ofctrl_seqno_cfg_run(update->seqno_type, update);
+    }
+}
+
+/* Returns the seqno to be used when sending a barrier request to OVS. */
+uint64_t
+ofctrl_seqno_get_req_cfg(void)
+{
+    return ofctrl_req_seqno;
+}
+
+/* Should be called whenever the openflow connection to OVS is lost.  Flushes
+ * all pending 'ofctrl_seqno_updates'.
+ */
+void
+ofctrl_seqno_flush(void)
+{
+    for (size_t i = 0; i < n_ofctrl_seqno_states; i++) {
+        ofctrl_seqno_update_list_destroy(&ofctrl_seqno_states[i].acked_cfgs);
+    }
+    ofctrl_seqno_update_list_destroy(&ofctrl_seqno_updates);
+    ofctrl_req_seqno = 0;
+}
+
+static void
+ofctrl_acked_seqnos_init(struct ofctrl_acked_seqnos *seqnos,
+                         uint64_t last_acked)
+{
+    hmap_init(&seqnos->acked);
+    seqnos->last_acked = last_acked;
+}
+
+static void
+ofctrl_acked_seqnos_add(struct ofctrl_acked_seqnos *seqnos, uint32_t val)
+{
+    seqnos->last_acked = val;
+
+    struct ofctrl_ack_seqno *sn = xmalloc(sizeof *sn);
+    hmap_insert(&seqnos->acked, &sn->node, hash_int(val, 0));
+    sn->seqno = val;
+}
+
+static void
+ofctrl_seqno_update_create__(size_t seqno_type, uint64_t req_cfg)
+{
+    struct ofctrl_seqno_update *update = xmalloc(sizeof *update);
+
+    ofctrl_req_seqno++;
+    ovs_list_push_back(&ofctrl_seqno_updates, &update->list_node);
+    update->seqno_type = seqno_type;
+    update->flow_cfg = ofctrl_req_seqno;
+    update->req_cfg = req_cfg;
+}
+
+static void
+ofctrl_seqno_update_list_destroy(struct ovs_list *seqno_list)
+{
+    struct ofctrl_seqno_update *update;
+
+    LIST_FOR_EACH_POP (update, list_node, seqno_list) {
+        free(update);
+    }
+}
+
+static void
+ofctrl_seqno_cfg_run(size_t seqno_type, struct ofctrl_seqno_update *update)
+{
+    ovs_assert(seqno_type < n_ofctrl_seqno_states);
+    ovs_list_push_back(&ofctrl_seqno_states[seqno_type].acked_cfgs,
+                       &update->list_node);
+    ofctrl_seqno_states[seqno_type].cur_cfg = update->req_cfg;
+}

controller/ofctrl-seqno.h
@@ -1,0 +1,49 @@
+/* Copyright (c) 2021, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OFCTRL_SEQNO_H
+#define OFCTRL_SEQNO_H 1
+
+#include <stdint.h>
+
+#include <openvswitch/hmap.h>
+
+/* Collection of acked ofctrl_seqno_update requests and the most recent
+ * 'last_acked' value.
+ */
+struct ofctrl_acked_seqnos {
+    struct hmap acked;
+    uint64_t last_acked;
+};
+
+/* Acked application specific seqno.  Stored in ofctrl_acked_seqnos.acked. */
+struct ofctrl_ack_seqno {
+    struct hmap_node node;
+    uint64_t seqno;
+};
+
+struct ofctrl_acked_seqnos *ofctrl_acked_seqnos_get(size_t seqno_type);
+void ofctrl_acked_seqnos_destroy(struct ofctrl_acked_seqnos *seqnos);
+bool ofctrl_acked_seqnos_contains(const struct ofctrl_acked_seqnos *seqnos,
+                                  uint32_t val);
+
+void ofctrl_seqno_init(void);
+size_t ofctrl_seqno_add_type(void);
+void ofctrl_seqno_update_create(size_t seqno_type, uint64_t new_cfg);
+void ofctrl_seqno_run(uint64_t flow_cfg);
+uint64_t ofctrl_seqno_get_req_cfg(void);
+void ofctrl_seqno_flush(void);
+
+#endif /* controller/ofctrl-seqno.h */

controller/test-ofctrl-seqno.c
@@ -1,0 +1,194 @@
+/* Copyright (c) 2021, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "tests/ovstest.h"
+#include "sort.h"
+#include "util.h"
+
+#include "ofctrl-seqno.h"
+
+static void
+test_init(void)
+{
+    ofctrl_seqno_init();
+}
+
+static bool
+test_read_uint_value(struct ovs_cmdl_context *ctx, unsigned int index,
+                     const char *descr, unsigned int *result)
+{
+    if (index >= ctx->argc) {
+        fprintf(stderr, "Missing %s argument\n", descr);
+        return false;
+    }
+
+    const char *arg = ctx->argv[index];
+    if (!str_to_uint(arg, 10, result)) {
+        fprintf(stderr, "Invalid %s: %s\n", descr, arg);
+        return false;
+    }
+    return true;
+}
+
+static int
+test_seqno_compare(size_t a, size_t b, void *values_)
+{
+    uint64_t *values = values_;
+
+    return values[a] == values[b] ? 0 : (values[a] < values[b] ? -1 : 1);
+}
+
+static void
+test_seqno_swap(size_t a, size_t b, void *values_)
+{
+    uint64_t *values = values_;
+    uint64_t tmp = values[a];
+
+    values[a] = values[b];
+    values[b] = tmp;
+}
+
+static void
+test_dump_acked_seqnos(size_t seqno_type)
+{
+    struct ofctrl_acked_seqnos * acked_seqnos =
+        ofctrl_acked_seqnos_get(seqno_type);
+
+    printf("ofctrl-seqno-type: %"PRIuSIZE"\n", seqno_type);
+    printf("  last-acked %"PRIu64"\n", acked_seqnos->last_acked);
+
+    size_t n_acked = hmap_count(&acked_seqnos->acked);
+    uint64_t *acked = xmalloc(n_acked * sizeof *acked);
+    struct ofctrl_ack_seqno *ack_seqno;
+    size_t i = 0;
+
+    /* A bit hacky but ignoring overflows the "total of all seqno + 1" should
+     * be a number that is not part of the acked seqnos.
+     */
+    uint64_t total_seqno = 1;
+    HMAP_FOR_EACH (ack_seqno, node, &acked_seqnos->acked) {
+        ovs_assert(ofctrl_acked_seqnos_contains(acked_seqnos,
+                                                ack_seqno->seqno));
+        total_seqno += ack_seqno->seqno;
+        acked[i++] = ack_seqno->seqno;
+    }
+    ovs_assert(!ofctrl_acked_seqnos_contains(acked_seqnos, total_seqno));
+
+    sort(n_acked, test_seqno_compare, test_seqno_swap, acked);
+
+    for (i = 0; i < n_acked; i++) {
+        printf("  %"PRIu64"\n", acked[i]);
+    }
+
+    free(acked);
+    ofctrl_acked_seqnos_destroy(acked_seqnos);
+}
+
+static void
+test_ofctrl_seqno_add_type(struct ovs_cmdl_context *ctx)
+{
+    unsigned int n_types;
+
+    test_init();
+
+    if (!test_read_uint_value(ctx, 1, "n_types", &n_types)) {
+        return;
+    }
+    for (unsigned int i = 0; i < n_types; i++) {
+        printf("%"PRIuSIZE"\n", ofctrl_seqno_add_type());
+    }
+}
+
+static void
+test_ofctrl_seqno_ack_seqnos(struct ovs_cmdl_context *ctx)
+{
+    unsigned int n_reqs = 0;
+    unsigned int shift = 2;
+    unsigned int n_types;
+    unsigned int n_acks;
+
+    test_init();
+    bool batch_acks = !strcmp(ctx->argv[1], "true");
+
+    if (!test_read_uint_value(ctx, shift++, "n_types", &n_types)) {
+        return;
+    }
+
+    for (unsigned int i = 0; i < n_types; i++) {
+        ovs_assert(ofctrl_seqno_add_type() == i);
+
+        /* Read number of app specific seqnos. */
+        unsigned int n_app_seqnos;
+
+        if (!test_read_uint_value(ctx, shift++, "n_app_seqnos",
+                                  &n_app_seqnos)) {
+            return;
+        }
+
+        for (unsigned int j = 0; j < n_app_seqnos; j++, n_reqs++) {
+            unsigned int app_seqno;
+
+            if (!test_read_uint_value(ctx, shift++, "app_seqno", &app_seqno)) {
+                return;
+            }
+            ofctrl_seqno_update_create(i, app_seqno);
+        }
+    }
+    printf("ofctrl-seqno-req-cfg: %u\n", n_reqs);
+
+    if (!test_read_uint_value(ctx, shift++, "n_acks", &n_acks)) {
+        return;
+    }
+    for (unsigned int i = 0; i < n_acks; i++) {
+        unsigned int ack_seqno;
+
+        if (!test_read_uint_value(ctx, shift++, "ack_seqno", &ack_seqno)) {
+            return;
+        }
+        ofctrl_seqno_run(ack_seqno);
+
+        if (!batch_acks) {
+            for (unsigned int st = 0; st < n_types; st++) {
+                test_dump_acked_seqnos(st);
+            }
+        }
+    }
+    if (batch_acks) {
+        for (unsigned int st = 0; st < n_types; st++) {
+            test_dump_acked_seqnos(st);
+        }
+    }
+}
+
+static void
+test_ofctrl_seqno_main(int argc, char *argv[])
+{
+    set_program_name(argv[0]);
+    static const struct ovs_cmdl_command commands[] = {
+        {"ofctrl_seqno_add_type", NULL, 1, 1,
+         test_ofctrl_seqno_add_type, OVS_RO},
+        {"ofctrl_seqno_ack_seqnos", NULL, 2, INT_MAX,
+         test_ofctrl_seqno_ack_seqnos, OVS_RO},
+        {NULL, NULL, 0, 0, NULL, OVS_RO},
+    };
+    struct ovs_cmdl_context ctx;
+    ctx.argc = argc - 1;
+    ctx.argv = argv + 1;
+    ovs_cmdl_run_command(&ctx, commands);
+}
+
+OVSTEST_REGISTER("test-ofctrl-seqno", test_ofctrl_seqno_main);

tests/ovn-ofctrl-seqno.at
@@ -1,0 +1,226 @@
+#
+# Unit tests for the controller/ofctrl-seqno.c module.
+#
+AT_BANNER([OVN unit tests - ofctrl-seqno])
+
+AT_SETUP([ovn -- unit test -- ofctrl-seqno add-type])
+
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_add_type 1], [0], [dnl
+0
+])
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_add_type 2], [0], [dnl
+0
+1
+])
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_add_type 3], [0], [dnl
+0
+1
+2
+])
+AT_CLEANUP
+
+AT_SETUP([ovn -- unit test -- ofctrl-seqno ack-seqnos])
+
+AS_BOX([No Ack Batching, 1 seqno type])
+n_types=1
+n_app_seqnos=3
+app_seqnos="40 41 42"
+
+n_acks=1
+acks="1"
+echo "ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+])
+
+n_acks=2
+acks="1 2"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+ofctrl-seqno-type: 0
+  last-acked 41
+  41
+])
+
+n_acks=3
+acks="1 2 3"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+ofctrl-seqno-type: 0
+  last-acked 41
+  41
+ofctrl-seqno-type: 0
+  last-acked 42
+  42
+])
+
+AS_BOX([Ack Batching, 1 seqno type])
+n_types=1
+n_app_seqnos=3
+app_seqnos="40 41 42"
+
+n_acks=1
+acks="1"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+])
+
+n_acks=2
+acks="1 2"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 41
+  40
+  41
+])
+
+n_acks=3
+acks="1 2 3"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos} ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 3
+ofctrl-seqno-type: 0
+  last-acked 42
+  40
+  41
+  42
+])
+
+AS_BOX([No Ack Batching, 2 seqno types])
+n_types=2
+n_app_seqnos=3
+app_seqnos1="40 41 42"
+app_seqnos2="50 51 52"
+
+n_acks=1
+acks="1"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+ofctrl-seqno-type: 1
+  last-acked 0
+])
+
+n_acks=3
+acks="1 2 3"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+ofctrl-seqno-type: 1
+  last-acked 0
+ofctrl-seqno-type: 0
+  last-acked 41
+  41
+ofctrl-seqno-type: 1
+  last-acked 0
+ofctrl-seqno-type: 0
+  last-acked 42
+  42
+ofctrl-seqno-type: 1
+  last-acked 0
+])
+
+n_acks=3
+acks="4 5 6"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos false ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 42
+  40
+  41
+  42
+ofctrl-seqno-type: 1
+  last-acked 50
+  50
+ofctrl-seqno-type: 0
+  last-acked 42
+ofctrl-seqno-type: 1
+  last-acked 51
+  51
+ofctrl-seqno-type: 0
+  last-acked 42
+ofctrl-seqno-type: 1
+  last-acked 52
+  52
+])
+
+AS_BOX([Ack Batching, 2 seqno types])
+n_types=2
+n_app_seqnos=3
+app_seqnos1="40 41 42"
+app_seqnos2="50 51 52"
+
+n_acks=1
+acks="1"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 40
+  40
+ofctrl-seqno-type: 1
+  last-acked 0
+])
+
+n_acks=3
+acks="1 2 3"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 42
+  40
+  41
+  42
+ofctrl-seqno-type: 1
+  last-acked 0
+])
+
+n_acks=3
+acks="4 5 6"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 6
+ofctrl-seqno-type: 0
+  last-acked 42
+  40
+  41
+  42
+ofctrl-seqno-type: 1
+  last-acked 52
+  50
+  51
+  52
+])
+AT_CLEANUP

controller/automake.mk
@@ -18,6 +18,8 @@ controller_ovn_controller_SOURCES = \
 	controller/lport.h \
 	controller/ofctrl.c \
 	controller/ofctrl.h \
+	controller/ofctrl-seqno.c \
+	controller/ofctrl-seqno.h \
 	controller/pinctrl.c \
 	controller/pinctrl.h \
 	controller/patch.c \

commit 292717d64f86b601a139164e70b75ba356cd31be

ofctrl-seqno: Do not truncate the last acked value
The requested and acked seqno values are allowed
to be uint64_t, however the values that were added
to the hmap were truncated to uint32_t. This would
lead to loss of information when the value is bigger.

Use uin64_t for the function signatures and for the
hash to prevent truncation.

Fixes: c93c626248c1 ("controller: Implement a generic barrier based on ofctrl cur_cfg sync.")
Reported-at: https://bugzilla.redhat.com/2074019
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/ofctrl-seqno.c
@@ -59,7 +59,7 @@ static struct ofctrl_seqno_state *ofctrl_seqno_states;
 static void ofctrl_acked_seqnos_init(struct ofctrl_acked_seqnos *seqnos,
                                      uint64_t last_acked);
 static void ofctrl_acked_seqnos_add(struct ofctrl_acked_seqnos *seqnos,
-                                    uint32_t val);
+                                    uint64_t val);
 
 /* ofctrl_seqno_update related static function prototypes. */
 static void ofctrl_seqno_update_create__(size_t seqno_type, uint64_t req_cfg);
@@ -106,11 +106,11 @@ ofctrl_acked_seqnos_destroy(struct ofctrl_acked_seqnos *seqnos)
 /* Returns true if 'val' is one of the acked sequence numbers in 'seqnos'. */
 bool
 ofctrl_acked_seqnos_contains(const struct ofctrl_acked_seqnos *seqnos,
-                             uint32_t val)
+                             uint64_t val)
 {
     struct ofctrl_ack_seqno *sn;
 
-    HMAP_FOR_EACH_WITH_HASH (sn, node, hash_int(val, 0), &seqnos->acked) {
+    HMAP_FOR_EACH_WITH_HASH (sn, node, hash_uint64(val), &seqnos->acked) {
         if (sn->seqno == val) {
             return true;
         }
@@ -213,12 +213,12 @@ ofctrl_acked_seqnos_init(struct ofctrl_acked_seqnos *seqnos,
 }
 
 static void
-ofctrl_acked_seqnos_add(struct ofctrl_acked_seqnos *seqnos, uint32_t val)
+ofctrl_acked_seqnos_add(struct ofctrl_acked_seqnos *seqnos, uint64_t val)
 {
     seqnos->last_acked = val;
 
     struct ofctrl_ack_seqno *sn = xmalloc(sizeof *sn);
-    hmap_insert(&seqnos->acked, &sn->node, hash_int(val, 0));
+    hmap_insert(&seqnos->acked, &sn->node, hash_uint64(val));
     sn->seqno = val;
 }
 

controller/ofctrl-seqno.h
@@ -37,7 +37,7 @@ struct ofctrl_ack_seqno {
 struct ofctrl_acked_seqnos *ofctrl_acked_seqnos_get(size_t seqno_type);
 void ofctrl_acked_seqnos_destroy(struct ofctrl_acked_seqnos *seqnos);
 bool ofctrl_acked_seqnos_contains(const struct ofctrl_acked_seqnos *seqnos,
-                                  uint32_t val);
+                                  uint64_t val);
 
 void ofctrl_seqno_init(void);
 size_t ofctrl_seqno_add_type(void);

controller/test-ofctrl-seqno.c
@@ -124,9 +124,10 @@ test_ofctrl_seqno_ack_seqnos(struct ovs_cmdl_context *ctx)
         }
 
         for (unsigned int j = 0; j < n_app_seqnos; j++, n_reqs++) {
-            unsigned int app_seqno;
+            unsigned long long int app_seqno;
 
-            if (!test_read_uint_value(ctx, shift++, "app_seqno", &app_seqno)) {
+            if (!test_read_ullong_value(ctx, shift++, "app_seqno",
+                                        &app_seqno)) {
                 return;
             }
             ofctrl_seqno_update_create(i, app_seqno);
@@ -138,9 +139,9 @@ test_ofctrl_seqno_ack_seqnos(struct ovs_cmdl_context *ctx)
         return;
     }
     for (unsigned int i = 0; i < n_acks; i++) {
-        unsigned int ack_seqno;
+        unsigned long long int ack_seqno;
 
-        if (!test_read_uint_value(ctx, shift++, "ack_seqno", &ack_seqno)) {
+        if (!test_read_ullong_value(ctx, shift++, "ack_seqno", &ack_seqno)) {
             return;
         }
         ofctrl_seqno_run(ack_seqno);

tests/ovn-ofctrl-seqno.at
@@ -223,4 +223,37 @@ ofctrl-seqno-type: 1
   51
   52
 ])
+
+AS_BOX([Ack seqno that doesn't fit in uint32_t])
+n_types=2
+n_app_seqnos=1
+app_seqnos1="4294967296"
+app_seqnos2="4294967297"
+
+n_acks=1
+acks="1"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 2
+ofctrl-seqno-type: 0
+  last-acked 4294967296
+  4294967296
+ofctrl-seqno-type: 1
+  last-acked 0
+])
+
+n_acks=1
+acks="2"
+AT_CHECK([ovstest test-ofctrl-seqno ofctrl_seqno_ack_seqnos true ${n_types} \
+          ${n_app_seqnos} ${app_seqnos1} ${n_app_seqnos} ${app_seqnos2} \
+          ${n_acks} ${acks}], [0], [dnl
+ofctrl-seqno-req-cfg: 2
+ofctrl-seqno-type: 0
+  last-acked 4294967296
+  4294967296
+ofctrl-seqno-type: 1
+  last-acked 4294967297
+  4294967297
+])
 AT_CLEANUP

tests/test-utils.c
@@ -61,3 +61,20 @@ test_read_value(struct ovs_cmdl_context *ctx, unsigned int index,
 
     return ctx->argv[index];
 }
+
+bool
+test_read_ullong_value(struct ovs_cmdl_context *ctx, unsigned int index,
+                       const char *descr, unsigned long long int *result)
+{
+    if (index >= ctx->argc) {
+        fprintf(stderr, "Missing %s argument\n", descr);
+        return false;
+    }
+
+    const char *arg = ctx->argv[index];
+    if (!str_to_ullong(arg, 10, result)) {
+        fprintf(stderr, "Invalid %s: %s\n", descr, arg);
+        return false;
+    }
+    return true;
+}

tests/test-utils.h
@@ -24,5 +24,7 @@ bool test_read_uint_hex_value(struct ovs_cmdl_context *ctx, unsigned int index,
                               const char *descr, unsigned int *result);
 const char *test_read_value(struct ovs_cmdl_context *ctx, unsigned int index,
                             const char *descr);
+bool test_read_ullong_value(struct ovs_cmdl_context *ctx, unsigned int index,
+                            const char *descr, unsigned long long int *result);
 
 #endif /* tests/test-utils.h */