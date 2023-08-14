ovn-org/ovn

commit 1b0dbde940706e5de6e60221be78a278361fa76d

ovn-controller: Only set monitor conditions on available tables.
Setting the monitor condition on a table that's not part of the SB
schema triggers an error from the server side, a downgrade to monitor v0
(from v2) and also causes the conditional monitoring sequence numbers to
go out of sync.

Use the <DB>rec_server_has_<TABLE>_table() API to determine if a table
is present in the server schema and therefore usable in the monitor
condition.

This commit also bumps the OVS submodule because the following fix is
needed:
  a787fbbf9dd6 ("ovsdb-cs: Consider default conditions implicitly acked.")

We also pick up the following relevant OVS changes:
  b8bf410a5c94 db-ctl-base: Use partial map/set updates for last add/set commands.
  55b9507e6824 ovsdb-idl: Add the support to specify the uuid for row insert.

Reported-by: Numan Siddique <numans@ovn.org>
Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2151063
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/ovn-controller.c
@@ -153,6 +153,14 @@ struct pending_pkt {
 /* Registered ofctrl seqno type for nb_cfg propagation. */
 static size_t ofctrl_seq_type_nb_cfg;
 
+/* Only set monitor conditions on tables that are available in the
+ * server schema.
+ */
+#define sb_table_set_monitor_condition(idl, table, cond)   \
+    (sbrec_server_has_##table##_table(idl)              \
+     ? sbrec_##table##_set_condition(idl, cond) \
+     : 0)
+
 static unsigned int
 update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
                    const struct sbrec_chassis *chassis,
@@ -288,17 +296,17 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
 
 out:;
     unsigned int cond_seqnos[] = {
-        sbrec_port_binding_set_condition(ovnsb_idl, &pb),
-        sbrec_logical_flow_set_condition(ovnsb_idl, &lf),
-        sbrec_logical_dp_group_set_condition(ovnsb_idl, &ldpg),
-        sbrec_mac_binding_set_condition(ovnsb_idl, &mb),
-        sbrec_multicast_group_set_condition(ovnsb_idl, &mg),
-        sbrec_dns_set_condition(ovnsb_idl, &dns),
-        sbrec_controller_event_set_condition(ovnsb_idl, &ce),
-        sbrec_ip_multicast_set_condition(ovnsb_idl, &ip_mcast),
-        sbrec_igmp_group_set_condition(ovnsb_idl, &igmp),
-        sbrec_chassis_private_set_condition(ovnsb_idl, &chprv),
-        sbrec_chassis_template_var_set_condition(ovnsb_idl, &tv),
+        sb_table_set_monitor_condition(ovnsb_idl, port_binding, &pb),
+        sb_table_set_monitor_condition(ovnsb_idl, logical_flow, &lf),
+        sb_table_set_monitor_condition(ovnsb_idl, logical_dp_group, &ldpg),
+        sb_table_set_monitor_condition(ovnsb_idl, mac_binding, &mb),
+        sb_table_set_monitor_condition(ovnsb_idl, multicast_group, &mg),
+        sb_table_set_monitor_condition(ovnsb_idl, dns, &dns),
+        sb_table_set_monitor_condition(ovnsb_idl, controller_event, &ce),
+        sb_table_set_monitor_condition(ovnsb_idl, ip_multicast, &ip_mcast),
+        sb_table_set_monitor_condition(ovnsb_idl, igmp_group, &igmp),
+        sb_table_set_monitor_condition(ovnsb_idl, chassis_private, &chprv),
+        sb_table_set_monitor_condition(ovnsb_idl, chassis_template_var, &tv),
     };
 
     unsigned int expected_cond_seqno = 0;

utilities/ovn-dbctl.c
@@ -721,7 +721,7 @@ do_dbctl(const struct ovn_dbctl_options *dbctl_options,
     ctl_context_init(ctx, NULL, idl, txn, symtab, NULL);
     for (size_t i = 0; i < n_commands; i++) {
         struct ctl_command *c = &commands[i];
-        ctl_context_init_command(ctx, c);
+        ctl_context_init_command(ctx, c, c == &commands[n_commands - 1]);
         if (c->syntax->run) {
             (c->syntax->run)(ctx);
         }

utilities/ovn-ic-nbctl.c
@@ -675,9 +675,9 @@ static const struct ctl_table_class tables[ICNBREC_N_TABLES] = {
 
 static void
 ic_nbctl_context_init_command(struct ic_nbctl_context *ic_nbctl_ctx,
-                           struct ctl_command *command)
+                           struct ctl_command *command, bool last_command)
 {
-    ctl_context_init_command(&ic_nbctl_ctx->base, command);
+    ctl_context_init_command(&ic_nbctl_ctx->base, command, last_command);
 }
 
 static void
@@ -762,7 +762,8 @@ do_ic_nbctl(const char *args, struct ctl_command *commands, size_t n_commands,
     }
     ic_nbctl_context_init(&ic_nbctl_ctx, NULL, idl, txn, symtab);
     for (c = commands; c < &commands[n_commands]; c++) {
-        ic_nbctl_context_init_command(&ic_nbctl_ctx, c);
+        ic_nbctl_context_init_command(&ic_nbctl_ctx, c,
+                                      c == &commands[n_commands - 1]);
         if (c->syntax->run) {
             (c->syntax->run)(&ic_nbctl_ctx.base);
         }

utilities/ovn-ic-sbctl.c
@@ -747,9 +747,9 @@ static const struct ctl_table_class tables[ICSBREC_N_TABLES] = {
 
 static void
 ic_sbctl_context_init_command(struct ic_sbctl_context *ic_sbctl_ctx,
-                           struct ctl_command *command)
+                           struct ctl_command *command, bool last_command)
 {
-    ctl_context_init_command(&ic_sbctl_ctx->base, command);
+    ctl_context_init_command(&ic_sbctl_ctx->base, command, last_command);
 }
 
 static void
@@ -833,7 +833,8 @@ do_ic_sbctl(const char *args, struct ctl_command *commands, size_t n_commands,
     }
     ic_sbctl_context_init(&ic_sbctl_ctx, NULL, idl, txn, symtab);
     for (c = commands; c < &commands[n_commands]; c++) {
-        ic_sbctl_context_init_command(&ic_sbctl_ctx, c);
+        ic_sbctl_context_init_command(&ic_sbctl_ctx, c,
+                                      c == &commands[n_commands - 1]);
         if (c->syntax->run) {
             (c->syntax->run)(&ic_sbctl_ctx.base);
         }

commit f66abc59e173a13eb299902006874d41fea40302

ovn-controller: Assume well-known tables are in the SB schema.
If no monitor condition is set then the IDL layer assumes the user
wishes to monitor all records of a given table.  Since 1b0dbde94070
("ovn-controller: Only set monitor conditions on available tables.")
ovn-controller doesn't set monitor conditions for tables that are not
part of the schema received from the server; ovn-controller doesn't
get a chance to update conditions between the moment when the schema
was received from the Southbound and the monitor_cond_since request.

That essentially means that ovn-controller ends up downloading all
remote SB content at start up, significantly increasing memory usage.

It's safe to assume that tables that existed in the previous LTS branch
first release (currently 22.03.0) can be monitored directly.  Do so and
only "optionally" monitor the ones that have been added since.

This way we avoid the need for the IDL to expose an API to change the
default condition for monitored tables.  It also avoids complex code in
ovn-controller because we'd otherwise have to explicitly re-initialize
conditions to a non-default (false) value after every SB reconnect.

NOTE: In order to make sure that pre-existing L3 and L2 gateways are not
initially considered "non-local" we explicitly request for all port
bindings of this type to be monitored in the startup stage (before we
got the initial contents of the database and our chassis record).

Fixes: 1b0dbde94070 ("ovn-controller: Only set monitor conditions on available tables.")
Reported-at: https://mail.openvswitch.org/pipermail/ovs-dev/2023-July/406201.html
Reported-by: Vladislav Odintsov <odivlad@gmail.com>
CC: Ales Musil <amusil@redhat.com>
CC: Han Zhou <hzhou@ovn.org>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Han Zhou <hzhou@ovn.org>
Acked-by: Ales Musil <amusil@redhat.com>

controller/ovn-controller.c
@@ -190,10 +190,14 @@ static char *get_file_system_id(void)
 /* Only set monitor conditions on tables that are available in the
  * server schema.
  */
-#define sb_table_set_monitor_condition(idl, table, cond)   \
-    (sbrec_server_has_##table##_table(idl)              \
-     ? sbrec_##table##_set_condition(idl, cond) \
+#define sb_table_set_opt_mon_condition(idl, table, cond) \
+    (sbrec_server_has_##table##_table(idl)               \
+     ? sbrec_##table##_set_condition(idl, cond)          \
      : 0)
+
+/* Assume the table exists in the server schema and set its condition. */
+#define sb_table_set_req_mon_condition(idl, table, cond) \
+    sbrec_##table##_set_condition(idl, cond)
 
 static unsigned int
 update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
@@ -294,6 +298,14 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
         /* During initialization, we monitor all records in Chassis_Private so
          * that we don't try to recreate existing ones. */
         ovsdb_idl_condition_add_clause_true(&chprv);
+        /* Also, to avoid traffic disruption (e.g., conntrack flushing for
+         * zones that are used by OVN but not yet known due to the SB initial
+         * contents not being available), monitor all port bindings
+         * connected to gateways; they might be claimed as soon as the
+         * chassis is available.
+         */
+        sbrec_port_binding_add_clause_type(&pb, OVSDB_F_EQ, "l2gateway");
+        sbrec_port_binding_add_clause_type(&pb, OVSDB_F_EQ, "l3gateway");
     }
 
     if (local_ifaces) {
@@ -342,17 +354,17 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
 
 out:;
     unsigned int cond_seqnos[] = {
-        sb_table_set_monitor_condition(ovnsb_idl, port_binding, &pb),
-        sb_table_set_monitor_condition(ovnsb_idl, logical_flow, &lf),
-        sb_table_set_monitor_condition(ovnsb_idl, logical_dp_group, &ldpg),
-        sb_table_set_monitor_condition(ovnsb_idl, mac_binding, &mb),
-        sb_table_set_monitor_condition(ovnsb_idl, multicast_group, &mg),
-        sb_table_set_monitor_condition(ovnsb_idl, dns, &dns),
-        sb_table_set_monitor_condition(ovnsb_idl, controller_event, &ce),
-        sb_table_set_monitor_condition(ovnsb_idl, ip_multicast, &ip_mcast),
-        sb_table_set_monitor_condition(ovnsb_idl, igmp_group, &igmp),
-        sb_table_set_monitor_condition(ovnsb_idl, chassis_private, &chprv),
-        sb_table_set_monitor_condition(ovnsb_idl, chassis_template_var, &tv),
+        sb_table_set_req_mon_condition(ovnsb_idl, port_binding, &pb),
+        sb_table_set_req_mon_condition(ovnsb_idl, logical_flow, &lf),
+        sb_table_set_req_mon_condition(ovnsb_idl, logical_dp_group, &ldpg),
+        sb_table_set_req_mon_condition(ovnsb_idl, mac_binding, &mb),
+        sb_table_set_req_mon_condition(ovnsb_idl, multicast_group, &mg),
+        sb_table_set_req_mon_condition(ovnsb_idl, dns, &dns),
+        sb_table_set_req_mon_condition(ovnsb_idl, controller_event, &ce),
+        sb_table_set_req_mon_condition(ovnsb_idl, ip_multicast, &ip_mcast),
+        sb_table_set_req_mon_condition(ovnsb_idl, igmp_group, &igmp),
+        sb_table_set_req_mon_condition(ovnsb_idl, chassis_private, &chprv),
+        sb_table_set_opt_mon_condition(ovnsb_idl, chassis_template_var, &tv),
     };
 
     unsigned int expected_cond_seqno = 0;