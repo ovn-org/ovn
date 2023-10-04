cf99264e252c20edf93ab5735e18aa3225c98398
Dumitru Ceara <dceara@redhat.com>
2023-10-04 13:23:56 +0200

    ci: Free up disk space in a more robust way.
    Use apt patterns [0] to only try to remove real packages.  Also unify
    the way we free up disk space for all workflows.
    
    [0] https://manpages.ubuntu.com/manpages/focal/man7/apt-patterns.7.html
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Acked-by: Ales Musil <amusil@redhat.com>
    (cherry picked from commit 440751ad7d7489a683459577c60f81fa7ee68fff)

---
+++ .ci/linux-util.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function free_up_disk_space_ubuntu()
+{
+    local pkgs='azure-cli aspnetcore-* dotnet-* ghc-* firefox*
+                google-chrome-stable google-cloud-sdk libmono-* llvm-*
+                microsoft-edge-stable mono-* msbuild mysql-server-core-*
+                php-* php7* powershell* temurin-* zulu-*'
+
+    # Use apt patterns to only select real packages that match the names
+    # in the list above.
+    local pkgs=$(echo $pkgs | sed 's/[^ ]* */~n&/g')
+
+    sudo apt update && sudo apt-get --auto-remove -y purge $pkgs
+}
