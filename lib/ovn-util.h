01acb54c79cf47449f3969e98fabc247bd93ec38
Ales Musil <amusil@redhat.com>
2023-10-06 09:02:18 +0200

    controller, northd: Wait for cleanup before replying to exit
    The unixctl exit command would receive reply immediately
    which is confusing and can cause some issues in some tests
    if the cleanup takes longer than expected. To avoid that
    make sure we reply to the exit command only after the
    main cleanup was done so there shouldn't be any possible
    window when the services are working when they are no longer
    expected to.
    
    Because it is in theory possible that we will receive multiple
    exit commands while waiting for the cleanup, make sure that we
    will reply to all of them by storing them in array.
    
    At the same time unify the exit structure for both ovn-controller
    and northd, so it can be easily extended as needed.
    
    This is inspired by OvS commit that was solving similar issue:
    24520a401e06 ("vswitchd: Wait for a bridge exit before replying to exit unixctl.")
    
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- lib/ovn-util.h
+++ lib/ovn-util.h
@@ -333,4 +333,16 @@ void daemon_started_recently_ignore(void);
 bool daemon_started_recently(void);
 int64_t daemon_startup_ts(void);
 
+/* Utilities around properly handling exit command. */
+struct ovn_exit_args {
+    struct unixctl_conn **conns;
+    size_t n_conns;
+    bool exiting;
+    bool restart;
+};
+
+void ovn_exit_command_callback(struct unixctl_conn *conn, int argc,
+                               const char *argv[], void *exit_args_);
+void ovn_exit_args_finish(struct ovn_exit_args *exit_args);
+
 #endif /* OVN_UTIL_H */
