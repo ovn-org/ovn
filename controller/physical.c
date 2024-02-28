894ffe8a812ba389d3c446bfd4d7d74c79d16cfc
Mohammad Heib <mheib@redhat.com>
2024-02-28 12:42:32 +0200

    controller: Release container lport when releasing parent port.
    Currently if the user sets the container parent_port:requested-chassis
    option after the VIF/CIF is bonded to the chassis, this will migrate
    the VIF/CIF flows to the new chassis but will still have the
    container flows installed in the old chassis which can allow unwanted
    tagged traffic to reach VMS/containers on the old chassis.
    
    This patch will resolve the above issue by remove the CIF flows
    from the old chassis and prevent the CIF from being bonded to a
    chassis different from the parent port VIF binding chassis.
    
    Rreported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2220938
    Signed-off-by: Mohammad Heib <mheib@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- controller/physical.c
+++ controller/physical.c
@@ -1631,6 +1631,15 @@ consider_port_binding(struct ovsdb_idl_index *sbrec_port_binding_by_name,
             nested_container = true;
             parent_port = lport_lookup_by_name(
                 sbrec_port_binding_by_name, binding->parent_port);
+
+            if (parent_port
+                && !lport_can_bind_on_this_chassis(chassis, parent_port)) {
+                /* Even though there is an ofport for this container
+                 * parent port, it is requested on different chassis ignore
+                 * this container port.
+                 */
+                return;
+            }
         }
     } else if (!strcmp(binding->type, "localnet")
              || !strcmp(binding->type, "l2gateway")) {
