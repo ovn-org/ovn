83c2238cbf41f6600fe024ce1df58c9800299bd6
Dumitru Ceara <dceara@redhat.com>
2024-06-18 21:36:56 +0200

    northd: ic: Remove unused function and fix unit test.
    There are differences between branches 23.09 and 24.03 (e.g., IC tests
    have been consolidated in ovn-ic.at) which made the original backport
    non-trivial and some changes were missed.
    
    This commit fixes that.
    
    Fixes: 5f0809be5657 ("Revert "northd: Don't skip transit switch LSP when creating mcast groups."")
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- northd/northd.c
+++ northd/northd.c
@@ -806,12 +806,6 @@ ovn_datapath_is_stale(const struct ovn_datapath *od)
     return !od->nbr && !od->nbs;
 }
 
-static inline bool
-ovn_datapath_is_transit_switch(const struct ovn_datapath *od)
-{
-    return od->tunnel_key >= OVN_MIN_DP_KEY_GLOBAL;
-}
-
 static struct ovn_datapath *
 ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
                         const struct hmap *lr_datapaths,
