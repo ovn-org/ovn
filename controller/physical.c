b48f7ea49883385ab8e4420ce8005ea0d9c3f866
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2025-01-17 18:29:46 +0100

    controller: Update physical flows for peer port when the patch port is removed.
    Similar to commit 1431276926 ("controller: fix ovn patch port
    incremental processing"), update peer logical flows when the related
    patch port is removed.
    
    Reported-at: https://issues.redhat.com/browse/FDP-947
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>
    (cherry picked from commit 73a10345a29c8d983566ea86c2a9ce1f91c7c963)

--- controller/physical.c
+++ controller/physical.c
@@ -2411,13 +2411,21 @@ physical_handle_flows_for_lport(const struct sbrec_port_binding *pb,
         physical_multichassis_reprocess(pb, p_ctx, flow_table);
     }
 
-    if (!removed) {
+    /* Always update pb and the configured peer for patch ports. */
+    if (!removed || !strcmp(pb->type, "patch")) {
         physical_eval_port_binding(p_ctx, pb, flow_table);
-        if (!strcmp(pb->type, "patch")) {
-            const struct sbrec_port_binding *peer =
-                get_binding_peer(p_ctx->sbrec_port_binding_by_name, pb);
-            if (peer) {
-                physical_eval_port_binding(p_ctx, peer, flow_table);
+    }
+
+    if (!strcmp(pb->type, "patch")) {
+        if (removed) {
+            ofctrl_remove_flows(flow_table, &pb->header_.uuid);
+        }
+        const struct sbrec_port_binding *peer =
+            get_binding_peer(p_ctx->sbrec_port_binding_by_name, pb);
+        if (peer) {
+            physical_eval_port_binding(p_ctx, peer, flow_table);
+            if (removed) {
+                ofctrl_remove_flows(flow_table, &peer->header_.uuid);
             }
         }
     }
