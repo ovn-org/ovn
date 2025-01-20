b5c13587fc6a56bf1d7a337b0f3dfee4fb7e28ed
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2025-01-20 14:43:08 +0100

    northd: Trigger a full recompute if lb neigh_mode option is updated.
    Trigger a full recompute in lb_data_load_balancer_handler() if
    neigh_mode option is updated. since it can't be managed
    incrementally for the moment.
    
    Reported-at: https://issues.redhat.com/browse/FDP-1054
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- northd/en-lb-data.c
+++ northd/en-lb-data.c
@@ -181,6 +181,7 @@ lb_data_load_balancer_handler(struct engine_node *node, void *data)
             struct sset old_ips_v6 = SSET_INITIALIZER(&old_ips_v6);
             sset_swap(&lb->ips_v4, &old_ips_v4);
             sset_swap(&lb->ips_v6, &old_ips_v6);
+            enum lb_neighbor_responder_mode neigh_mode = lb->neigh_mode;
             bool routable = lb->routable;
             ovn_northd_lb_reinit(lb, tracked_lb);
             health_checks |= lb->health_checks;
@@ -213,6 +214,10 @@ lb_data_load_balancer_handler(struct engine_node *node, void *data)
             if (routable != lb->routable) {
                 /* If routable is toggled trigger a full recompute.
                  */
+                return false;
+            }
+            if (neigh_mode != lb->neigh_mode) {
+                /* If neigh_mode is updated trigger a full recompute. */
                 return false;
             }
         }
