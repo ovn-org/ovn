b5f1b884fcc68a9b5854b15de0da3ed997a8a747
Ilya Maximets <i.maximets@ovn.org>
2025-02-03 22:15:42 +0100

    mac-cache: Fix expiration of active MAC binding due to skipped update.
    ovn-controller doesn't update timestamps until 3/4 of the expiration
    threshold have passed, but then it may wait for another 3/4.  And since
    3/4 + 3/4 > 1, northd would remove the entry before ovn-controller
    wakes up to refresh it next time.
    
    For example:
    Let's say the threshold is 20 seconds and all the entries we create
    have constant traffic flow, so should never expire.  Let's see the
    timeline that starts at time A:
    
      1. [A]    We create one mac entry, next statistics request: A + 15.
    
      2. [A+5]  We create second entry, next request doesn't change and
                is still at A + 15.
    
      3. [A+15] We send an OF stats request and get a reply.
    
         a. First mac entry is updated, because dump interval is 15 and
            the original timestamp is A, which is now + some delta,
            i.e., A + 15 <= A + 15.
    
         b. Second entry is not updated, because (A + 5) + 15 > A + 15.
    
         c. The next request is scheduled for 'now + 15' (thresholds didn't
            change), so the next request is at A + 30.
    
      4. [A+25] northd removes the second entry since it wasn't updated and
                the threshold passed.
    
      5. [A+30] We send an OF stats request and get a reply, but the entry
                is already removed.
    
    In order to fix that we must never skip the update if the next dump
    period may go over the threshold.  This can be achieved by choosing the
    cooldown period small enough, so together with the dump period they do
    not go over the threshold and actually leave some room for all the
    processing and the updates.
    
    Let's use 1/2 of the threshold as the dump period and 1/4 as a cooldown
    period.  This should fix the issue while still not sending too many
    transactions to the database and not requesting the OpenFlow statistics
    too often.  1/2 + 1/4 = 3/4, so we still have enough room for all the
    processing before the aging threshold.
    
    NOTE: In the worst case this will increase the number of transactions to
    the database to 4 per mac binding, per aging period.
    
    New logs added for easier debugging of such issues.
    
    A new test is added to demonstrate the issue and verify the fix.
    
    Fixes: e1ab41ee32aa ("controller: Update MAC binding timestamp")
    Reported-at: https://issues.redhat.com/browse/FDP-1130
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
    Acked-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    (cherry picked from commit ab4433837b261ebf8e9821c0f6bfb5507c29a774)

--- controller/mac_cache.c
+++ controller/mac_cache.c
@@ -36,6 +36,13 @@ mac_cache_mac_binding_find(struct mac_cache_data *data,
 static bool
 mac_cache_mb_data_from_sbrec(struct mac_cache_mb_data *data,
                               const struct sbrec_mac_binding *mb);
+static void mac_cache_mac_binding_update_log(
+    const char *action,
+    const struct mac_cache_mb_data *,
+    bool print_times,
+    const struct mac_cache_threshold *,
+    int64_t idle_age_ms,
+    uint64_t since_updated_ms);
 static uint32_t
 mac_cache_fdb_data_hash(const struct mac_cache_fdb_data *fdb_data);
 static inline bool
@@ -78,7 +85,16 @@ mac_cache_threshold_add(struct mac_cache_data *data,
     threshold = xmalloc(sizeof *threshold);
     threshold->uuid = dp->header_.uuid;
     threshold->value = value;
-    threshold->dump_period = (3 * value) / 4;
+    threshold->dump_period = value / 2;
+    threshold->cooldown_period = value / 4;
+
+    /* (cooldown_period + dump_period) is the maximum time the timestamp may
+     * be not updated.  So, the sum of those times must be lower than the
+     * threshold, otherwise we may fail to update an active MAC binding in
+     * time and risk it being removed.  Giving it an extra 1/10 of the time
+     * for all the processing that needs to happen. */
+    ovs_assert(threshold->cooldown_period + threshold->dump_period
+               < (9 * value) / 10);
 
     hmap_insert(thresholds, &threshold->hmap_node,
                 uuid_hash(&dp->header_.uuid));
@@ -131,6 +147,7 @@ mac_cache_mac_binding_add(struct mac_cache_data *data,
 
     mc_mb->data = mb_data;
     mc_mb->sbrec = mb;
+    mac_cache_mac_binding_update_log("Added", &mb_data, false, NULL, 0, 0);
 }
 
 void
@@ -148,6 +165,8 @@ mac_cache_mac_binding_remove(struct mac_cache_data *data,
         return;
     }
 
+    mac_cache_mac_binding_update_log("Removed", &mc_mb->data, false,
+                                     NULL, 0, 0);
     hmap_remove(&data->mac_bindings, &mc_mb->hmap_node);
     free(mc_mb);
 }
@@ -306,6 +325,34 @@ mac_cache_mb_stats_process_flow_stats(struct ovs_list *stats_list,
     ovs_list_push_back(stats_list, &stats->list_node);
 }
 
+static void
+mac_cache_mac_binding_update_log(
+    const char *action,
+    const struct mac_cache_mb_data *data,
+    bool print_times,
+    const struct mac_cache_threshold *threshold,
+    int64_t idle_age_ms, uint64_t since_updated_ms)
+{
+    if (!VLOG_IS_DBG_ENABLED()) {
+        return;
+    }
+
+    struct ds s = DS_EMPTY_INITIALIZER;
+
+    ds_put_cstr(&s, action);
+    ds_put_cstr(&s, " MAC binding (");
+    mac_cache_mac_binding_data_to_string(data, &s);
+    if (print_times) {
+        ds_put_format(&s, "), last update: %"PRIu64"ms ago,"
+                          " idle age: %"PRIi64"ms, threshold: %"PRIu64"ms",
+                      since_updated_ms, idle_age_ms, threshold->value);
+    } else {
+        ds_put_char(&s, ')');
+    }
+    VLOG_DBG("%s.", ds_cstr_ro(&s));
+    ds_destroy(&s);
+}
+
 void
 mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
                        void *data)
@@ -319,27 +366,48 @@ mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
         struct mac_cache_mac_binding *mc_mb =
                 mac_cache_mac_binding_find(cache_data, &stats->data.mb);
         if (!mc_mb) {
+            mac_cache_mac_binding_update_log("Not found in the cache:",
+                                             &stats->data.mb, false,
+                                             NULL, 0, 0);
             free(stats);
             continue;
         }
 
         struct uuid *dp_uuid = &mc_mb->sbrec->datapath->header_.uuid;
+        uint64_t since_updated_ms = timewall_now - mc_mb->sbrec->timestamp;
         struct mac_cache_threshold *threshold =
                 mac_cache_threshold_find(thresholds, dp_uuid);
 
         /* If "idle_age" is under threshold it means that the mac binding is
-         * used on this chassis. Also make sure that we don't update the
-         * timestamp more than once during the dump period. */
-        if (stats->idle_age_ms < threshold->value &&
-            (timewall_now - mc_mb->sbrec->timestamp) >=
-            threshold->dump_period) {
-            sbrec_mac_binding_set_timestamp(mc_mb->sbrec, timewall_now);
+         * used on this chassis. */
+        if (stats->idle_age_ms < threshold->value) {
+            if (since_updated_ms >= threshold->cooldown_period) {
+                mac_cache_mac_binding_update_log("Updating active",
+                                                 &mc_mb->data, true, threshold,
+                                                 stats->idle_age_ms,
+                                                 since_updated_ms);
+                sbrec_mac_binding_set_timestamp(mc_mb->sbrec, timewall_now);
+            } else {
+                /* Postponing the update to avoid sending database transactions
+                 * too frequently. */
+                mac_cache_mac_binding_update_log("Not updating active",
+                                                 &mc_mb->data, true, threshold,
+                                                 stats->idle_age_ms,
+                                                 since_updated_ms);
+            }
+        } else {
+            mac_cache_mac_binding_update_log("Not updating non-active",
+                                             &mc_mb->data, true, threshold,
+                                             stats->idle_age_ms,
+                                             since_updated_ms);
         }
-
         free(stats);
     }
 
     mac_cache_update_req_delay(thresholds, req_delay);
+    if (*req_delay) {
+        VLOG_DBG("MAC binding statistics dalay: %"PRIu64, *req_delay);
+    }
 }
 
 void
