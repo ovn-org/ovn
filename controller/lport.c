6aed782d7b33c22a63c2d1aedaefcfd6b19431d7
Numan Siddique <numans@ovn.org>
2025-03-13 11:31:50 -0400

    controller: Fix possible NULL ptr access in lport_is_local().
    lport_is_local() after calling lport_lookup_by_name() is not
    checking if the returned port_binding is NULL or not. Its
    better to check for NULL before accessing it.
    
    Fixes: 712fca55b3b1 ("controller: Prioritize host routes.")
    Acked-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- controller/lport.c
+++ controller/lport.c
@@ -99,6 +99,10 @@ lport_is_local(struct ovsdb_idl_index *sbrec_port_binding_by_name,
     const struct sbrec_port_binding *pb = lport_lookup_by_name(
         sbrec_port_binding_by_name, port_name);
 
+    if (!pb) {
+        return false;
+    }
+
     if (lport_pb_is_chassis_resident(chassis, active_tunnels, pb)) {
         return true;
     }
