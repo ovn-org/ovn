ddd2124c39e4193acc30fe58c8c5e082f7a63da9
Ales Musil <amusil@redhat.com>
2025-04-28 12:08:22 +0200

    northd: Prevent SB LB duplicates.
    It can happen that RAFT makes multiple SB LB that are the same.
    Make sure we catch those cases and reconcile the SB DB to remove the
    duplicate LB.
    
    Fixes: d81e7b4f513c ("northd: Use the same UUID for SB representation of Load_Balancer.")
    Reported-at: https://issues.redhat.com/browse/FDP-1351
    Acked-by: Xavier Simonart <xsimonar@redhat.com>
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- northd/en-sync-sb.h
+++ northd/en-sync-sb.h
@@ -21,6 +21,8 @@ void *en_sync_to_sb_lb_init(struct engine_node *, struct engine_arg *);
 void en_sync_to_sb_lb_run(struct engine_node *, void *data);
 void en_sync_to_sb_lb_cleanup(void *data);
 bool sync_to_sb_lb_northd_handler(struct engine_node *, void *data OVS_UNUSED);
+bool sync_to_sb_lb_sb_load_balancer(struct engine_node *,
+                                    void *data OVS_UNUSED);
 
 void *en_sync_to_sb_pb_init(struct engine_node *, struct engine_arg *);
 void en_sync_to_sb_pb_run(struct engine_node *, void *data);
