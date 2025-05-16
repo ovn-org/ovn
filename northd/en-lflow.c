ovn-org/ovn

commit d8a8b3fe6e4c025ae4cca1b7bcc3459705661300

inc-proc-eng: Update return type of change handlers.
Change handlers' return type is a boolean: true if the handler could
handle the changed input data, and false if the handler could not handle
the input data. Unfortunately, a change handler also has a hidden task
it must do: update the node state if its data has been updated.

To make this responsibility more clear, this commit updates the return
type of change handlers to be an enum. This way, the change handler can
indicate if the change cannot be handled, if the change could be handled
but the node data is unchanged, or if the change could be handled and
the node data is updated. The incremental engine uses this return value
to update the node state.

This has a positive side effect as well, as this change now eliminates
the possibility of inputs overwriting state set by previous inputs.
Consider if node X has inputs A and B. A's change handler sets X's state
to EN_UPDATED. Then B's change handler sets X's state to EN_UNCHANGED. B
has overwritten the update that A made and therefore, X appears to be
unchanged, even though its data has been updated. With this commit, this
can no longer happen, since the incremental engine library itself is now
responsible for setting the state based on the change handler results.
The incremental engine will not change a node's state back to
EN_UNCHANGED if a previous change handler has set the state TO
EN_UPDATED.

Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

northd/en-lflow.c
@@ -126,7 +126,7 @@ en_lflow_run(struct engine_node *node, void *data)
     return EN_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_northd_handler(struct engine_node *node,
                      void *data)
 {
@@ -145,20 +145,19 @@ lflow_northd_handler(struct engine_node *node,
                                           &northd_data->trk_data.trk_lsps,
                                           &lflow_input,
                                           lflow_data->lflow_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     if (!lflow_handle_northd_lb_changes(
             eng_ctx->ovnsb_idl_txn, &northd_data->trk_data.trk_lbs,
             &lflow_input, lflow_data->lflow_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_port_group_handler(struct engine_node *node, void *data OVS_UNUSED)
 {
     struct port_group_data *pg_data =
@@ -168,21 +167,20 @@ lflow_port_group_handler(struct engine_node *node, void *data OVS_UNUSED)
      * need to reprocess lflows.  Otherwise, there might be a need to
      * add/delete port-group ACLs to/from switches. */
     if (pg_data->ls_port_groups_sets_changed) {
-        return false;
+        return EN_UNHANDLED;
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_lr_stateful_handler(struct engine_node *node, void *data)
 {
     struct ed_type_lr_stateful *lr_sful_data =
         engine_get_input_data("lr_stateful", node);
 
     if (!lr_stateful_has_tracked_data(&lr_sful_data->trk_data)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     const struct engine_context *eng_ctx = engine_get_context();
@@ -194,21 +192,20 @@ lflow_lr_stateful_handler(struct engine_node *node, void *data)
                                           &lr_sful_data->trk_data,
                                           &lflow_input,
                                           lflow_data->lflow_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_ls_stateful_handler(struct engine_node *node, void *data)
 {
     struct ed_type_ls_stateful *ls_sful_data =
         engine_get_input_data("ls_stateful", node);
 
     if (!ls_stateful_has_tracked_data(&ls_sful_data->trk_data)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     const struct engine_context *eng_ctx = engine_get_context();
@@ -220,14 +217,13 @@ lflow_ls_stateful_handler(struct engine_node *node, void *data)
                                           &ls_sful_data->trk_data,
                                           &lflow_input,
                                           lflow_data->lflow_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_multicast_igmp_handler(struct engine_node *node, void *data)
 {
     struct multicast_igmp_data *mcast_igmp_data =
@@ -246,7 +242,7 @@ lflow_multicast_igmp_handler(struct engine_node *node, void *data)
                                 lflow_input.ovn_internal_version_changed,
                                 lflow_input.sbrec_logical_flow_table,
                                 lflow_input.sbrec_logical_dp_group_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     build_igmp_lflows(&mcast_igmp_data->igmp_groups,
@@ -262,14 +258,13 @@ lflow_multicast_igmp_handler(struct engine_node *node, void *data)
                                lflow_input.ovn_internal_version_changed,
                                lflow_input.sbrec_logical_flow_table,
                                lflow_input.sbrec_logical_dp_group_table)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
-bool
+enum engine_input_handler_result
 lflow_group_ecmp_route_change_handler(struct engine_node *node,
                                       void *data OVS_UNUSED)
 {
@@ -278,7 +273,7 @@ lflow_group_ecmp_route_change_handler(struct engine_node *node,
 
     /* If we do not have tracked data we need to recompute. */
     if (!group_ecmp_route_data->tracked) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     const struct engine_context *eng_ctx = engine_get_context();
@@ -305,7 +300,7 @@ lflow_group_ecmp_route_change_handler(struct engine_node *node,
             lflow_input.sbrec_logical_flow_table,
             lflow_input.sbrec_logical_dp_group_table);
         if (!handled) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
 
@@ -327,12 +322,11 @@ lflow_group_ecmp_route_change_handler(struct engine_node *node,
             lflow_input.sbrec_logical_flow_table,
             lflow_input.sbrec_logical_dp_group_table);
         if (!handled) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
 
-    engine_set_node_state(node, EN_UPDATED);
-    return true;
+    return EN_HANDLED_UPDATED;
 }
 
 void *en_lflow_init(struct engine_node *node OVS_UNUSED,

lib/inc-proc-eng.c
@@ -255,7 +255,8 @@ engine_get_input_data(const char *input_name, struct engine_node *node)
 
 void
 engine_add_input(struct engine_node *node, struct engine_node *input,
-                 bool (*change_handler)(struct engine_node *, void *))
+                 enum engine_input_handler_result (*change_handler)
+                     (struct engine_node *, void *))
 {
     ovs_assert(node->n_inputs < ENGINE_MAX_INPUT);
     node->inputs[node->n_inputs].node = input;
@@ -437,7 +438,8 @@ engine_compute(struct engine_node *node, bool recompute_allowed)
              * the node handler.
              */
             long long int now = time_msec();
-            bool handled = node->inputs[i].change_handler(node, node->data);
+            enum engine_input_handler_result handled;
+            handled = node->inputs[i].change_handler(node, node->data);
             long long int delta_time = time_msec() - now;
             if (delta_time > engine_compute_log_timeout_msec) {
                 static struct vlog_rate_limit rl =
@@ -449,11 +451,17 @@ engine_compute(struct engine_node *node, bool recompute_allowed)
                 VLOG_DBG("node: %s, handler for input %s took %lldms",
                          node->name, node->inputs[i].node->name, delta_time);
             }
-            if (!handled) {
+            if (handled == EN_UNHANDLED) {
                 engine_recompute(node, recompute_allowed,
                                  "failed handler for input %s",
                                  node->inputs[i].node->name);
                 return (node->state != EN_CANCELED);
+            } else if (!engine_node_changed(node)) {
+                /* We only want to update the state if the node is unchanged.
+                 * Otherwise, handlers might change the state from EN_UPDATED
+                 * back to EN_UNCHANGED.
+                 */
+                engine_set_node_state(node, (enum engine_node_state) handled);
             }
         }
     }

lib/inc-proc-eng.h
@@ -170,22 +170,6 @@ struct engine_arg {
 
 struct engine_node;
 
-struct engine_node_input {
-    /* The input node. */
-    struct engine_node *node;
-
-    /* Change handler for changes of the input node. The changes may need to be
-     * evaluated against all the other inputs. Returns:
-     *  - true: if change can be handled
-     *  - false: if change cannot be handled (indicating full recompute needed)
-     * A change handler can also call engine_get_context() but it must make
-     * sure the txn pointers returned by it are non-NULL. In case the change
-     * handler needs to use the txn pointers returned by engine_get_context(),
-     * and the pointers are NULL, the change handler MUST return false.
-     */
-    bool (*change_handler)(struct engine_node *node, void *data);
-};
-
 enum engine_node_state {
     EN_STALE,     /* Data in the node is not up to date with the DB. */
     EN_UPDATED,   /* Data in the node is valid but was updated during the
@@ -198,6 +182,33 @@ enum engine_node_state {
                    * this node.
                    */
     EN_STATE_MAX,
+};
+
+enum engine_input_handler_result {
+    EN_UNHANDLED = -1,
+    EN_HANDLED_UPDATED = EN_UPDATED,
+    EN_HANDLED_UNCHANGED = EN_UNCHANGED,
+};
+
+struct engine_node_input {
+    /* The input node. */
+    struct engine_node *node;
+
+    /* Change handler for changes of the input node. The changes may need to be
+     * evaluated against all the other inputs. Returns:
+     *  - EN_UNHANDLED: the change cannot be handled (indicating full
+     *    recompute needed).
+     *  - EN_HANDLED_UPDATED: the change can be handled, and the node's
+     *    data was updated as a result.
+     *  - EN_HANDLED_UNCHANGED: the change can be handled, and the node's
+     *    data was left unchanged.
+     * A change handler can also call engine_get_context() but it must make
+     * sure the txn pointers returned by it are non-NULL. In case the change
+     * handler needs to use the txn pointers returned by engine_get_context(),
+     * and the pointers are NULL, the change handler MUST return EN_UNHANDLED.
+     */
+    enum engine_input_handler_result (*change_handler)
+        (struct engine_node *node, void *data);
 };
 
 struct engine_stats {
@@ -295,7 +306,8 @@ void *engine_get_input_data(const char *input_name, struct engine_node *);
  * be able to process the change incrementally, and will fall back to call
  * the run method to recompute. */
 void engine_add_input(struct engine_node *node, struct engine_node *input,
-                      bool (*change_handler)(struct engine_node *, void *));
+                      enum engine_input_handler_result (*change_handler)
+                          (struct engine_node *, void *));
 
 /* Force the engine to recompute everything. It is used
  * in circumstances when we are not sure there is change or not, or
@@ -385,10 +397,10 @@ struct ovsdb_idl_index * engine_ovsdb_node_get_index(struct engine_node *,
                                                      const char *name);
 
 /* Any engine node can use this function for no-op handlers. */
-static inline bool
+static inline enum engine_input_handler_result
 engine_noop_handler(struct engine_node *node OVS_UNUSED, void *data OVS_UNUSED)
 {
-    return true;
+    return EN_HANDLED_UNCHANGED;
 }
 
 /* Adds an OVSDB IDL index to the node. This should be called only after

northd/en-acl-ids.h
@@ -6,7 +6,6 @@
 
 #include "lib/inc-proc-eng.h"
 
-bool northd_acl_id_handler(struct engine_node *node, void *data);
 void *en_acl_id_init(struct engine_node *, struct engine_arg *);
 enum engine_node_state en_acl_id_run(struct engine_node *, void *data);
 void en_acl_id_cleanup(void *data);

northd/en-advertised-route-sync.c
@@ -132,7 +132,7 @@ advertised_route_table_sync(
     const struct hmap *dynamic_routes,
     struct advertised_route_sync_data *data);
 
-bool
+enum engine_input_handler_result
 advertised_route_sync_lr_stateful_change_handler(struct engine_node *node,
                                                  void *data_)
 {
@@ -149,21 +149,21 @@ advertised_route_sync_lr_stateful_change_handler(struct engine_node *node,
         lr_stateful_rec = hmapx_node->data;
         if (uuidset_contains(&data->nb_lr,
                              &lr_stateful_rec->nbr_uuid)) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
 
-    return true;
+    return EN_HANDLED_UNCHANGED;
 }
 
-bool
+enum engine_input_handler_result
 advertised_route_sync_northd_change_handler(struct engine_node *node,
                                             void *data_)
 {
     struct advertised_route_sync_data *data = data_;
     struct northd_data *northd_data = engine_get_input_data("northd", node);
     if (!northd_has_tracked_data(&northd_data->trk_data)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     /* We indirectly use northd_data->ls_ports if we announce host routes.
@@ -175,25 +175,25 @@ advertised_route_sync_northd_change_handler(struct engine_node *node,
         op = hmapx_node->data;
         if (uuidset_contains(&data->nb_ls,
                              &op->od->nbs->header_.uuid)) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
     HMAPX_FOR_EACH (hmapx_node, &northd_data->trk_data.trk_lsps.updated) {
         op = hmapx_node->data;
         if (uuidset_contains(&data->nb_ls,
                              &op->od->nbs->header_.uuid)) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
     HMAPX_FOR_EACH (hmapx_node, &northd_data->trk_data.trk_lsps.deleted) {
         op = hmapx_node->data;
         if (uuidset_contains(&data->nb_ls,
                              &op->od->nbs->header_.uuid)) {
-            return false;
+            return EN_UNHANDLED;
         }
     }
 
-    return true;
+    return EN_HANDLED_UNCHANGED;
 }
 
 static void

northd/en-advertised-route-sync.h
@@ -28,9 +28,11 @@ struct advertised_route_sync_data {
   struct uuidset nb_ls;
 };
 
-bool advertised_route_sync_lr_stateful_change_handler(struct engine_node *,
-                                                      void *data);
-bool advertised_route_sync_northd_change_handler(struct engine_node *,
+enum engine_input_handler_result
+advertised_route_sync_lr_stateful_change_handler(struct engine_node *,
+                                                 void *data);
+enum engine_input_handler_result
+advertised_route_sync_northd_change_handler(struct engine_node *,
                                                  void *data);
 void *en_advertised_route_sync_init(struct engine_node *, struct engine_arg *);
 void en_advertised_route_sync_cleanup(void *data);

northd/en-ecmp-nexthop.c
@@ -149,7 +149,7 @@ ecmp_nexthop_lookup(struct ovsdb_idl_index *sbrec_ecmp_by_nexthop,
     return retval;
 }
 
-bool
+enum engine_input_handler_result
 ecmp_nexthop_mac_binding_handler(struct engine_node *node,
                                  void *data OVS_UNUSED)
 {
@@ -179,7 +179,7 @@ ecmp_nexthop_mac_binding_handler(struct engine_node *node,
         }
     }
 
-    return true;
+    return EN_HANDLED_UNCHANGED;
 }
 
 void *

commit 0baaa026eccb0b9f200bad7058e51dbc81ed9ba6

en-lflow: Return correct type in northd_handler.
A recent refactor changed the return type of input node handlers to no
longer return a boolean. This line was missed during the refactor and
apparently caused no testsuite issues.

Fixes: d8a8b3fe6e4c ("inc-proc-eng: Update return type of change handlers.")
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

northd/en-lflow.c
@@ -132,7 +132,7 @@ lflow_northd_handler(struct engine_node *node,
 {
     struct northd_data *northd_data = engine_get_input_data("northd", node);
     if (!northd_has_tracked_data(&northd_data->trk_data)) {
-        return false;
+        return EN_UNHANDLED;
     }
 
     const struct engine_context *eng_ctx = engine_get_context();