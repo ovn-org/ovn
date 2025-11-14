1f6866060cbfd5a2bf2e8523f6e5e50b2ea2e6c7
Felix Moebius <felix.moebius@mail.schwarz>
2025-11-14 12:19:01 +0100

    ofctrl: Prevent duplicate desired flow references.
    Address sets may contain duplicate values such as "::1" and "::01" or
    "1.1.1.1" and "1.1.1.1/32", which produce the same desired flow.
    When adding such a duplicate to an existing address set, an lflow using
    the address set that is updated incrementally will produce the same
    desired flow that it produced for the original value, find that this
    desired flow already exist and link the lflow to the desired flow again.
    This currently assumes that the existing desired flow was produced by
    another lflow, which is not the case here and results in a second
    reference from the lflow to the desired flow.
    
    This then leads to a crash later on when removing or recomputing the
    lflow and going through remove_flows_from_sb_to_flow by tripping over
    the ovs_assert(ovs_list_is_empty(&f->list_node)) as the code there does
    not anticipate seeing the same desired flow twice for one lflow.
    
    Fix this by detecting and preventing the duplicate reference. Also log
    a warning as this likely indicates a problem somewhere else.
    
    Fixes: 7c0b538762f6 ("ovn-controller: Handle addresses addition in address set incrementally.")
    Signed-off-by: Felix Moebius <felix.moebius@mail.schwarz>
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- controller/ofctrl.c
+++ controller/ofctrl.c
@@ -1402,11 +1402,22 @@ ofctrl_add_or_append_conj_flow(struct ovn_desired_flow_table *desired_flows,
          * actions properly when handle addrset ip deletion, instead of simply
          * delete the flow. */
         struct sb_flow_ref *sfr;
+        bool duplicate = false;
         LIST_FOR_EACH (sfr, sb_list, &f->references) {
+            duplicate |= uuid_equals(&sfr->sb_uuid, sb_uuid);
             ovs_list_remove(&sfr->as_ip_flow_list);
             ovs_list_init(&sfr->as_ip_flow_list);
         }
-        link_flow_to_sb(desired_flows, f, sb_uuid, NULL);
+
+        if (duplicate) {
+            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+            char *s = ovn_flow_to_string(&f->flow);
+            VLOG_WARN_RL(&rl, "lflow "UUID_FMT" already references desired "
+                         "flow: %s", UUID_ARGS(sb_uuid), s);
+            free(s);
+        } else {
+            link_flow_to_sb(desired_flows, f, sb_uuid, NULL);
+        }
     } else {
         actions = create_conjunction_actions(conjunctions, NULL);
         f = desired_flow_alloc(table_id, priority, 0, match, actions,
