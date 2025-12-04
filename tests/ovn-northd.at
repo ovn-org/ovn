ovn-org/ovn

commit 50a483086d80029ec989ca673eae2b56acb4f377

northd: Add IP for new logical switches in en-datapath-logical-switch node.
Introduce Incremental Processing logic for en-datapath-logical-switch
node.

Reported-at: https://issues.redhat.com/browse/FDP-1519
Acked-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-datapath-logical-switch.c
@@ -38,6 +38,140 @@ en_datapath_logical_switch_init(struct engine_node *node OVS_UNUSED,
     return map;
 }
 
+static uint32_t
+get_requested_tunnel_key(const struct nbrec_logical_switch *nbs,
+                         bool vxlan_mode)
+{
+    uint32_t requested_tunnel_key = smap_get_int(&nbs->other_config,
+                                                 "requested-tnl-key", 0);
+    const char *ts = smap_get(&nbs->other_config, "interconn-ts");
+
+    if (!ts && vxlan_mode && requested_tunnel_key >= 1 << 12) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+        VLOG_WARN_RL(&rl, "Tunnel key %"PRIu32" for datapath %s is "
+                     "incompatible with VXLAN", requested_tunnel_key,
+                     nbs->name);
+        requested_tunnel_key = 0;
+    }
+
+    return requested_tunnel_key;
+}
+
+static void
+gather_external_ids(const struct nbrec_logical_switch *nbs,
+                    struct smap *external_ids)
+{
+    smap_add(external_ids, "name", nbs->name);
+    const char *neutron_network = smap_get(&nbs->other_config,
+                                           "neutron:network_name");
+    if (neutron_network && neutron_network[0]) {
+        smap_add(external_ids, "name2", neutron_network);
+    }
+
+    int64_t ct_zone_limit = ovn_smap_get_llong(&nbs->other_config,
+                                               "ct-zone-limit", -1);
+    if (ct_zone_limit > 0) {
+        smap_add_format(external_ids, "ct-zone-limit", "%"PRId64,
+                        ct_zone_limit);
+    }
+
+    const char *ts = smap_get(&nbs->other_config, "interconn-ts");
+    if (ts) {
+        smap_add(external_ids, "interconn-ts", ts);
+    }
+
+    uint32_t age_threshold = smap_get_uint(&nbs->other_config,
+                                           "fdb_age_threshold", 0);
+    if (age_threshold) {
+        smap_add_format(external_ids, "fdb_age_threshold",
+                        "%u", age_threshold);
+    }
+
+    /* For backwards-compatibility, also store the NB UUID in
+     * external-ids:logical-switch. This is useful if ovn-controller
+     * has not updated and expects this to be where to find the
+     * UUID.
+     */
+    smap_add_format(external_ids, "logical-switch", UUID_FMT,
+                    UUID_ARGS(&nbs->header_.uuid));
+}
+
+static struct ovn_unsynced_datapath *
+datapath_unsynced_new_logical_switch_handler(
+        const struct nbrec_logical_switch *nbs,
+        const struct ed_type_global_config *global_config,
+        struct ovn_unsynced_datapath_map *map)
+{
+    uint32_t requested_tunnel_key = get_requested_tunnel_key(
+        nbs, global_config->vxlan_mode);
+
+    struct ovn_unsynced_datapath *udp =
+        ovn_unsynced_datapath_alloc(nbs->name, DP_SWITCH,
+                                    requested_tunnel_key, &nbs->header_);
+
+    gather_external_ids(nbs, &udp->external_ids);
+    hmap_insert(&map->dps, &udp->hmap_node, uuid_hash(&nbs->header_.uuid));
+    return udp;
+}
+
+enum engine_input_handler_result
+datapath_logical_switch_handler(struct engine_node *node, void *data)
+{
+    const struct nbrec_logical_switch_table *nb_ls_table =
+        EN_OVSDB_GET(engine_get_input("NB_logical_switch", node));
+    const struct ed_type_global_config *global_config =
+        engine_get_input_data("global_config", node);
+    struct ovn_unsynced_datapath_map *map = data;
+
+    const struct nbrec_logical_switch *nbs;
+    NBREC_LOGICAL_SWITCH_TABLE_FOR_EACH_TRACKED (nbs, nb_ls_table) {
+        struct ovn_unsynced_datapath *udp =
+            ovn_unsynced_datapath_find(map, &nbs->header_.uuid);
+
+        if (nbrec_logical_switch_is_new(nbs)) {
+            if (udp) {
+                return EN_UNHANDLED;
+            }
+            udp = datapath_unsynced_new_logical_switch_handler(nbs,
+                                                               global_config,
+                                                               map);
+            hmapx_add(&map->new, udp);
+        } else if (nbrec_logical_switch_is_deleted(nbs)) {
+            if (!udp) {
+                return EN_UNHANDLED;
+            }
+            hmap_remove(&map->dps, &udp->hmap_node);
+            hmapx_add(&map->deleted, udp);
+        } else {
+            if (!udp) {
+                return EN_UNHANDLED;
+            }
+
+            udp->requested_tunnel_key = get_requested_tunnel_key(
+                    nbs, global_config->vxlan_mode);
+            smap_destroy(&udp->external_ids);
+            smap_init(&udp->external_ids);
+            gather_external_ids(nbs, &udp->external_ids);
+            hmapx_add(&map->updated, udp);
+        }
+    }
+
+    if (!(hmapx_is_empty(&map->new) &&
+          hmapx_is_empty(&map->updated) &&
+          hmapx_is_empty(&map->deleted))) {
+        return EN_HANDLED_UPDATED;
+    }
+
+    return EN_HANDLED_UNCHANGED;
+}
+
+void
+en_datapath_logical_switch_clear_tracked_data(void *data)
+{
+    struct ovn_unsynced_datapath_map *map = data;
+    ovn_unsynced_datapath_map_clear_tracked_data(map);
+}
+
 enum engine_node_state
 en_datapath_logical_switch_run(struct engine_node *node , void *data)
 {
@@ -53,57 +187,7 @@ en_datapath_logical_switch_run(struct engine_node *node , void *data)
 
     const struct nbrec_logical_switch *nbs;
     NBREC_LOGICAL_SWITCH_TABLE_FOR_EACH (nbs, nb_ls_table) {
-        uint32_t requested_tunnel_key = smap_get_int(&nbs->other_config,
-                                                     "requested-tnl-key", 0);
-        const char *ts = smap_get(&nbs->other_config, "interconn-ts");
-
-        if (!ts && global_config->vxlan_mode &&
-            requested_tunnel_key >= 1 << 12) {
-            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
-            VLOG_WARN_RL(&rl, "Tunnel key %"PRIu32" for datapath %s is "
-                         "incompatible with VXLAN", requested_tunnel_key,
-                         nbs->name);
-            requested_tunnel_key = 0;
-        }
-
-        struct ovn_unsynced_datapath *dp
-            = ovn_unsynced_datapath_alloc(nbs->name, DP_SWITCH,
-                                          requested_tunnel_key, &nbs->header_);
-
-        smap_add(&dp->external_ids, "name", dp->name);
-        const char *neutron_network = smap_get(&nbs->other_config,
-                                               "neutron:network_name");
-        if (neutron_network && neutron_network[0]) {
-            smap_add(&dp->external_ids, "name2", neutron_network);
-        }
-
-        int64_t ct_zone_limit = ovn_smap_get_llong(&nbs->other_config,
-                                                   "ct-zone-limit", -1);
-        if (ct_zone_limit > 0) {
-            smap_add_format(&dp->external_ids, "ct-zone-limit", "%"PRId64,
-                            ct_zone_limit);
-        }
-
-        if (ts) {
-            smap_add(&dp->external_ids, "interconn-ts", ts);
-        }
-
-        uint32_t age_threshold = smap_get_uint(&nbs->other_config,
-                                               "fdb_age_threshold", 0);
-        if (age_threshold) {
-            smap_add_format(&dp->external_ids, "fdb_age_threshold",
-                            "%u", age_threshold);
-        }
-
-        /* For backwards-compatibility, also store the NB UUID in
-         * external-ids:logical-switch. This is useful if ovn-controller
-         * has not updated and expects this to be where to find the
-         * UUID.
-         */
-        smap_add_format(&dp->external_ids, "logical-switch", UUID_FMT,
-                        UUID_ARGS(&nbs->header_.uuid));
-
-        hmap_insert(&map->dps, &dp->hmap_node, uuid_hash(&nbs->header_.uuid));
+        datapath_unsynced_new_logical_switch_handler(nbs, global_config, map);
     }
 
     return EN_UPDATED;

tests/ovn-northd.at
@@ -17666,3 +17666,31 @@ CHECK_NO_CHANGE_AFTER_RECOMPUTE
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Datapath Logical switch incremental processing])
+ovn_start
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb ls-add sw0
+check_engine_stats datapath_logical_switch norecompute compute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb set logical_switch sw0 other_config:fdb_age_threshold=5
+check_engine_stats datapath_logical_switch norecompute compute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb set logical_switch sw0 other_config:requested-tnl-key=123
+check_engine_stats datapath_logical_switch norecompute compute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb ls-del sw0
+check_engine_stats datapath_logical_switch norecompute compute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+AT_CLEANUP
+])
+

northd/en-datapath-logical-switch.h
@@ -23,9 +23,12 @@
 void *en_datapath_logical_switch_init(struct engine_node *,
                                       struct engine_arg *);
 
+enum engine_input_handler_result
+datapath_logical_switch_handler(struct engine_node *, void *data);
 enum engine_node_state en_datapath_logical_switch_run(struct engine_node *,
                                                       void *data);
 void en_datapath_logical_switch_cleanup(void *data);
+void en_datapath_logical_switch_clear_tracked_data(void *data);
 
 struct ovn_synced_logical_switch {
     struct hmap_node hmap_node;

northd/inc-proc-northd.c
@@ -183,7 +183,7 @@ static ENGINE_NODE(learned_route_sync, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(dynamic_routes);
 static ENGINE_NODE(group_ecmp_route, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(datapath_logical_router, CLEAR_TRACKED_DATA);
-static ENGINE_NODE(datapath_logical_switch);
+static ENGINE_NODE(datapath_logical_switch, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(datapath_sync);
 static ENGINE_NODE(datapath_synced_logical_router);
 static ENGINE_NODE(datapath_synced_logical_switch);
@@ -217,8 +217,10 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_acl_id, &en_nb_acl, NULL);
     engine_add_input(&en_acl_id, &en_sb_acl_id, NULL);
 
-    engine_add_input(&en_datapath_logical_switch, &en_nb_logical_switch, NULL);
-    engine_add_input(&en_datapath_logical_switch, &en_global_config, NULL);
+    engine_add_input(&en_datapath_logical_switch, &en_nb_logical_switch,
+                     datapath_logical_switch_handler);
+    engine_add_input(&en_datapath_logical_switch, &en_global_config,
+                     datapath_logical_switch_handler);
 
     engine_add_input(&en_datapath_logical_router, &en_nb_logical_router,
                      en_datapath_logical_router_logical_router_handler);

commit cde6035830b80a4466ac92683b78630c8f709de7

datapaths: Handle simultaneous "new" and "deleted" datapaths.
If a logical switch or logical router is added and removed very quickly,
then it is possible for the IDL to place it in both the "new" and
"deleted" tracked maps.

This was not being accounted for in the logical switch and logical
router handlers.

For logical routers, this was not a huge issue. We would detect the
logical router as deleted, then not be able to find the in-memory
unsynced datapath. This would result in a recompute. Adding the check
for this specific situation allows us to avoid an unnecessary recompute.

For logical switches, on the other hand, this was a much worse
situation. The code first checks if the logical switch is new. In this
case, the added and deleted logical switch matched this check, so we
treated it as a new logical switch. This "new" logical switch would then
get propagated to all other engine nodes. If this logical switch
happened to reference other database rows that also had been deleted,
this could cause chaos. One place in particular where this was observed
was in the en_lb_data node. If the logical switch referenced a load
balancer group that was deleted, then we would try to look up the load
balancer group in our in-memory map of load balancer groups. However,
since the actual database row had been deleted, it no longer existed in
our in-memory map. This resulted in an assertion being triggered and
crashing ovn-northd.

Added and deleted logical switches could also trigger a crash in the
datapath syncing code as well. Since the logical switch was deleted, its
memory could be freed by the IDL. This could cause a crash at any point
in its processing.

The fix here is to detect the situation where the IDL reports the
logical router or logical switch as both new and deleted. In this case,
we treat it as a no-op and continue. This avoids both the recompute for
logical routers, and the crashes with logical switches.

Fixes: 50a483086d80 ("northd: Add IP for new logical switches in en-datapath-logical-switch node.")

northd/en-datapath-logical-router.c
@@ -164,6 +164,13 @@ en_datapath_logical_router_logical_router_handler(struct engine_node *node,
     struct ovn_unsynced_datapath *udp;
     const struct nbrec_logical_router *nbr;
     NBREC_LOGICAL_ROUTER_TABLE_FOR_EACH_TRACKED (nbr, nb_lr_table) {
+        /* If the logical router is added and removed within the same
+         * transaction, then this is a no-op
+         */
+        if (nbrec_logical_router_is_new(nbr) &&
+            nbrec_logical_router_is_deleted(nbr)) {
+            continue;
+        }
         udp = ovn_unsynced_datapath_find(map, &nbr->header_.uuid);
 
         if (nbrec_logical_router_is_deleted(nbr) && !udp) {

northd/en-datapath-logical-switch.c
@@ -143,6 +143,13 @@ datapath_logical_switch_handler(struct engine_node *node, void *data)
 
     const struct nbrec_logical_switch *nbs;
     NBREC_LOGICAL_SWITCH_TABLE_FOR_EACH_TRACKED (nbs, nb_ls_table) {
+        /* If the logical switch is added and removed within the same
+         * transaction, then this is a no-op
+         */
+        if (nbrec_logical_switch_is_new(nbs) &&
+            nbrec_logical_switch_is_deleted(nbs)) {
+            continue;
+        }
         struct ovn_unsynced_datapath *udp =
             ovn_unsynced_datapath_find(map, &nbs->header_.uuid);
 

tests/ovn-northd.at
@@ -18914,3 +18914,34 @@ wait_row_count Igmp_Group 0 address=mrouters
 OVN_CLEANUP_NORTHD
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Added and Deleted Logical Switch])
+ovn_start
+
+# For this test, we will pause northd. While northd is paused, we will
+# add a logical switch, a load balancer, and a load balancer group. The load
+# balancer will be in the load balancer group, and the logical switch will use
+# the load balancer group. We will then delete the logical switch, the load
+# balancer, and the load balancer group. We then will unpause ovn-northd.
+#
+# The test only fails if putting northd to sleep or waking it up fails due to
+# ovn-northd crashing.
+#
+# We run this a few times in a loop just to be certain we aren't causing any
+# catastrophic issues.
+for i in $(seq 5); do
+    sleep_northd
+    check ovn-nbctl ls-add ls1
+    check ovn-nbctl lb-add lb1 192.168.0.1 10.0.0.1
+    lb_uuid=$(fetch_column nb:Load_Balancer _uuid name=lb1)
+    lbg_uuid=$(ovn-nbctl create load_balancer_group name=lbg1 load_balancer=$lb_uuid)
+    check ovn-nbctl set logical_switch ls1 load_balancer_group=$lbg_uuid
+    check ovn-nbctl ls-del ls1
+    check ovn-nbctl lb-del lb1
+    check ovn-nbctl --all destroy load_balancer_group
+    wake_up_northd
+done
+OVN_CLEANUP_NORTHD
+AT_CLEANUP
+])