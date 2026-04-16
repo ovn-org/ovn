ovn-org/ovn

commit e3a398e9146eb889a339fd9d75cf86a94093782b

controller: Add ipv6 prefix delegation state machine
Introduce IPv6 Prefix delegation state machine according to RFC 3633
https://tools.ietf.org/html/rfc3633.
Add handle_dhcpv6_reply controller action to parse advertise/reply from
IPv6 delegation server. Advertise/reply are parsed running respectively:
- pinctrl_parse_dhcv6_advt
- pinctrl_parse_dhcv6_reply
The IPv6 requesting router starts sending dhcpv6 solicit through the logical
router port marked with ipv6_prefix_delegation set to true.
An IPv6 prefix will be requested for each logical router port marked
with "prefix" set to true in option column of logical router port table.
Save IPv6 prefix received by IPv6 delegation router in the options columns of
SB port binding table in order to be reused by Router Advertisement framework
run by ovn logical router pipeline.
IPv6 Prefix delegation state machine is enabled on Gateway Router or on
a Gateway Router Port

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

lib/ovn-l7.h
@@ -178,8 +178,11 @@ struct dhcp_opt6_header {
 #define DHCPV6_OPT_SERVER_ID_CODE        2
 #define DHCPV6_OPT_IA_NA_CODE            3
 #define DHCPV6_OPT_IA_ADDR_CODE          5
+#define DHCPV6_OPT_STATUS_CODE           13
 #define DHCPV6_OPT_DNS_SERVER_CODE       23
 #define DHCPV6_OPT_DOMAIN_SEARCH_CODE    24
+#define DHCPV6_OPT_IA_PD                 25
+#define DHCPV6_OPT_IA_PREFIX             26
 
 #define DHCPV6_OPT_SERVER_ID \
     DHCP_OPTION("server_id", DHCPV6_OPT_SERVER_ID_CODE, "mac")
@@ -257,6 +260,22 @@ struct ovs_nd_route_info {
     ovs_be32 route_lifetime;
 };
 BUILD_ASSERT_DECL(ND_ROUTE_INFO_OPT_LEN == sizeof(struct ovs_nd_route_info));
+
+OVS_PACKED(
+struct dhcpv6_opt_ia_prefix {
+    struct dhcpv6_opt_header opt;
+    ovs_be32 plife_time;
+    ovs_be32 vlife_time;
+    uint8_t plen;
+    struct in6_addr ipv6;
+});
+
+OVS_PACKED(
+struct dhcpv6_opt_status {
+    struct dhcpv6_opt_header opt;
+    ovs_be16 status_code;
+    uint8_t msg[];
+});
 
 #define DHCPV6_DUID_LL      3
 #define DHCPV6_HW_TYPE_ETH  1

include/ovn/actions.h
@@ -91,7 +91,8 @@ struct ovn_extend_table;
     OVNACT(TRIGGER_EVENT,     ovnact_controller_event) \
     OVNACT(BIND_VPORT,        ovnact_bind_vport)       \
     OVNACT(HANDLE_SVC_CHECK,  ovnact_handle_svc_check) \
-    OVNACT(FWD_GROUP,         ovnact_fwd_group)
+    OVNACT(FWD_GROUP,         ovnact_fwd_group)       \
+    OVNACT(DHCP6_REPLY,       ovnact_null)
 
 /* enum ovnact_type, with a member OVNACT_<ENUM> for each action. */
 enum OVS_PACKED_ENUM ovnact_type {
@@ -584,6 +585,11 @@ enum action_opcode {
      *     MFF_LOG_INPORT = port
      */
     ACTION_OPCODE_HANDLE_SVC_CHECK,
+    /* handle_dhcpv6_reply { ...actions ...}."
+     *
+     *  The actions, in OpenFlow 1.3 format, follow the action_header.
+     */
+    ACTION_OPCODE_DHCP6_SERVER,
 };
 
 /* Header. */

lib/actions.c
@@ -2362,6 +2362,20 @@ ovnact_put_opts_free(struct ovnact_put_opts *pdo)
 }
 
 static void
+format_DHCP6_REPLY(const struct ovnact_null *a OVS_UNUSED, struct ds *s)
+{
+    ds_put_cstr(s, "handle_dhcpv6_reply;");
+}
+
+static void
+encode_DHCP6_REPLY(const struct ovnact_null *a OVS_UNUSED,
+                   const struct ovnact_encode_params *ep OVS_UNUSED,
+                   struct ofpbuf *ofpacts)
+{
+    encode_controller_op(ACTION_OPCODE_DHCP6_SERVER, ofpacts);
+}
+
+static void
 parse_SET_QUEUE(struct action_context *ctx)
 {
     int queue_id;
@@ -3308,6 +3322,8 @@ parse_action(struct action_context *ctx)
         parse_handle_svc_check(ctx);
     } else if (lexer_match_id(ctx->lexer, "fwd_group")) {
         parse_fwd_group_action(ctx);
+    } else if (lexer_match_id(ctx->lexer, "handle_dhcpv6_reply")) {
+        ovnact_put_DHCP6_REPLY(ctx->ovnacts);
     } else {
         lexer_syntax_error(ctx->lexer, "expecting action");
     }

ovn-sb.xml
@@ -2149,6 +2149,17 @@ tcp.flags = RST;
           <p><b>Example:</b> <code>handle_svc_check(inport);</code></p>
         </dd>
 
+        <dt><code>handle_dhcpv6_reply;</code></dt>
+        <dd>
+          <p>
+            Handle DHCPv6 prefix delegation advertisements/replies from
+            a IPv6 delegation server. <code>ovn-controller</code> will
+            add an entry <code>ipv6_ra_pd_list</code> in the
+            <ref table="Port_Binding" column="options"/> table for each
+            prefix received from the delegation server
+          </p>
+        </dd>
+
         <dt><code><var>R</var> = select(<var>N1</var>[=<var>W1</var>], <var>N2</var>[=<var>W2</var>], ...);</code></dt>
         <dd>
           <p>
@@ -2182,6 +2193,13 @@ tcp.flags = RST;
           </p>
         </dd>
 
+        <dt><code>handle_dhcpv6_reply;</code></dt>
+        <dd>
+          <p>
+            This action is used to parse DHCPv6 replies from IPv6
+            Delegation Router and managed IPv6 Prefix delegation state machine
+          </p>
+        </dd>
       </dl>
     </column>
 

tests/ovn.at
@@ -1554,6 +1554,10 @@ fwd_group();
 fwd_group(liveness="false", childports="eth0", "lsp1");
     Syntax error at `"false"' expecting `,'.
 
+# prefix delegation
+handle_dhcpv6_reply;
+    encodes as controller(userdata=00.00.00.13.00.00.00.00)
+
 # Miscellaneous negative tests.
 ;
     Syntax error at `;'.

utilities/ovn-trace.c
@@ -2292,6 +2292,8 @@ trace_actions(const struct ovnact *ovnacts, size_t ovnacts_len,
 
         case OVNACT_FWD_GROUP:
             break;
+        case OVNACT_DHCP6_REPLY:
+            break;
         }
     }
     ds_destroy(&s);

commit 0e3d1d908b765ca29c6744f22005c60a6f72b76e

pinctrl: Unify handling of DHCPv6 options.
Unify the handling of DHCPv6 options. This is addressing several
problems that were present in the DHCPv6 handling:

1) There were inconsistent length checks for the packet length. It
   would be possible to craft a packet that had an option header
   without any data which could lead to a crash because we would
   attempt to deref data after the packet buffer.

2) We could end up reading data after the packet buffer. This could
   happen when the option header would lie about the data length in
   the option.

3) Unbounded strcmp for a string created by user.

4) The parsing was inconsistent and very hard to read.

Make sure the parsing is done using helpers that should prevent
the mentioned issues.

Fixes: e3a398e9146e ("controller: Add ipv6 prefix delegation state machine")
Fixes: 32fc42fdbb20 ("ovn-controller: Add 'put_dhcpv6_opts' action in ovn-controller")
Fixes: c5fd51bd1541 ("Introduce IPv6 iPXE chainload support")
Fixes: b3ae86a15e81 ("northd, controller: Add support for DHCPv6 FQDN option")
Reported-by: Seiji Sakurai <Seiji.Sakurai@outlook.com>
Co-authored-by: Seiji Sakurai <Seiji.Sakurai@outlook.com>
Acked-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Seiji Sakurai <Seiji.Sakurai@outlook.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/pinctrl.c
@@ -822,32 +822,100 @@ pinctrl_find_prefixd_state(const struct flow *ip_flow, unsigned aid)
     return NULL;
 }
 
+static const struct dhcpv6_opt_header *
+next_dhcpv6_opt(const uint8_t *data, size_t opts_len,
+                size_t *len, size_t *opt_len)
+{
+    size_t len_inner = *len + sizeof(struct dhcpv6_opt_header);
+    if (len_inner > opts_len) {
+        return NULL;
+    }
+
+    const struct dhcpv6_opt_header *hdr =
+        (const struct dhcpv6_opt_header *) (data + *len);
+    len_inner += ntohs(hdr->len);
+    if (len_inner > opts_len) {
+        return NULL;
+    }
+
+    *len = len_inner;
+    *opt_len = sizeof *hdr + ntohs(hdr->len);
+    return hdr;
+}
+
+static void
+dhcpv6_opt_ia_na_parse_inner(const struct dhcpv6_opt_ia_na *ia_na,
+                             size_t opts_len,
+                             struct dhcpv6_opt_ia_prefix **ia_prefix,
+                             struct dhcpv6_opt_status **status)
+{
+    /* Check if there are at least some data. */
+    opts_len -= sizeof *ia_na;
+    if (!opts_len) {
+        return;
+    }
+
+    const uint8_t *opts_data = (uint8_t *) ia_na + sizeof *ia_na;
+    size_t len = 0, opt_len = 0;
+
+    const struct dhcpv6_opt_header *in_opt;
+    for (in_opt = next_dhcpv6_opt(opts_data, opts_len, &len, &opt_len);
+         in_opt;
+         in_opt = next_dhcpv6_opt(opts_data, opts_len,
+                                  &len, &opt_len)) {
+        switch (ntohs(in_opt->code)) {
+        case DHCPV6_OPT_IA_PREFIX: {
+            /* Consider only the first found option. */
+            if (*ia_prefix) {
+                break;
+            }
+
+            if (opt_len < sizeof(struct dhcpv6_opt_ia_prefix)) {
+                break;
+            }
+
+            *ia_prefix = (struct dhcpv6_opt_ia_prefix *) in_opt;
+            break;
+        }
+        case DHCPV6_OPT_STATUS_CODE: {
+            /* Consider only the first found option. */
+            if (*status) {
+                break;
+            }
+
+            if (opt_len < sizeof(struct dhcpv6_opt_status)) {
+                break;
+            }
+
+            *status = (struct dhcpv6_opt_status *) in_opt;
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
+
 static void
 pinctrl_parse_dhcpv6_advt(struct rconn *swconn, const struct flow *ip_flow,
-                          struct dp_packet *pkt_in, const struct match *md)
+                          const void *opts_data, size_t opts_len,
+                          const struct match *md)
 {
-    struct udp_header *udp_in = dp_packet_l4(pkt_in);
-    size_t dlen = MIN(ntohs(udp_in->udp_len), dp_packet_l4_size(pkt_in));
-    unsigned char *in_dhcpv6_data = (unsigned char *)(udp_in + 1);
-    uint8_t *data, *end = (uint8_t *)udp_in + dlen;
-    int len = 0, aid = 0;
+    size_t len = 0, opt_len = 0, data_len = 0;
+    int aid = 0;
 
-    data = xmalloc(dlen);
-    /* skip DHCPv6 common header */
-    in_dhcpv6_data += 4;
-    while (in_dhcpv6_data < end) {
-        struct dhcpv6_opt_header *in_opt =
-             (struct dhcpv6_opt_header *)in_dhcpv6_data;
-        int opt_len = sizeof *in_opt + ntohs(in_opt->len);
+    uint8_t *data = xmalloc(opts_len);
 
-        if (dlen < opt_len + len) {
-            goto out;
-        }
-
+    const struct dhcpv6_opt_header *in_opt;
+    for (in_opt = next_dhcpv6_opt(opts_data, opts_len, &len, &opt_len);
+         in_opt;
+         in_opt = next_dhcpv6_opt(opts_data, opts_len, &len, &opt_len)) {
         switch (ntohs(in_opt->code)) {
         case DHCPV6_OPT_IA_PD: {
+            if (opt_len < sizeof(struct dhcpv6_opt_ia_na)) {
+                break;
+            }
             struct dhcpv6_opt_ia_na *ia_na = (struct dhcpv6_opt_ia_na *)in_opt;
-            int orig_len = len, hdr_len = 0, size = sizeof *in_opt + 12;
             uint32_t t1 = ntohl(ia_na->t1), t2 = ntohl(ia_na->t2);
 
             if (t1 > t2 && t2 > 0) {
@@ -855,55 +923,49 @@ pinctrl_parse_dhcpv6_advt(struct rconn *swconn, const struct flow *ip_flow,
             }
 
             aid = ntohl(ia_na->iaid);
-            memcpy(&data[len], in_opt, size);
-            in_opt = (struct dhcpv6_opt_header *)(in_dhcpv6_data + size);
-            len += size;
 
-            while (size < opt_len) {
-                int flen = sizeof *in_opt + ntohs(in_opt->len);
+            memcpy(data + data_len, in_opt, sizeof *ia_na);
+            size_t ia_na_offset = data_len;
+            data_len += sizeof *ia_na;
 
-                if (dlen < flen + len) {
+            struct dhcpv6_opt_ia_prefix *ia_prefix = NULL;
+            struct dhcpv6_opt_status *status = NULL;
+            dhcpv6_opt_ia_na_parse_inner(ia_na, opt_len, &ia_prefix, &status);
+
+            if (ia_prefix) {
+                uint32_t plife_time = ntohl(ia_prefix->plife_time);
+                uint32_t vlife_time = ntohl(ia_prefix->vlife_time);
+
+                if (plife_time > vlife_time) {
                     goto out;
                 }
 
-                if (ntohs(in_opt->code) == DHCPV6_OPT_IA_PREFIX) {
-                    struct dhcpv6_opt_ia_prefix *ia_hdr =
-                        (struct dhcpv6_opt_ia_prefix *)in_opt;
-                    uint32_t plife_time = ntohl(ia_hdr->plife_time);
-                    uint32_t vlife_time = ntohl(ia_hdr->vlife_time);
-
-                    if (plife_time > vlife_time) {
-                        goto out;
-                    }
-
-                    memcpy(&data[len], in_opt, flen);
-                    hdr_len += flen;
-                    len += flen;
-                }
-                if (ntohs(in_opt->code) == DHCPV6_OPT_STATUS_CODE) {
-                   struct dhcpv6_opt_status *status;
-
-                   status = (struct dhcpv6_opt_status *)in_opt;
-                   if (ntohs(status->status_code)) {
-                       goto out;
-                   }
-                }
-                size += flen;
-                in_opt = (struct dhcpv6_opt_header *)(in_dhcpv6_data + size);
+                memcpy(data + data_len, ia_prefix, sizeof *ia_prefix);
+                data_len += sizeof *ia_prefix;
             }
-            in_opt = (struct dhcpv6_opt_header *)&data[orig_len];
-            in_opt->len = htons(hdr_len + 12);
+
+            if (status) {
+                if (ntohs(status->status_code)) {
+                    goto out;
+                }
+            }
+
+            /* Adjust the copied IA NA option header len. */
+            struct dhcpv6_opt_header *copied_hdr =
+                (struct dhcpv6_opt_header *) (data + ia_na_offset);
+            copied_hdr->len = htons(sizeof *ia_na - sizeof *copied_hdr +
+                                    (ia_prefix ? sizeof *ia_prefix : 0));
+
             break;
         }
         case DHCPV6_OPT_SERVER_ID_CODE:
         case DHCPV6_OPT_CLIENT_ID_CODE:
-            memcpy(&data[len], in_opt, opt_len);
-            len += opt_len;
+            memcpy(data + data_len, in_opt, opt_len);
+            data_len += opt_len;
             break;
         default:
             break;
         }
-        in_dhcpv6_data += opt_len;
     }
 
     struct ipv6_prefixd_state *pfd = pinctrl_find_prefixd_state(ip_flow, aid);
@@ -931,14 +993,14 @@ pinctrl_parse_dhcpv6_advt(struct rconn *swconn, const struct flow *ip_flow,
                                             &ip_flow->ipv6_dst,
                                             &in6addr_all_dhcp_agents,
                                             0, 0, 255,
-                                            len + UDP_HEADER_LEN + 4);
-    udp_h->udp_len = htons(len + UDP_HEADER_LEN + 4);
+                                            data_len + UDP_HEADER_LEN + 4);
+    udp_h->udp_len = htons(data_len + UDP_HEADER_LEN + 4);
     udp_h->udp_csum = 0;
     packet_set_udp_port(&packet, htons(546), htons(547));
 
     unsigned char *dhcp_hdr = (unsigned char *)(udp_h + 1);
     *dhcp_hdr = DHCPV6_MSG_TYPE_REQUEST;
-    memcpy(dhcp_hdr + 4, data, len);
+    memcpy(dhcp_hdr + 4, data, data_len);
 
     uint32_t csum = packet_csum_pseudoheader6(dp_packet_l3(&packet));
     csum = csum_continue(csum, udp_h, dp_packet_size(&packet) -
@@ -1006,39 +1068,29 @@ pinctrl_prefixd_state_handler(const struct flow *ip_flow,
 }
 
 static void
-pinctrl_parse_dhcpv6_reply(struct dp_packet *pkt_in,
+pinctrl_parse_dhcpv6_reply(const void *opts_data, size_t opts_len,
                            const struct flow *ip_flow)
     OVS_REQUIRES(pinctrl_mutex)
 {
-    struct udp_header *udp_in = dp_packet_l4(pkt_in);
-    unsigned char *in_dhcpv6_data = (unsigned char *)(udp_in + 1);
-    size_t dlen = MIN(ntohs(udp_in->udp_len), dp_packet_l4_size(pkt_in));
     unsigned t1 = 0, t2 = 0, vlife_time = 0, plife_time = 0;
-    uint8_t *end = (uint8_t *) udp_in + dlen;
     uint8_t prefix_len = 0, uuid_len = 0;
     uint8_t uuid[DHCPV6_MAX_DUID_LEN];
     struct in6_addr ipv6 = in6addr_any;
+    size_t len = 0, opt_len = 0;
     bool status = false;
     unsigned aid = 0;
 
-    /* skip DHCPv6 common header */
-    in_dhcpv6_data += 4;
-
-    while (in_dhcpv6_data < end) {
-        struct dhcpv6_opt_header *in_opt =
-             (struct dhcpv6_opt_header *)in_dhcpv6_data;
-        int opt_len = sizeof *in_opt + ntohs(in_opt->len);
-
-        if (in_dhcpv6_data + opt_len > end) {
-            break;
-        }
-
+    const struct dhcpv6_opt_header *in_opt;
+    for (in_opt = next_dhcpv6_opt(opts_data, opts_len, &len, &opt_len);
+         in_opt;
+         in_opt = next_dhcpv6_opt(opts_data, opts_len, &len, &opt_len)) {
         switch (ntohs(in_opt->code)) {
         case DHCPV6_OPT_IA_PD: {
-            int size = sizeof *in_opt + 12;
-            in_opt = (struct dhcpv6_opt_header *)(in_dhcpv6_data + size);
+            if (opt_len < sizeof(struct dhcpv6_opt_ia_na)) {
+                break;
+            }
             struct dhcpv6_opt_ia_na *ia_na =
-                (struct dhcpv6_opt_ia_na *)in_dhcpv6_data;
+                (struct dhcpv6_opt_ia_na *) in_opt;
 
             aid = ntohl(ia_na->iaid);
             t1 = ntohl(ia_na->t1);
@@ -1047,30 +1099,25 @@ pinctrl_parse_dhcpv6_reply(struct dp_packet *pkt_in,
                 break;
             }
 
-            while (size < opt_len) {
-                if (ntohs(in_opt->code) == DHCPV6_OPT_IA_PREFIX) {
-                    struct dhcpv6_opt_ia_prefix *ia_hdr =
-                        (struct dhcpv6_opt_ia_prefix *)(in_dhcpv6_data + size);
+            struct dhcpv6_opt_ia_prefix *ia_hdr = NULL;
+            struct dhcpv6_opt_status *status_hdr = NULL;
+            dhcpv6_opt_ia_na_parse_inner(ia_na, opt_len, &ia_hdr, &status_hdr);
 
-                    plife_time = ntohl(ia_hdr->plife_time);
-                    vlife_time = ntohl(ia_hdr->vlife_time);
-                    if (plife_time > vlife_time) {
-                        break;
-                    }
-                    prefix_len = ia_hdr->plen;
-                    memcpy(&ipv6, &ia_hdr->ipv6, sizeof (struct in6_addr));
-                    status = true;
+            if (ia_hdr) {
+                plife_time = ntohl(ia_hdr->plife_time);
+                vlife_time = ntohl(ia_hdr->vlife_time);
+                if (plife_time > vlife_time) {
+                    break;
                 }
-                if (ntohs(in_opt->code) == DHCPV6_OPT_STATUS_CODE) {
-                   struct dhcpv6_opt_status *status_hdr;
+                prefix_len = ia_hdr->plen;
+                memcpy(&ipv6, &ia_hdr->ipv6, sizeof (struct in6_addr));
+                status = true;
+            }
 
-                   status_hdr = (struct dhcpv6_opt_status *)in_opt;
-                   if (ntohs(status_hdr->status_code)) {
-                       status = false;
-                   }
+            if (status_hdr) {
+                if (ntohs(status_hdr->status_code)) {
+                    status = false;
                 }
-                size += sizeof *in_opt + ntohs(in_opt->len);
-                in_opt = (struct dhcpv6_opt_header *)(in_dhcpv6_data + size);
             }
             break;
         }
@@ -1081,7 +1128,6 @@ pinctrl_parse_dhcpv6_reply(struct dp_packet *pkt_in,
         default:
             break;
         }
-        in_dhcpv6_data += opt_len;
     }
     if (status) {
         char prefix[INET6_ADDRSTRLEN + 1];
@@ -1108,14 +1154,20 @@ pinctrl_handle_dhcp6_server(struct rconn *swconn, const struct flow *ip_flow,
     }
 
     struct udp_header *udp_in = dp_packet_l4(pkt_in);
-    unsigned char *dhcp_hdr = (unsigned char *)(udp_in + 1);
+    size_t dlen = MIN(ntohs(udp_in->udp_len), dp_packet_l4_size(pkt_in));
+    if (dlen < UDP_HEADER_LEN + DHCPV6_HEADER_LEN) {
+        return;
+    }
 
-    switch (*dhcp_hdr) {
+    size_t opts_len = dlen - UDP_HEADER_LEN - DHCPV6_HEADER_LEN;
+    const struct dhcpv6_header *hdr = dp_packet_get_udp_payload(pkt_in);
+    switch (hdr->msg_type) {
     case DHCPV6_MSG_TYPE_ADVT:
-        pinctrl_parse_dhcpv6_advt(swconn, ip_flow, pkt_in, md);
+        pinctrl_parse_dhcpv6_advt(swconn, ip_flow, DHCPV6_PAYLOAD(hdr),
+                                  opts_len, md);
         break;
     case DHCPV6_MSG_TYPE_REPLY:
-        pinctrl_parse_dhcpv6_reply(pkt_in, ip_flow);
+        pinctrl_parse_dhcpv6_reply(DHCPV6_PAYLOAD(hdr), opts_len, ip_flow);
         break;
     default:
         break;
@@ -3073,6 +3125,8 @@ compose_dhcpv6_status(struct ofpbuf *userdata, struct ofpbuf *opts)
     return true;
 }
 
+#define DHCPV6_UC_PXE_OFFSET 2
+
 /* Called with in the pinctrl_handler thread context. */
 static void
 pinctrl_handle_put_dhcpv6_opts(
@@ -3115,16 +3169,17 @@ pinctrl_handle_put_dhcpv6_opts(
     }
 
     struct udp_header *in_udp = dp_packet_l4(pkt_in);
-    const uint8_t *in_dhcpv6_data = dp_packet_get_udp_payload(pkt_in);
-    if (!in_udp || !in_dhcpv6_data) {
+    size_t dlen = MIN(ntohs(in_udp->udp_len), dp_packet_l4_size(pkt_in));
+    if (dlen < UDP_HEADER_LEN + DHCPV6_HEADER_LEN) {
         VLOG_WARN_RL(&rl, "truncated dhcpv6 packet");
         goto exit;
     }
 
+    const struct dhcpv6_header *hdr = dp_packet_get_udp_payload(pkt_in);
+
     uint8_t out_dhcpv6_msg_type;
-    uint8_t in_dhcpv6_msg_type = *in_dhcpv6_data;
     bool status_only = false;
-    switch (in_dhcpv6_msg_type) {
+    switch (hdr->msg_type) {
     case DHCPV6_MSG_TYPE_SOLICIT:
         out_dhcpv6_msg_type = DHCPV6_MSG_TYPE_ADVT;
         break;
@@ -3145,8 +3200,6 @@ pinctrl_handle_put_dhcpv6_opts(
         /* Invalid or unsupported DHCPv6 message type */
         goto exit;
     }
-    /* Skip 4 bytes (message type (1 byte) + transaction ID (3 bytes). */
-    in_dhcpv6_data += 4;
     /* We need to extract IAID from the IA-NA option of the client's DHCPv6
      * solicit/request/confirm packet and copy the same IAID in the Server's
      * response.
@@ -3155,17 +3208,24 @@ pinctrl_handle_put_dhcpv6_opts(
      * */
     ovs_be32 iaid = 0;
     struct dhcpv6_opt_header const *in_opt_client_id = NULL;
-    size_t udp_len = ntohs(in_udp->udp_len);
-    size_t l4_len = dp_packet_l4_size(pkt_in);
-    uint8_t *end = (uint8_t *)in_udp + MIN(udp_len, l4_len);
     bool ipxe_req = false;
     uint8_t fqdn_flags = DHCPV6_FQDN_FLAGS_UNDEFINED;
-    while (in_dhcpv6_data < end) {
-        struct dhcpv6_opt_header const *in_opt =
-             (struct dhcpv6_opt_header *)in_dhcpv6_data;
-        switch(ntohs(in_opt->code)) {
+    size_t len = 0, opt_len = 0;
+    size_t opts_len = dlen - UDP_HEADER_LEN - DHCPV6_HEADER_LEN;
+
+    const struct dhcpv6_opt_header *in_opt;
+    for (in_opt = next_dhcpv6_opt(DHCPV6_PAYLOAD(hdr), opts_len,
+                                  &len, &opt_len);
+         in_opt;
+         in_opt = next_dhcpv6_opt(DHCPV6_PAYLOAD(hdr), opts_len,
+                                  &len, &opt_len)) {
+        switch (ntohs(in_opt->code)) {
         case DHCPV6_OPT_IA_NA_CODE:
         {
+            if (opt_len < sizeof(struct dhcpv6_opt_ia_na)) {
+                break;
+            }
+
             struct dhcpv6_opt_ia_na *opt_ia_na = (
                 struct dhcpv6_opt_ia_na *)in_opt;
             iaid = opt_ia_na->iaid;
@@ -3177,21 +3237,30 @@ pinctrl_handle_put_dhcpv6_opts(
             break;
 
         case DHCPV6_OPT_USER_CLASS: {
-            char *user_class = (char *)(in_opt + 1);
-            if (!strcmp(user_class + 2, "iPXE")) {
+            if (opt_len <
+                sizeof(struct dhcpv6_opt_header) + DHCPV6_UC_PXE_OFFSET + 4) {
+                break;
+            }
+
+            const char *user_class = DHCPV6_OPT_PAYLOAD(in_opt);
+            if (!strncmp(user_class + DHCPV6_UC_PXE_OFFSET, "iPXE", 4)) {
                 ipxe_req = true;
             }
             break;
         }
 
         case DHCPV6_OPT_FQDN_CODE:
-            fqdn_flags = *(in_dhcpv6_data + sizeof *in_opt);
+            if (opt_len <
+                sizeof(struct dhcpv6_opt_header) + sizeof(uint8_t)) {
+                break;
+            }
+
+            fqdn_flags = *(uint8_t *) DHCPV6_OPT_PAYLOAD(in_opt);
             break;
 
         default:
             break;
         }
-        in_dhcpv6_data += sizeof *in_opt + ntohs(in_opt->len);
     }
 
     if (!in_opt_client_id) {
@@ -3200,7 +3269,7 @@ pinctrl_handle_put_dhcpv6_opts(
         goto exit;
     }
 
-    if (!iaid && in_dhcpv6_msg_type != DHCPV6_MSG_TYPE_INFO_REQ) {
+    if (!iaid && hdr->msg_type != DHCPV6_MSG_TYPE_INFO_REQ) {
         VLOG_WARN_RL(&rl, "DHCPv6 option - IA NA not present in the "
                      "DHCPv6 packet");
         goto exit;

lib/ovn-l7.h
@@ -312,6 +312,16 @@ extern const struct in6_addr in6addr_all_dhcp_agents;
                                            0x00,0x00,0x00,0x00,0x00,0x00,     \
                                            0x00,0x01,0x00,0x02 } } }
 
+#define DHCPV6_HEADER_LEN 4
+OVS_PACKED(
+struct dhcpv6_header {
+    uint8_t msg_type;
+    uint8_t transaction_id[3];
+});
+BUILD_ASSERT_DECL(DHCPV6_HEADER_LEN == sizeof(struct dhcpv6_header));
+
+#define DHCPV6_PAYLOAD(hdr) \
+    (const void *)((uint8_t *) (hdr) + sizeof(struct dhcpv6_header))
 
 #define DHCP6_OPT_HEADER_LEN 4
 OVS_PACKED(

tests/system-ovn.at
@@ -21748,3 +21748,69 @@ OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([DHCPv6 - Options heap overread])
+AT_SKIP_IF([test $HAVE_SCAPY = no])
+
+ovn_start
+
+OVS_TRAFFIC_VSWITCHD_START()
+ADD_BR([br-int])
+
+# Set external-ids in br-int needed for ovn-controller.
+check ovs-vsctl \
+        -- set Open_vSwitch . external-ids:system-id=hv1 \
+        -- set Open_vSwitch . external-ids:ovn-remote=unix:$ovs_base/ovn-sb/ovn-sb.sock \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve \
+        -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
+        -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
+
+start_daemon ovn-controller
+
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lsp-add ls1 ls1-lp1 \
+    -- lsp-set-addresses ls1-lp1 "f0:00:00:00:00:01 fd10::4"
+
+check_uuid ovn-nbctl -- --id=@opt create DHCP_Options cidr="fd10\:\:/64" \
+    options="\"server_id\"=\"00:00:00:10:00:01\"" \
+    -- set Logical_Switch_Port ls1-lp1 dhcpv6_options=[@opt]
+
+ADD_NAMESPACES(ls1-lp1)
+ADD_VETH(ls1-lp1, ls1-lp1, br-int, "fd10::4/96", "f0:00:00:00:00:01", \
+         "fd10::1", "nodad")
+
+NETNS_START_TCPDUMP([ls1-lp1], [-nnne -i ls1-lp1 udp port 546 and udp port 547], [ls1-lp1])
+
+OVN_POPULATE_ARP
+wait_for_ports_up
+check ovn-nbctl --wait=hv sync
+
+ip netns exec ls1-lp1 scapy -H <<-EOF
+p = Ether(dst='33:33:00:01:00:02', src='f0:00:00:00:00:01') / \
+    IPv6(dst='ff02::1:2', src='fe80::f200:ff:fe00:1') / \
+    UDP(sport=546, dport=547) / \
+    Raw(load=bytes.fromhex( \
+        '01' '010203' \
+        '0003' '000c' '01020304' '00000000' '00000000' \
+        '0001' '0200' \
+        '0003' '0001' \
+        'f00000000001'
+    ))
+sendp (p, iface='ls1-lp1', loop = 0, verbose = 0, count = 1)
+EOF
+
+# ovn-contorller should report a warning that the packet didn't contain valid Client ID.
+OVS_WAIT_UNTIL([grep -q "DHCPv6 option - Client id not present" ovn-controller.log])
+AT_CHECK([grep -q "advertise" ls1-lp1.tcpdump], [1])
+
+OVN_CLEANUP_CONTROLLER([hv1])
+OVN_CLEANUP_NORTHD
+
+as
+OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
+/connection dropped.*/d
+/DHCPv6 option - Client id not present.*/d"])
+
+AT_CLEANUP
+])