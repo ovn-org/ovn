68668ab12db53a041339b35f0248c8f7b36188e0
Mark Michelson <mmichels@redhat.com>
2026-05-27 07:53:58 +0200

    Revert "controller: Fix bfd up too early after unexpected reboot."
    This commit fixed a BFD issue by toggling the value of
    flow-restore-wait. OVN would set the value false to allow OVS to start
    up, then set the value back to true afterwards. This way, if OVS
    rebooted unexpectedly, then it would not restore flows until OVN once
    again set the value false.
    
    Unfortunately, this results in an unintended side effect. If a node is
    using an OVS bridge to manage its connection to the southbound database,
    then the OVS and OVN processes are essentially deadlocked. OVS is
    waiting for OVN to set flow-restore-wait to false before it will
    establish any networking for its bridges. OVN is waiting for a
    connection to be established with the southbound database before it will
    set flow-restore-wait to false.
    
    This reverts commit 621f85e924377b2c440369ec08f9f95312e68e3f. A new
    method for resolving the BFD issue will need to be devised.
    
    Fixes: 621f85e92437 ("controller: Fix bfd up too early after unexpected reboot.")
    Reported-at: https://redhat.atlassian.net/browse/FDP-3862
    Signed-off-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Ales Musil <amusil@redhat.com>
    (cherry picked from commit 30f431174f7f42cc7593b6a312fdf2c8b51abf3e)

--- controller/bfd.h
+++ controller/bfd.h
@@ -16,8 +16,6 @@
 #ifndef OVN_BFD_H
 #define OVN_BFD_H 1
 
-#include <stdbool.h>
-
 struct hmap;
 struct ovsdb_idl;
 struct ovsdb_idl_index;
@@ -37,7 +35,7 @@ void bfd_run(const struct ovsrec_interface_table *,
              const struct sbrec_chassis *,
              const struct sbrec_sb_global_table *);
 
-bool bfd_calculate_chassis(
+void bfd_calculate_chassis(
     const struct sbrec_chassis *,
     const struct sbrec_ha_chassis_group_table *,
     struct sset *);
