5ad50fff53d293d4d7ace064ee5aca2f5121aa2e
Ales Musil <amusil@redhat.com>
2026-06-23 14:04:34 +0200

    northd: Add missing feature change checks.
    The feature change check was missing a bunch of flags.
    Add all of them so the I-P can properly propagate the change.
    
    Fixes: 43f741c2f029 ("northd: Explicitly handle SNAT for ICMP need frag.")
    Fixes: d9c97878eb23 ("actions: New action ct_commit_to_zone.")
    Fixes: 71752140ec24 ("features: Add detection for sample with registers.")
    Fixes: 8114a5c606eb ("northd, controller: Use ct_next to get the CT state for direct SNAT.")
    Fixes: 0d3203cc4630 ("chassis: Check for ability to flush using CT label/mark.")
    Fixes: a0870575c138 ("lib: northd: Add a new ct-state-save feature flag.")
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    (cherry picked from commit 45fcde5779844c3da1b935acf8929e393fc3a5d5)

--- northd/en-global-config.c
+++ northd/en-global-config.c
@@ -621,5 +621,21 @@ chassis_features_changed(const struct chassis_features *present,
         return true;
     }
 
+    if (present->ct_commit_nat_v2 != updated->ct_commit_nat_v2) {
+        return true;
+    }
+
+    if (present->ct_commit_to_zone != updated->ct_commit_to_zone) {
+        return true;
+    }
+
+    if (present->ct_next_zone != updated->ct_next_zone) {
+        return true;
+    }
+
+    if (present->ct_state_save != updated->ct_state_save) {
+        return true;
+    }
+
     return false;
 }
