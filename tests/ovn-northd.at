ovn-org/ovn

commit d1d69752f1218d2a5ac05e5bfdedf5589c6f4f74

northd: Use reachable sset of 'ovn_lb_ip_set' when adding arp req lflows.
When adding the arp req lflows in the 'ls_in_l2_lkup' stage for the
load balancer VIPs associated to a logical router we iterate through
the lb_ips sset of the router datapath.  For each ip of this sset we
check if this is reachable or not from any of the router ports.
We can instead use the reachable sset of ips (which is generated by
northd engine node).

Acked-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/northd.c
@@ -8853,7 +8853,7 @@ build_lswitch_rport_arp_req_flows(struct ovn_port *op,
      */
 
     const char *ip_addr;
-    SSET_FOR_EACH (ip_addr, &op->od->lb_ips->ips_v4) {
+    SSET_FOR_EACH (ip_addr, &op->od->lb_ips->ips_v4_reachable) {
         ovs_be32 ipv4_addr;
 
         /* Check if the ovn port has a network configured on which we could
@@ -8866,7 +8866,7 @@ build_lswitch_rport_arp_req_flows(struct ovn_port *op,
                 stage_hint);
         }
     }
-    SSET_FOR_EACH (ip_addr, &op->od->lb_ips->ips_v6) {
+    SSET_FOR_EACH (ip_addr, &op->od->lb_ips->ips_v6_reachable) {
         struct in6_addr ipv6_addr;
 
         /* Check if the ovn port has a network configured on which we could

commit 543f3c621d8f5f3d554e8b4b75fade6f67f1275a

northd: Add missing ARP/ND flows for unreachable LB NATs.
Currently, priority-80 ls_in_l2_lkup ARP/ND response flows are
generated for all NAT entries except those associated with
unreachable LB IPs. In contrast, these flows are correctly
generated for unreachable non-LB NATs.

Generate the missing ARP/ND response flows for unreachable LB NATs
as well, making their behavior consistent with other unreachable
NAT entries.

Fixes: d1d69752f121 ("northd: Use reachable sset of 'ovn_lb_ip_set' when adding arp req lflows.")
Signed-off-by: Amir Aslan Aslani <amiraslanaslani6@gmail.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

northd/northd.c
@@ -9883,14 +9883,14 @@ build_lswitch_rport_arp_req_flows_for_lbnats(
          * expect ARP requests/NS for the DNAT external_ip.
          */
         if (nat_entry_is_v6(nat_entry)) {
-            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v6,
+            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v6_reachable,
                                nat->external_ip)) {
                 build_lswitch_rport_arp_req_flow(
                     nat->external_ip, AF_INET6, sw_op, sw_od, 80, lflows,
                     stage_hint, lflow_ref);
             }
         } else {
-            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v4,
+            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v4_reachable,
                                nat->external_ip)) {
                 build_lswitch_rport_arp_req_flow(
                     nat->external_ip, AF_INET, sw_op, sw_od, 80, lflows,
@@ -9927,14 +9927,14 @@ build_lswitch_rport_arp_req_flows_for_lbnats(
          * expect ARP requests/NS for the SNAT external_ip.
          */
         if (nat_entry_is_v6(nat_entry)) {
-            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v6,
+            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v6_reachable,
                                nat->external_ip)) {
                 build_lswitch_rport_arp_req_flow(
                     nat->external_ip, AF_INET6, sw_op, sw_od, 80, lflows,
                     stage_hint, lflow_ref);
             }
         } else {
-            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v4,
+            if (!sset_contains(&lr_stateful_rec->lb_ips->ips_v4_reachable,
                                nat->external_ip)) {
                 build_lswitch_rport_arp_req_flow(
                     nat->external_ip, AF_INET, sw_op, sw_od, 80, lflows,

tests/ovn-northd.at
@@ -6340,7 +6340,16 @@ AT_CHECK([grep "ls_in_l2_lkup" ls1_lflows | ovn_strip_lflows], [0], [dnl
 
 
 # Make sure that there is no flow for VIP 192.168.4.100 as it is unreachable.
-AT_CHECK([grep "ls_in_l2_lkup" ls1_lflows | grep "192.168.4.100" | grep -v clone | grep "_MC_flood" -c], [1], [0
+AT_CHECK([grep "ls_in_l2_lkup" ls1_lflows | grep "192.168.4.100" -c], [1], [0
+])
+
+AS_BOX([Adding an dnat_and_snat for LB unreachable VIP on ro1])
+check ovn-nbctl --wait=sb lr-nat-add ro1 dnat_and_snat 192.168.4.100 192.168.1.100
+
+ovn-sbctl lflow-list ls1 > ls1_lflows
+AT_CHECK([grep "ls_in_l2_lkup" ls1_lflows | grep "192.168.4.100" | ovn_strip_lflows], [0], [dnl
+  table=??(ls_in_l2_lkup      ), priority=80   , match=(flags[[1]] == 0 && eth.dst == ff:ff:ff:ff:ff:ff && arp.op == 1 && arp.tpa == 192.168.4.100), action=(clone {outport = "ls1-ro1"; output; }; outport = "_MC_unknown"; output;)
+  table=??(ls_in_l2_lkup      ), priority=90   , match=(flags[[1]] == 0 && eth.dst == ff:ff:ff:ff:ff:ff && arp.op == 1 && arp.tpa == 192.168.4.100 && arp.spa == 192.168.4.100), action=(outport = "_MC_flood_l2"; output;)
 ])
 
 OVN_CLEANUP_NORTHD