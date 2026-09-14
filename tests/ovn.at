ovn-org/ovn

commit 02839c4d893486854993567586b890e3b7c2c56b

controller: bfd: introduce BFD state machine.
Introduce BFD state machine for BFD packet parsing
according to RFC880 https://tools.ietf.org/html/rfc5880.
Introduce BFD logical flows in ovn-northd.

Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/pinctrl.c
@@ -330,9 +330,10 @@ static void bfd_monitor_destroy(void);
 static void bfd_monitor_send_msg(struct rconn *swconn, long long int *bfd_time)
                                  OVS_REQUIRES(pinctrl_mutex);
 static void
-pinctrl_handle_bfd_msg(void)
+pinctrl_handle_bfd_msg(const struct flow *ip_flow, struct dp_packet *pkt_in)
                        OVS_REQUIRES(pinctrl_mutex);
-static void bfd_monitor_run(const struct sbrec_bfd_table *bfd_table,
+static void bfd_monitor_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
+                            const struct sbrec_bfd_table *bfd_table,
                             struct ovsdb_idl_index *sbrec_port_binding_by_name,
                             const struct sbrec_chassis *chassis,
                             const struct sset *active_tunnels)
@@ -2980,7 +2981,7 @@ process_packet_in(struct rconn *swconn, const struct ofp_header *msg)
 
     case ACTION_OPCODE_BFD_MSG:
         ovs_mutex_lock(&pinctrl_mutex);
-        pinctrl_handle_bfd_msg();
+        pinctrl_handle_bfd_msg(&headers, &packet);
         ovs_mutex_unlock(&pinctrl_mutex);
         break;
 
@@ -3206,10 +3207,8 @@ pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
                          local_datapaths);
     sync_svc_monitors(ovnsb_idl_txn, svc_mon_table, sbrec_port_binding_by_name,
                       chassis);
-    if (ovnsb_idl_txn) {
-        bfd_monitor_run(bfd_table, sbrec_port_binding_by_name, chassis,
-                        active_tunnels);
-    }
+    bfd_monitor_run(ovnsb_idl_txn, bfd_table, sbrec_port_binding_by_name,
+                    chassis, active_tunnels);
     ovs_mutex_unlock(&pinctrl_mutex);
 }
 
@@ -6378,7 +6377,47 @@ sync_svc_monitors(struct ovsdb_idl_txn *ovnsb_idl_txn,
 
 }
 
+enum bfd_state {
+    BFD_STATE_ADMIN_DOWN,
+    BFD_STATE_DOWN,
+    BFD_STATE_INIT,
+    BFD_STATE_UP,
+};
+
+enum bfd_flags {
+    BFD_FLAG_MULTIPOINT = 1 << 0,
+    BFD_FLAG_DEMAND = 1 << 1,
+    BFD_FLAG_AUTH = 1 << 2,
+    BFD_FLAG_CTL = 1 << 3,
+    BFD_FLAG_FINAL = 1 << 4,
+    BFD_FLAG_POLL = 1 << 5
+};
+
+#define BFD_FLAGS_MASK  0x3f
+
+static char *
+bfd_get_status(enum bfd_state state)
+{
+    switch (state) {
+    case BFD_STATE_ADMIN_DOWN:
+        return "admin_down";
+    case BFD_STATE_DOWN:
+        return "down";
+    case BFD_STATE_INIT:
+        return "init";
+    case BFD_STATE_UP:
+        return "up";
+    default:
+        return "";
+    }
+}
+
 static struct hmap bfd_monitor_map;
+
+#define BFD_UPDATE_BATCH_TH     10
+static uint16_t bfd_pending_update;
+#define BFD_UPDATE_TIMEOUT      5000LL
+static long long bfd_last_update;
 
 struct bfd_entry {
     struct hmap_node node;
@@ -6398,11 +6437,23 @@ struct bfd_entry {
      * sessions on the system
      */
     uint16_t udp_src;
-    ovs_be32 disc;
+    ovs_be32 local_disc;
+    ovs_be32 remote_disc;
+
+    uint32_t local_min_tx;
+    uint32_t local_min_rx;
+    uint32_t remote_min_rx;
+
+    uint8_t local_mult;
 
     int64_t port_key;
     int64_t metadata;
 
+    enum bfd_state state;
+    bool change_state;
+
+    uint32_t detection_timeout;
+    long long int last_rx;
     long long int next_tx;
 };
 
@@ -6410,6 +6461,7 @@ static void
 bfd_monitor_init(void)
 {
     hmap_init(&bfd_monitor_map);
+    bfd_last_update = time_msec();
 }
 
 static void
@@ -6433,6 +6485,24 @@ pinctrl_find_bfd_monitor_entry_by_port(char *ip, uint16_t port)
         }
     }
     return NULL;
+}
+
+static struct bfd_entry *
+pinctrl_find_bfd_monitor_entry_by_disc(ovs_be32 ip, ovs_be32 disc)
+{
+    char *ip_src = xasprintf(IP_FMT, IP_ARGS(ip));
+    struct bfd_entry *ret = NULL, *entry;
+
+    HMAP_FOR_EACH_WITH_HASH (entry, node, hash_string(ip_src, 0),
+                             &bfd_monitor_map) {
+        if (entry->local_disc == disc) {
+            ret = entry;
+            break;
+        }
+    }
+
+    free(ip_src);
+    return ret;
 }
 
 static bool
@@ -6486,9 +6556,60 @@ bfd_monitor_put_bfd_msg(struct bfd_entry *entry, struct dp_packet *packet)
     udp->udp_dst = htons(BFD_DEST_PORT);
     udp->udp_len = htons(sizeof *udp + sizeof *msg);
 
-    msg = dp_packet_put_uninit(packet, sizeof *msg);
+    msg = dp_packet_put_zeros(packet, sizeof *msg);
     msg->vers_diag = (BFD_VERSION << 5);
+    msg->mult = entry->local_mult;
     msg->length = BFD_PACKET_LEN;
+    msg->flags = entry->state << 6;
+    msg->my_disc = entry->local_disc;
+    msg->your_disc = entry->remote_disc;
+    /* min_tx and min_rx are in us - RFC 5880 page 9 */
+    msg->min_tx = htonl(entry->local_min_tx * 1000);
+    msg->min_rx = htonl(entry->local_min_rx * 1000);
+}
+
+static bool
+bfd_monitor_need_update(void)
+{
+    long long int cur_time = time_msec();
+
+    if (bfd_pending_update == BFD_UPDATE_BATCH_TH) {
+        goto update;
+    }
+
+    if (bfd_pending_update &&
+        bfd_last_update + BFD_UPDATE_TIMEOUT < cur_time) {
+        goto update;
+    }
+    return false;
+
+update:
+    bfd_last_update = cur_time;
+    bfd_pending_update = 0;
+    return true;
+}
+
+static void
+bfd_check_detection_timeout(struct bfd_entry *entry)
+{
+    if (entry->state == BFD_STATE_ADMIN_DOWN) {
+        return;
+    }
+
+    if (!entry->detection_timeout) {
+        return;
+    }
+
+    long long int cur_time = time_msec();
+    if (cur_time < entry->last_rx + entry->detection_timeout) {
+        return;
+    }
+
+    entry->state = BFD_STATE_DOWN;
+    entry->change_state = true;
+    bfd_last_update = cur_time;
+    bfd_pending_update = 0;
+    notify_pinctrl_main();
 }
 
 static void
@@ -6498,9 +6619,25 @@ bfd_monitor_send_msg(struct rconn *swconn, long long int *bfd_time)
     long long int cur_time = time_msec();
     struct bfd_entry *entry;
 
+    if (bfd_monitor_need_update()) {
+        notify_pinctrl_main();
+    }
+
     HMAP_FOR_EACH (entry, node, &bfd_monitor_map) {
+        unsigned long tx_timeout;
+
+        bfd_check_detection_timeout(entry);
+
         if (cur_time < entry->next_tx) {
             goto next;
+        }
+
+        if (!entry->remote_min_rx) {
+            continue;
+        }
+
+        if (entry->state == BFD_STATE_ADMIN_DOWN) {
+            continue;
         }
 
         uint64_t packet_stub[256 / 8];
@@ -6537,7 +6674,9 @@ bfd_monitor_send_msg(struct rconn *swconn, long long int *bfd_time)
         dp_packet_uninit(&packet);
         ofpbuf_uninit(&ofpacts);
 
-        entry->next_tx = cur_time + 5000;
+        tx_timeout = MAX(entry->local_min_tx, entry->remote_min_rx);
+        tx_timeout -= random_range((tx_timeout * 25) / 100);
+        entry->next_tx = cur_time + tx_timeout;
 next:
         if (*bfd_time > entry->next_tx) {
             *bfd_time = entry->next_tx;
@@ -6545,14 +6684,167 @@ next:
     }
 }
 
-static void
-pinctrl_handle_bfd_msg(void)
-    OVS_REQUIRES(pinctrl_mutex)
+static bool
+pinctrl_check_bfd_msg(const struct flow *ip_flow, struct dp_packet *pkt_in)
 {
+    if (ip_flow->dl_type != htons(ETH_TYPE_IP) &&
+        ip_flow->dl_type != htons(ETH_TYPE_IPV6)) {
+        return false;
+    }
+
+    if (ip_flow->nw_proto != IPPROTO_UDP) {
+        return false;
+    }
+
+    struct udp_header *udp_hdr = dp_packet_l4(pkt_in);
+    if (udp_hdr->udp_dst != htons(BFD_DEST_PORT)) {
+        return false;
+    }
+
+    const struct bfd_msg *msg = dp_packet_get_udp_payload(pkt_in);
+    uint8_t version = msg->vers_diag >> 5;
+    if (version != BFD_VERSION) {
+        return false;
+    }
+
+    enum bfd_flags flags = msg->flags & BFD_FLAGS_MASK;
+    if (flags & BFD_FLAG_AUTH) {
+        /* AUTH not supported yet */
+        return false;
+    }
+
+    if (msg->length < BFD_PACKET_LEN) {
+        return false;
+    }
+
+    if (!msg->mult) {
+        return false;
+    }
+
+    if (flags & BFD_FLAG_MULTIPOINT) {
+        return false;
+    }
+
+    if (!msg->my_disc) {
+        return false;
+    }
+
+    enum bfd_state peer_state = msg->flags >> 6;
+    if (peer_state >= BFD_STATE_INIT && !msg->your_disc) {
+        return false;
+    }
+
+    return true;
 }
 
 static void
-bfd_monitor_run(const struct sbrec_bfd_table *bfd_table,
+pinctrl_handle_bfd_msg(const struct flow *ip_flow, struct dp_packet *pkt_in)
+    OVS_REQUIRES(pinctrl_mutex)
+{
+    if (!pinctrl_check_bfd_msg(ip_flow, pkt_in)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+        VLOG_WARN_RL(&rl, "BFD packet discarded");
+        return;
+    }
+
+    const struct bfd_msg *msg = dp_packet_get_udp_payload(pkt_in);
+    struct bfd_entry *entry = pinctrl_find_bfd_monitor_entry_by_disc(
+            ip_flow->nw_src, msg->your_disc);
+    if (!entry) {
+        return;
+    }
+
+    bool change_state = false;
+    entry->remote_disc = msg->my_disc;
+    uint32_t remote_min_tx = ntohl(msg->min_tx) / 1000;
+    entry->remote_min_rx = ntohl(msg->min_rx) / 1000;
+    entry->detection_timeout = msg->mult * MAX(remote_min_tx,
+                                               entry->local_min_rx);
+
+    enum bfd_state peer_state = msg->flags >> 6;
+    if (peer_state == BFD_STATE_ADMIN_DOWN &&
+        entry->state >= BFD_STATE_INIT) {
+        entry->state = BFD_STATE_DOWN;
+        entry->last_rx = time_msec();
+        change_state = true;
+        goto out;
+    }
+
+    /* bfd state machine */
+    switch (entry->state) {
+    case BFD_STATE_DOWN:
+        if (peer_state == BFD_STATE_DOWN) {
+            entry->state = BFD_STATE_INIT;
+            change_state = true;
+        }
+        if (peer_state == BFD_STATE_INIT) {
+            entry->state = BFD_STATE_UP;
+            change_state = true;
+        }
+        entry->last_rx = time_msec();
+        break;
+    case BFD_STATE_INIT:
+        if (peer_state == BFD_STATE_INIT ||
+            peer_state == BFD_STATE_UP) {
+            entry->state = BFD_STATE_UP;
+            change_state = true;
+        }
+        if (peer_state == BFD_STATE_ADMIN_DOWN) {
+            entry->state = BFD_STATE_DOWN;
+            change_state = true;
+        }
+        entry->last_rx = time_msec();
+        break;
+    case BFD_STATE_UP:
+        if (peer_state == BFD_STATE_ADMIN_DOWN ||
+            peer_state == BFD_STATE_DOWN) {
+            entry->state = BFD_STATE_DOWN;
+            change_state = true;
+        }
+        entry->last_rx = time_msec();
+        break;
+    case BFD_STATE_ADMIN_DOWN:
+    default:
+        break;
+    }
+
+out:
+    /* let's try to bacth db updates */
+    if (change_state) {
+        entry->change_state = true;
+        bfd_pending_update++;
+    }
+    if (bfd_monitor_need_update()) {
+        notify_pinctrl_main();
+    }
+}
+
+static void
+bfd_monitor_check_sb_conf(const struct sbrec_bfd *sb_bt,
+                          struct bfd_entry *entry)
+{
+    ovs_be32 ip_dst;
+
+    if (ip_parse(sb_bt->dst_ip, &ip_dst) && ip_dst != entry->ip_dst) {
+        entry->ip_dst = ip_dst;
+    }
+
+    if (sb_bt->min_tx != entry->local_min_tx) {
+        entry->local_min_tx = sb_bt->min_tx;
+    }
+
+    if (sb_bt->min_rx != entry->local_min_rx) {
+        entry->local_min_rx = sb_bt->min_rx;
+    }
+
+    if (sb_bt->detect_mult != entry->local_mult) {
+        entry->local_mult = sb_bt->detect_mult;
+    }
+}
+
+static void
+bfd_monitor_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
+                const struct sbrec_bfd_table *bfd_table,
                 struct ovsdb_idl_index *sbrec_port_binding_by_name,
                 const struct sbrec_chassis *chassis,
                 const struct sset *active_tunnels)
@@ -6632,15 +6924,39 @@ bfd_monitor_run(const struct sbrec_bfd_table *bfd_table,
             entry->ip_src = ip_src;
             entry->ip_dst = ip_dst;
             entry->udp_src = bt->src_port;
-            entry->disc = htonl(bt->disc);
+            entry->local_disc = htonl(bt->disc);
             entry->next_tx = cur_time;
+            entry->last_rx = cur_time;
+            entry->detection_timeout = 30000;
             entry->metadata = pb->datapath->tunnel_key;
             entry->port_key = pb->tunnel_key;
+            entry->state = BFD_STATE_ADMIN_DOWN;
+            entry->local_min_tx = bt->min_tx;
+            entry->local_min_rx = bt->min_rx;
+            entry->remote_min_rx = 1; /* RFC5880 page 29 */
+            entry->local_mult = bt->detect_mult;
 
             uint32_t hash = hash_string(bt->dst_ip, 0);
             hmap_insert(&bfd_monitor_map, &entry->node, hash);
+        } else if (!strcmp(bt->status, "admin_down") &&
+                   entry->state != BFD_STATE_ADMIN_DOWN) {
+            entry->state = BFD_STATE_ADMIN_DOWN;
+            entry->change_state = false;
+            entry->remote_disc = 0;
+        } else if (strcmp(bt->status, "admin_down") &&
+                   entry->state == BFD_STATE_ADMIN_DOWN) {
+            entry->state = BFD_STATE_DOWN;
+            entry->change_state = false;
+            entry->remote_disc = 0;
             changed = true;
+        } else if (entry->change_state && ovnsb_idl_txn) {
+            if (entry->state == BFD_STATE_DOWN) {
+                entry->remote_disc = 0;
+            }
+            sbrec_bfd_set_status(bt, bfd_get_status(entry->state));
+            entry->change_state = false;
         }
+        bfd_monitor_check_sb_conf(bt, entry);
         entry->erase = false;
     }
 

northd/ovn-northd.8.xml
@@ -1938,6 +1938,27 @@ next;
 
       <li>
         <p>
+            For each BFD port the two following priority-110 flows are added
+            to manage BFD traffic:
+
+            <ul>
+              <li>
+               if <code>ip4.src</code> or <code>ip6.src</code> is any IP
+               address owned by the router port and <code>udp.dst == 3784
+               </code>, the packet is advanced to the next pipeline stage.
+              </li>
+
+              <li>
+               if <code>ip4.dst</code> or <code>ip6.dst</code> is any IP
+               address owned by the router port and <code>udp.dst == 3784
+               </code>, the <code>handle_bfd_msg</code> action is executed.
+              </li>
+            </ul>
+        </p>
+      </li>
+
+      <li>
+        <p>
           L3 admission control: A priority-100 flow drops packets that match
           any of the following:
         </p>

commit 06cb7e24cf79adf22b7989c342509fc7a78415fa

controller: Reconcile BFD status with local session state.
bfd_monitor_run() only writes the BFD table status column on state
machine transitions: pinctrl_handle_bfd_msg() and
bfd_check_detection_timeout() mark the entry with change_state = true
and the next bfd_monitor_run() invocation flushes the new state to the
Southbound database.  If the Southbound status diverges from the local
session state without a transition pending, nothing ever repairs the
row.  This happens in two scenarios:

1. A session (re)created while the Southbound row still holds a stale
   non-admin_down status, e.g. after an ovn-controller restart or after
   the gateway port moved back to the chassis.  The freshly created
   entry starts in BFD_STATE_ADMIN_DOWN and is then nudged to
   BFD_STATE_DOWN with change_state = false because the row status is
   not "admin_down".  If the session never establishes (peer
   unreachable, no egress path), no transition ever fires and a stale
   "up" persists forever, keeping BFD-controlled routes active.

2. An external client rewriting the row, e.g. "ovn-sbctl set bfd X
   status=down" while the session is genuinely up.  The bogus value
   sticks until the next real flap.

Fix this by reconciling the row at the end of the per-row processing in
bfd_monitor_run(): when no transition is pending and the Southbound
status differs from what the state machine would have written, issue a
corrective write.  An "admin_down" status is never overwritten since it
is an administrative override set by ovn-northd/CMS rather than a state
machine product; the pre-existing branches keep moving the local session
in and out of BFD_STATE_ADMIN_DOWN to honor it, and reconciliation only
runs when they did not fire.  In steady state the strcmp() guard ensures
no Southbound writes are issued.

Reconciliation attempts are rate limited per session (BFD_UPDATE_TIMEOUT
apart) so that a write which the server keeps rejecting cannot busy loop
the main loop.  This matters with RBAC enabled: the BFD table authorizes
updates through chassis_name, which ovn-northd only fills in when the
logical port is bound, so rows for distributed gateway ports are not
writable by ovn-controller at all (a pre-existing problem which also
drops the transition-driven status updates).

Transactions commit asynchronously, so a rejected corrective write is
not observable at the point where it is issued and would otherwise be
retried every BFD_UPDATE_TIMEOUT silently forever.  Detect this through
the divergence itself: count consecutive corrective writes for a row
that keeps diverging across passes and, once a few attempts clearly
took no effect, emit a rate limited warning naming the logical port and
destination IP and pointing at the server rejecting the updates (e.g.
RBAC with a stale chassis_name) as the likely cause.  The counter
resets as soon as the Southbound status matches the local session state
again.  A new coverage counter, pinctrl_bfd_status_reconcile, counts
the corrective writes issued.

Tested: "make check" BFD tests pass ("BFD - reconcile SB status with
local session state" for both ovn_monitor_all variants, including a new
scenario asserting the warning fires when RBAC rejects the writes and
stops once chassis_name is restored, and "check BFD config propagation
to SBDB").  Also validated on a live 2-chassis deployment: planted
divergent statuses were corrected within ~30 ms, and a stale "up" after
an ovn-controller restart converged within ~1 s.

Fixes: 02839c4d8934 ("controller: bfd: introduce BFD state machine.")

controller/pinctrl.c
@@ -356,6 +356,7 @@ static void notify_pinctrl_handler(void);
 
 static bool bfd_monitor_should_inject(void);
 static void bfd_monitor_wait(long long int timeout);
+static void bfd_monitor_reconcile_wait(void);
 static void bfd_monitor_init(void);
 static void bfd_monitor_destroy(void);
 static void bfd_monitor_send_msg(struct rconn *swconn, long long int *bfd_time)
@@ -400,6 +401,7 @@ COVERAGE_DEFINE(pinctrl_drop_put_vport_binding);
 COVERAGE_DEFINE(pinctrl_notify_main_thread);
 COVERAGE_DEFINE(pinctrl_notify_handler_thread);
 COVERAGE_DEFINE(pinctrl_total_pin_pkts);
+COVERAGE_DEFINE(pinctrl_bfd_status_reconcile);
 
 /* DNS query statistics - thread-safe coverage counters */
 COVERAGE_DEFINE(dns_query_total);
@@ -4753,6 +4755,7 @@ pinctrl_wait(struct ovsdb_idl_txn *ovnsb_idl_txn)
     if (ovnsb_idl_txn) {
         wait_controller_event();
         wait_put_vport_bindings();
+        bfd_monitor_reconcile_wait();
         seq_wait(pinctrl_main_seq, main_seq);
     }
     wait_activated_ports();
@@ -7584,7 +7587,16 @@ struct bfd_entry {
     uint32_t detection_timeout;
     long long int last_rx;
     long long int next_tx;
+    /* Earliest time for the next SB status reconciliation attempt. */
+    long long int next_reconcile;
+    /* Consecutive SB status reconciliation writes without the row
+     * converging; reset once the SB status matches the local state. */
+    unsigned int reconcile_attempts;
 };
+
+/* Reconciliation writes tolerated before warning that they do not appear
+ * to take effect on the server. */
+#define BFD_RECONCILE_MAX_ATTEMPTS 3
 
 static void
 bfd_monitor_init(void)
@@ -7651,6 +7663,21 @@ bfd_monitor_wait(long long int timeout)
 {
     if (!hmap_is_empty(&bfd_monitor_map)) {
         poll_timer_wait_until(timeout);
+    }
+}
+
+/* Register reconciliation deadlines with the main controller thread.  BFD
+ * packet transmission is handled by the pinctrl handler thread, but SB IDL
+ * transactions are owned by the main thread. */
+static void
+bfd_monitor_reconcile_wait(void)
+{
+    struct bfd_entry *entry;
+
+    HMAP_FOR_EACH (entry, node, &bfd_monitor_map) {
+        if (entry->next_reconcile) {
+            poll_timer_wait_until(entry->next_reconcile);
+        }
     }
 }
 
@@ -8173,8 +8200,52 @@ bfd_monitor_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
             if (entry->state == BFD_STATE_DOWN) {
                 entry->remote_disc = 0;
             }
+            sbrec_bfd_verify_status(bt);
             sbrec_bfd_set_status(bt, bfd_get_status(entry->state));
             entry->change_state = false;
+        }
+
+        /* Reconcile the SB status with the local session state.  The
+         * state machine only writes the status out on transitions (see
+         * the change_state branch above), so a divergent SB value would
+         * otherwise persist until the next transition, e.g. if the row
+         * held a stale status when the session was created or if an
+         * external client rewrote the column.  Skip "admin_down": it is
+         * an administrative override written by northd/CMS, not a state
+         * machine product, and the branches above already move the local
+         * session in and out of BFD_STATE_ADMIN_DOWN to honor it.
+         * Rate limit the attempts so that a write repeatedly rejected by
+         * the server, e.g. by RBAC, does not busy loop the main loop. */
+        if (ovnsb_idl_txn && !entry->change_state &&
+            entry->state != BFD_STATE_ADMIN_DOWN &&
+            cur_time >= entry->next_reconcile &&
+            strcmp(bt->status, "admin_down") &&
+            strcmp(bt->status, bfd_get_status(entry->state))) {
+            /* Transactions commit asynchronously, so a rejected write is
+             * not observable here.  A row that stays divergent across
+             * several attempts means the corrective writes never take
+             * effect on the server; warn so this does not go unnoticed
+             * forever. */
+            COVERAGE_INC(pinctrl_bfd_status_reconcile);
+            if (entry->reconcile_attempts++ > BFD_RECONCILE_MAX_ATTEMPTS) {
+                static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+                VLOG_WARN_RL(&rl, "BFD %s: SB status updates for logical "
+                             "port %s do not appear to take effect (%u "
+                             "attempts, local state %s, SB status %s); the "
+                             "server may be rejecting them, e.g. RBAC with "
+                             "a stale BFD chassis_name",
+                             bt->dst_ip, bt->logical_port,
+                             entry->reconcile_attempts,
+                             bfd_get_status(entry->state), bt->status);
+            }
+            sbrec_bfd_verify_status(bt);
+            sbrec_bfd_set_status(bt, bfd_get_status(entry->state));
+            entry->next_reconcile = cur_time + BFD_UPDATE_TIMEOUT;
+        } else if (entry->state == BFD_STATE_ADMIN_DOWN ||
+                   !strcmp(bt->status, "admin_down") ||
+                   !strcmp(bt->status, bfd_get_status(entry->state))) {
+            entry->next_reconcile = 0;
+            entry->reconcile_attempts = 0;
         }
         bfd_monitor_check_sb_conf(bt, entry);
         entry->erase = false;

tests/ovn.at
@@ -47254,3 +47254,127 @@ AT_CHECK([grep "skipping output to input port" \
 OVN_CLEANUP([hv1])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([BFD - reconcile SB status with local session state])
+AT_KEYWORDS([bfd])
+ovn_start
+
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+
+check ovn-nbctl lr-add R1
+check ovn-nbctl ls-add sw0
+check ovn-nbctl ls-add public
+
+check ovn-nbctl lrp-add R1 rp-sw0 00:00:01:01:02:03 192.168.1.1/24
+check ovn-nbctl lrp-add R1 rp-public 00:00:02:01:02:03 172.16.1.1/24
+check ovn-nbctl lrp-set-gateway-chassis rp-public hv1
+
+check ovn-nbctl lsp-add sw0 sw0-rp -- set Logical_Switch_Port sw0-rp \
+    type=router options:router-port=rp-sw0 -- lsp-set-addresses sw0-rp router
+
+check ovn-nbctl lsp-add public public-rp -- set Logical_Switch_Port \
+    public-rp type=router options:router-port=rp-public \
+    -- lsp-set-addresses public-rp router
+
+# Wait for the distributed gateway port to be claimed by hv1.
+OVS_WAIT_UNTIL([
+    hv1_uuid=$(ovn-sbctl --bare --columns _uuid list chassis hv1)
+    cr_ch=$(ovn-sbctl --bare --columns chassis list port_binding cr-rp-public)
+    test -n "$hv1_uuid" && test "$cr_ch" = "$hv1_uuid"
+])
+
+# Create a BFD session towards a peer that never answers: the local
+# session on hv1 stays down.
+check ovn-nbctl --bfd lr-route-add R1 100.0.0.0/8 172.16.1.50 rp-public
+check ovn-nbctl --wait=sb sync
+wait_row_count bfd 1
+
+# ovn-northd only fills in chassis_name when the logical port is bound,
+# which never happens for a distributed gateway port, and the SB RBAC
+# permissions for the BFD table (used when the sandbox connects the
+# chassis over SSL) authorize status updates through chassis_name.
+# Attribute the session to hv1 the way an l3gateway row would be.
+check ovn-sbctl set bfd . chassis_name=hv1
+
+wait_column "down" bfd status logical_port=rp-public
+
+# Scenario 1: externally rewrite the status of the SB row while the
+# local session state does not change.  Since no state machine
+# transition happens, only the reconciliation logic in ovn-controller
+# can repair the row.
+check ovn-sbctl set bfd . status=up
+wait_column "down" bfd status logical_port=rp-public
+
+check ovn-sbctl set bfd . status=init
+wait_column "down" bfd status logical_port=rp-public
+
+# Scenario 2: a session (re)created while the SB row holds a stale
+# status.  Stop ovn-controller, fake a stale "up" left over from a
+# previous incarnation, then restart the controller: the fresh session
+# never establishes (no peer), so without reconciliation the bogus "up"
+# would persist forever.
+check as hv1 ovn-appctl -t ovn-controller exit
+check ovn-sbctl set bfd . status=up
+
+as hv1 start_daemon ovn-controller
+OVS_WAIT_UNTIL([
+    hv1_uuid=$(ovn-sbctl --bare --columns _uuid list chassis hv1)
+    cr_ch=$(ovn-sbctl --bare --columns chassis list port_binding cr-rp-public)
+    test -n "$hv1_uuid" && test "$cr_ch" = "$hv1_uuid"
+])
+wait_column "down" bfd status logical_port=rp-public
+
+# Scenario 3: reconciliation writes that never take effect.  With SSL
+# available the sandbox chassis connects through the pssl+RBAC
+# connection (role=ovn-controller, see ovn_start), and the SB RBAC
+# permissions authorize BFD status updates through chassis_name.
+# Mis-attribute the session to another chassis: the server rejects
+# hv1's corrective writes, the row stays divergent and ovn-controller
+# must warn that its updates do not take effect instead of silently
+# retrying forever.  Without SSL the chassis connects over the unix
+# socket, no RBAC is enforced and the writes cannot be made to fail,
+# so the scenario is skipped.
+if test X$HAVE_OPENSSL = Xyes; then
+    check ovn-sbctl set bfd . chassis_name=other
+    check ovn-sbctl set bfd . status=up
+    OVS_WAIT_UNTIL([grep -q "do not appear to take effect" \
+                    hv1/ovn-controller.log])
+    # The planted value must still be in place: the writes were rejected.
+    check_column up bfd status logical_port=rp-public
+
+    # Restore chassis_name: the next reconciliation attempt goes through
+    # and the row converges.
+    check ovn-sbctl set bfd . chassis_name=hv1
+    wait_column "down" bfd status logical_port=rp-public
+
+    # Once converged no further reconciliation writes may be issued: the
+    # attempt coverage counter must stop moving and no new warnings may
+    # accumulate across more than one reconciliation interval (5s).
+    reconcile_attempts() {
+        as hv1 ovn-appctl -t ovn-controller coverage/read-counter \
+            pinctrl_bfd_status_reconcile
+    }
+    warn_count() {
+        grep -c "do not appear to take effect" hv1/ovn-controller.log
+    }
+    attempts=$(reconcile_attempts)
+    warns=$(warn_count)
+    sleep 7
+    check_column down bfd status logical_port=rp-public
+    check test "$(reconcile_attempts)" = "$attempts"
+    check test "$(warn_count)" = "$warns"
+fi
+
+# The RBAC scenario intentionally provokes rejected SB transactions and
+# the resulting divergence warning in hv1's log.
+OVN_CLEANUP([hv1
+/do not appear to take effect/d
+/transaction error/d
+])
+AT_CLEANUP
+])